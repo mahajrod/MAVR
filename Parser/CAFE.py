bf1a528300d9e4fd03767dcf5f97efcd0868ca3d
Sergei Kliver <mahajrod@gmail.com>
2015-02-10 02:40:41 +0300

    added CAFE parser

--- Parser/CAFE.py
+++ Parser/CAFE.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 __author__ = 'mahajrod'
 
-from ete2 import Tree
+import numpy as np
+from ete2 import Tree, TreeStyle, AttrFace, faces
 
 from Parser.Abstract import Header
 
 
 class HeaderCAFE(list, Header):
-
     def __str__(self):
         return "#" + "\t".join(self)
 
@@ -24,61 +24,110 @@ class RecordCAFE():
         self.tree = tree
         self.family_p_value = family_p_value
 
+    def __str__(self):
+        return "%s\t%f\t%s" % (self.id, self.family_p_value,
+                               self.tree.write(format=100,
+                                               features=self.tree.features - set(["support", "name", "dist"]),
+                                               format_root_node=True))
+
+    def node_str(self):
+        node_string = "%s\t%f" % (self.id, self.family_p_value)
+        features = self.tree.features - set(["support", "name", "dist"])
+        for node in self.tree.traverse():
+            list_of_features = [eval("node.%s" % feature) for feature in features]
+            list_of_features = map(lambda x: str(x) if x is not None else ".", list_of_features)
+            node_string += "\t" + ",".join(list_of_features)
+        return node_string
+
+
 class GeneralDataCAFE():
     def __init__(self, tree, lambda_value=None):
         self.tree = tree
         self.lambda_value = lambda_value
+        self.node_id_list = [node.id for node in self.tree.traverse()]
+
+    def __str__(self):
+        tree_str = "##Common tree\t%s\n" % self.tree.write(format=8,
+                                                           features=self.tree.features - set(["support", "name"]),
+                                                           format_root_node=True)
+        lambda_str = "##Lambda\t%f" % self.lambda_value
+        return tree_str + lambda_str
+
+    def draw(self, out_file_prefix, w=140, units="mm"):
+        def layout(node, feature):
+            if node.up is not None or feature == "id":
+                attr = AttrFace(feature, fsize=5, fgcolor="green")
+                faces.add_face_to_node(attr, node, 0, position="branch-top")
+        for feature in self.tree.features - set(["support", "name"]):
+            if feature[0] == "_":
+                continue            # skip _nid feature and other technical features of tree
+
+            def layout_arg(node):
+                return layout(node, feature)
+            ts = TreeStyle()
+            ts.layout_fn = layout_arg
+            self.tree.render("%s_%s.png" % (out_file_prefix, feature), w=w, units=units, tree_style=ts)
 
 
 class ReportCAFE():
-    def __init__(self, report_file):
-        with open(report_file, "r") as report_fd:
-            common_tree = Tree(report_fd.readline().strip()[5:] + ";")
-            lambda_value = float(report_fd.readline().strip().split(":")[-1])
-            id_tree = Tree(report_fd.readline().strip().split(":")[-1] + ";", format=8)
-            for id_tree_node, common_tree_node in zip(id_tree.traverse(), common_tree.traverse()):
-                node_id = int(id_tree_node.name.split("<")[-1][:-1])
-                common_tree_node.add_feature("id", node_id)
-            data_format1 = self.split_format1(report_fd.readline().strip().split(":")[-1])
-            data_format2 = report_fd.readline().strip().split(":")[-1].split()
-            data_format2[0] = data_format2[0][1:-1]
-            for i in range(1, len(data_format2)):
-                data_format2[i] = data_format2[i][:-1]
-            data_format2 = map(lambda x: int(x), data_format2)
-            for data_type in [float, int, int, int]:
-                name, data = report_fd.readline().strip().split(":")
-                data = self.split_format1(data, data_type=data_type)
-                name = name.strip().replace(" ", "_").lower()
-                for node in common_tree.traverse():
-                    node.add_feature(name, data[data_format1.index(node.id)] if node.id in data_format1 else None)
+    def __init__(self, report_file=None, from_file=True, records=None, metadata=None, general_data=None, header=None):
+        if from_file:
+            with open(report_file, "r") as report_fd:
+                common_tree = Tree(report_fd.readline().strip()[5:] + ";")
+                lambda_value = float(report_fd.readline().strip().split(":")[-1])
+                id_tree = Tree(report_fd.readline().strip().split(":")[-1] + ";", format=8)
+                for id_tree_node, common_tree_node in zip(id_tree.traverse(), common_tree.traverse()):
+                    node_id = int(id_tree_node.name.split("<")[-1][:-1])
+                    common_tree_node.add_feature("id", node_id)
+                data_format1 = self.split_format1(report_fd.readline().strip().split(":")[-1])
+                data_format2 = report_fd.readline().strip().split(":")[-1].split()
+                data_format2[0] = data_format2[0][1:-1]
+                for i in range(1, len(data_format2)):
+                    data_format2[i] = data_format2[i][:-1]
+                data_format2 = map(lambda x: int(x), data_format2)
+                for data_type in [float, int, int, int]:
+                    name, data = report_fd.readline().strip().split(":")
+                    data = self.split_format1(data, data_type=data_type)
+                    name = name.strip().replace(" ", "_").lower()
+                    for node in common_tree.traverse():
+                        node.add_feature(name, data[data_format1.index(node.id)] if node.id in data_format1 else None)
 
-            self.general_data = GeneralDataCAFE(common_tree, lambda_value=lambda_value)
-            self.metadata = MetadataCAFE(data_format1, data_format2)
-            self.header = HeaderCAFE(report_fd.readline().strip().split("\t"))
-            self.records = []
-            for line in report_fd:
-                id, tree, p_value, vitebri_p_value = line.strip().split()
-                p_value = float(p_value)
-                tree = Tree(tree + ";", format=8)
-                #print(vitebri_p_value)
-                vitebri_p_value = self.split_format1(vitebri_p_value[1:-1], separator=",", data_type=float)
-                for tree_node, id_node in zip(tree.traverse(), self.general_data.tree.traverse()):
-                    if tree_node.is_leaf:
-                        tree_node.name, number_of_genes = tree_node.name.split("_")
-                    else:
-                        number_of_genes = tree_node.name[1:]
-                    #print (node.name)
-                    number_of_genes = int(number_of_genes)
-                    tree_node.add_feature("N", number_of_genes)
-                    tree_node.add_feature("p_value", vitebri_p_value[data_format1.index(id_node.id)] if id_node.id in data_format1 else None)
-                    parent_tree_node = tree_node.up
-                    if parent_tree_node is None:
-                        delta = None
-                    else:
-                        delta = tree_node.N - parent_tree_node.N
-                    tree_node.add_feature("delta", delta)
+                self.general_data = GeneralDataCAFE(common_tree, lambda_value=lambda_value)
+                self.metadata = MetadataCAFE(data_format1, data_format2)
+                self.header = HeaderCAFE(report_fd.readline().strip().split("\t"))
+                self.records = []
+                for line in report_fd:
+                    id, tree, p_value, vitebri_p_value = line.strip().split()
+                    p_value = float(p_value)
+                    tree = Tree(tree + ";", format=8)
+                    #print(vitebri_p_value)
+                    vitebri_p_value = self.split_format1(vitebri_p_value[1:-1], separator=",", data_type=float)
+                    for tree_node, id_node in zip(tree.traverse(), self.general_data.tree.traverse()):
+                        if tree_node.is_leaf:
+                            tree_node.name, number_of_genes = tree_node.name.split("_")
+                        else:
+                            number_of_genes = tree_node.name[1:]
+                        #print (node.name)
+                        number_of_genes = int(number_of_genes)
+                        tree_node.add_feature("N", number_of_genes)
+                        tree_node.add_feature("p_value", vitebri_p_value[data_format1.index(id_node.id)] if id_node.id in data_format1 else None)
+                        parent_tree_node = tree_node.up
+                        if parent_tree_node is None:
+                            delta = None
+                        else:
+                            delta = tree_node.N - parent_tree_node.N
+                        tree_node.add_feature("delta", delta)
 
-                self.records.append(RecordCAFE(id, tree, p_value))
+                    self.records.append(RecordCAFE(id, tree, p_value))
+        else:
+            self.general_data = general_data
+            self.metadata = metadata
+            self.header = header
+            self.records = records
+
+    def __iter__(self):
+        for record in self.records:
+            yield record
 
     def split_format1(self, format1_string, separator=None, data_type=int):
 
@@ -89,18 +138,71 @@ class ReportCAFE():
             format1 += map(lambda x: None if x == '-' else data_type(x), tmp_format1[i].split(","))
         return format1
 
+    def convert(self, out_file, output_type="tree"):
+        # ouput_type - possible values: tree, node
+        with open(out_file, "w") as out_fd:
+            out_fd.write(str(self.general_data))
+            out_fd.write("\n")
+            if output_type == "tree":
+                header = "#id\tfamily p-value\ttree\n"
+                out_fd.write(header)
+                for record in self:
+                    out_fd.write(str(record))
+                    out_fd.write("\n")
+            elif output_type == "node":
+                out_fd.write("#Node_format: %s\n" % (",".join(self.records[0].tree.features - set(["support", "name", "dist"]))))
+                header = "#id\tfamily_p_value"
+                for node in self.general_data.tree.traverse():
+                    header += "\t%s" % node.id
+                header += "\n"
+                out_fd.write(header)
+                for record in self:
+                    out_fd.write(record.node_str())
+                    out_fd.write("\n")
+
+    def get_per_node_values(self, filter_by_p_value=False, p_value_cutoff=0.05):
+        node_values_dict = dict((node_id, []) for node_id in self.general_data.node_id_list)
+        feature_names_list = ["family_id"] + [feature for feature in self.records[0].tree.features - set(["support", "name", "dist"])]
+        for record in self:
+            node_index = 0
+            for node in record.tree.traverse():
+                if not filter_by_p_value or node.p_value <= p_value_cutoff:
+                    node_values_dict[self.general_data.node_id_list[node_index]].append([record.id] + [eval("node.%s" % feature) for feature in node.features - set(["support", "name", "dist"])])
+                node_index += 1
+        return node_values_dict, feature_names_list
+
+    #----------------------------General filtering functions--------------------------------------
+    def filter_records(self, expression_function):
+        #expression_function must return  boolean value
+        filtered = []
+        filtered_out = []
+        for record in self:
+            if expression_function(record):
+                filtered.append(record)
+            else:
+                filtered_out.append(record)
+        return filtered, filtered_out
+
+    def filter(self, expression_function):
+        filtered_records, filtered_out_records = self.filter_records(expression_function)
+
+        return ReportCAFE(from_file=False, records=filtered_records, general_data=self.general_data,
+                          metadata=self.metadata, header=self.header), \
+               ReportCAFE(from_file=False, records=filtered_out_records, general_data=self.general_data,
+                          metadata=self.metadata, header=self.header),
+    #---------------------------------------------------------------------------------------------
+
+    #--------------------------Custom filtering functions-----------------------------------------
+    def filter_by_family_p_value(self, p_value_cutoff):
+        return self.filter(lambda record: record.family_p_value <= p_value_cutoff)
+
 
 if __name__ == "__main__":
     cafe_report_file = "/home/mahajrod/genetics/Dobrzhansky/project/gene_families/data/pashas/resultfilereport.cafe"
     cafe_report = ReportCAFE(cafe_report_file)
     #print(cafe_report.general_data.tree.write(features=["id", "remain", "expansion", "decrease", "average_expansion"]))
     #print(cafe_report.general_data.lambda_value)
-    print(cafe_report.records[3].tree.write(features=["N", "delta", "p_value"], format=8))
-    for node in cafe_report.records[3].tree.traverse():
-        print(node.name, node.N)
-        parent_node = node.up
-        if parent_node is None:
-            print("None")
-        else:
-            print(parent_node.name, parent_node.N)
-        print ("")
+    #print(cafe_report.records[3].tree.write(features=["N", "delta", "p_value"], format=8, format_root_node=True))
+
+    cafe_report.general_data.tree.render("mytree.png", w=183, units="mm")
+    cafe_report.general_data.draw("labeled_tree")
---
+++ scripts/analyse_cafe_report.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+__author__ = 'mahajrod'
+import os
+import argparse
+from random import choice
+
+import matplotlib.pyplot as plt
+from numpy import linspace
+
+from collections import OrderedDict
+from Collections.GeneralCollections import TwoLvlDict
+from Parser.CAFE import ReportCAFE
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-c", "--cafe_report", action="store", dest="cafe_report",
+                    help="Input cafe report")
+parser.add_argument("-n", "--convert", action="store_true", dest="convert_flag", default=False,
+                    help="If present enables conversion of report to tree and node variants")
+parser.add_argument("-g", "--general_trees_prefix", action="store", dest="general_trees_prefix", default="general_tree",
+                    help="Prefix for general trees")
+parser.add_argument("-o", "--output_prefix", action="store", dest="out_prefix", default="node",
+                    help="Prefix for node specific output")
+parser.add_argument("-f", "--family_pvalue", action="store", dest="family_p_value", type=float, default=0.05,
+                    help="Family p-value cutoff")
+parser.add_argument("-p", "--node_pvalue", action="store", dest="node_p_value", type=float, default=0.05,
+                    help="Node p-value cutoff")
+parser.add_argument("-r", "--ref_species_gene_file", action="store", dest="ref_species_gene_file",
+                    help="File with gene of genes families of reference species ")
+args = parser.parse_args()
+
+general_trees_dir = "general_trees/"
+node_info_dir = "node_info/"
+node_ref_dir = "node_ref/"
+converted_dir = "converted/"
+statistics_dir = "statistics/"
+background_genes_dir = "background/"
+hist_subdir = "histogram/"
+
+for directory in general_trees_dir, node_info_dir, node_ref_dir, converted_dir, statistics_dir, background_genes_dir:
+    try:
+        os.mkdir(directory)
+    except OSError:             # intercept exception raised if directory already exists
+        pass
+for directory in node_info_dir, node_ref_dir:
+    for subdirectory in "all/", "new/", "lost/":
+        try:
+            os.mkdir(directory + subdirectory)
+        except OSError:             # intercept exception raised if directory already exists
+            pass
+try:
+    os.mkdir(node_info_dir + hist_subdir)
+except OSError:             # intercept exception raised if directory already exists
+    pass
+cafe_report = ReportCAFE(report_file=args.cafe_report, from_file=True)
+if args.convert_flag:
+    for output_type in "tree", "node":
+        print("Converting report to %s format" % output_type)
+        cafe_report.convert(converted_dir + "converted_report_mode_%s.t" % output_type, output_type=output_type)
+cafe_report.general_data.draw(general_trees_dir + args.general_trees_prefix)
+
+filtered_report, filtered_out_report = cafe_report.filter_by_family_p_value(args.family_p_value)
+node_values, features_list = filtered_report.get_per_node_values(filter_by_p_value=True,
+                                                                 p_value_cutoff=args.node_p_value)
+print("Totally %i gene families without statistically significant changes" % len(filtered_out_report.records))
+
+reference_genes_dict = {}
+with open(args.ref_species_gene_file, "r") as ref_fd:
+    for line in ref_fd:
+        gene_family_id, genes = line.strip().split("\t")
+        genes = [] if genes == "." else genes.split(",")
+        reference_genes_dict[gene_family_id] = [genes[:]]
+        if genes:
+            reference_genes_dict[gene_family_id].append(choice(genes))
+
+node_header_list = features_list + ["reference_gene"]
+delta_index = features_list.index("delta")
+statistics_dict = TwoLvlDict({})
+
+for node_id in node_values:
+    statistics_dict[node_id] = OrderedDict({"lost": 0, "new": 0, "lost_ref_ann": 0, "new_ref_ann": 0})
+
+for node_id in node_values:
+    fd_list = []
+    for directory in node_info_dir, node_ref_dir:
+        for mode in "all", "new", "lost":
+            fd_list.append(open(directory + "%s/%s_%s_%s.t" % (mode, args.out_prefix, node_id, mode), "w"))
+
+    all_fd, new_fd, lost_fd, all_ref_id_fd, new_ref_id_fd, lost_ref_id_fd = fd_list
+
+    for fd in all_fd, new_fd, lost_fd:
+        fd.write("#%s\n" % "\t".join(node_header_list))
+    lost_values = []
+    new_values = []
+    for entry in node_values[node_id]:
+        if entry[delta_index] is None:
+            continue
+        node_string = "\t".join(map(lambda x: str(x), entry))
+        node_reference_genes = reference_genes_dict[entry[0]][0]
+        if node_reference_genes:
+            random_reference_gene = reference_genes_dict[entry[0]][1]
+            all_ref_id_fd.write(random_reference_gene + "\n")
+        else:
+            random_reference_gene = "."
+        node_string += "\t%s\n" % random_reference_gene
+        all_fd.write(node_string)
+        if entry[delta_index] > 0:
+            new_fd.write(node_string)
+            statistics_dict[node_id]["new"] += 1
+            new_values.append(entry[delta_index])
+            if node_reference_genes:
+                new_ref_id_fd.write(random_reference_gene + "\n")
+                statistics_dict[node_id]["new_ref_ann"] += 1
+        elif entry[delta_index] < 0:
+            lost_fd.write(node_string)
+            statistics_dict[node_id]["lost"] += 1
+            lost_values.append(-entry[delta_index])
+            if node_reference_genes:
+                lost_ref_id_fd.write(random_reference_gene + "\n")
+                statistics_dict[node_id]["lost_ref_ann"] += 1
+
+    plt.figure(1, figsize=(5, 10))
+    index = 1
+    subplot_list = []
+    for values, info in zip([new_values, lost_values], ["new", "lost"]):
+        if not values:
+            continue
+        maximum = max(values)
+        subplot_list.append(plt.subplot(2, 1, index))
+        bins = linspace(1, maximum, maximum)
+        plt.hist(values, bins=bins)
+        plt.xlabel("Number of %s genes in family" % info)
+        plt.xlim(xmin=1)
+        plt.ylabel("Number of families")
+        index += 1
+    plt.suptitle("Distribution of losses and additions in gene families")
+    plt.savefig(node_info_dir + hist_subdir + "%s_%s.svg" % (args.out_prefix, node_id))
+    plt.close()
+    for fd in fd_list:
+        fd.close()
+
+with open(background_genes_dir + "background_genes.t", "w") as back_fd:
+    with open(background_genes_dir + "background_genes_list.txt", "w") as back_list_fd:
+        back_fd.write("#id\tfamaliy_p_value\tref_gene\n")
+        for record in filtered_out_report:
+            #print(record)
+            if reference_genes_dict[record.id][0]:
+                random_reference_gene = choice(reference_genes_dict[record.id][0])
+                back_list_fd.write(random_reference_gene + "\n")
+            else:
+                random_reference_gene = "."
+            back_string = "%s\t%f\t%s\n" % (record.id, record.family_p_value, random_reference_gene)
+            back_fd.write(back_string)
+
+statistics_dict.write(statistics_dir + "node_statistics.t", absent_symbol=".")
+
+
+
