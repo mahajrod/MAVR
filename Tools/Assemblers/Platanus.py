3d70c2fd399140c54ce8966cc3a9536842f06ed0
Sergei Kliver <mahajrod@gmail.com>
2015-06-13 18:00:52 +0200

    a

--- Tools/Assemblers/Platanus.py
+++ Tools/Assemblers/Platanus.py
@@ -6,11 +6,124 @@ import os
 from Tools.Abstract import Tool
 
 
-class ToolTemplate(Tool):
+class Platanus(Tool):
     def __init__(self, path="", max_threads=4):
-        Tool.__init__(self, "tool_command", path=path, max_threads=max_threads)
+        Tool.__init__(self, "platanus", path=path, max_threads=max_threads)
 
-    def tool_method(self, tool_options):
+    def assemble_unitigs(self, reads_files, out_prefix="out", memory_limit=None,
+                         kmer_extension_step=None, branch_cutting_diference=None,
+                         bubble_crush_difference=None, initial_kmer_coverage_cutoff=None,
+                         min_kmer_coverage=None, kmer_extension_safety_level=None,
+                         unitig_assembly_log="unitig_assembly.log"):
+        # TODO: add -k and -K options
+
+        options = " -t %i" % self.threads
+        options += " -o %s" % out_prefix
+        options += " -m %i" % memory_limit if memory_limit else ""
+        options += " -d %f" % branch_cutting_diference if branch_cutting_diference else ""
+        options += " -u %f" % bubble_crush_difference if bubble_crush_difference else ""
+        options += " -s %i" % kmer_extension_step if kmer_extension_step else ""
+        options += " -n %i" % initial_kmer_coverage_cutoff if initial_kmer_coverage_cutoff else ""
+        options += " -c %i" % min_kmer_coverage if min_kmer_coverage else ""
+        options += " -a %f" % kmer_extension_safety_level if kmer_extension_safety_level else ""
+        options += " "
+        options += " "
+        reads_string = ""
+        for reads in reads_files:
+            if isinstance(reads, str):
+                reads_string += " %s" % reads
+            else:
+                reads_string += " " + " ".join(reads)
+        options += " -f %s" % reads_string
+        options += " > %s" % unitig_assembly_log
+        self.execute(options, cmd="platanus assemble")
+
+    def scaffold(self, contig_file, bubble_file, pe_reads_list=None, mp_reads_list=None,
+                 out_prefix="out", min_insert_size=None,scaffolding_log="scaffolding.log"):
+
+        if (not pe_reads_list) and (not mp_reads_list):
+            raise ValueError("Both PE and MP read files were not set")
+
+        options = " -t %i" % self.threads
+        options += " -o %s" % out_prefix
+        options += " -c %s" % contig_file
+        options += " -b %s" % bubble_file
+        options += " -n %i" % min_insert_size
+        options += " "
+        options += " "
+        options += " "
+        options += " "
+        options += " "
+        options += " > %s" % scaffolding_log
+
+        """
+         Map paired-end (mate-pair) reads on contigs, determine the order of contigs
+    and construct scaffolds.
+
+
+    INPUT OPTIONS
+
+    -ip{INT} PAIR1 [PAIR2 ...]         : Lib_id inward_pair_file (reads in 1 file, fasta or fastq)
+                                         Ex. -ip1 lib1.fa
+
+    -IP{INT} FWD1 REV1 [FWD2 REV2 ...] : Lib_id inward_pair_files (reads in 2 files, fasta or fastq)
+                                         Ex. -IP1 lib1_1.fa lib1_2.fa
+
+    -op{INT} PAIR1 [PAIR2 ...]         : Lib_id outward_pair_file (reads in 1 file, fasta or fastq)
+                                         Ex. -op1 lib1.fa
+
+    -OP{INT} FWD1 REV1 [FWD2 REV2 ...] : lib_id outward_pair_files (reads in 2 files, fasta or fastq)
+                                         Ex. -OP1 lib1_1.fa lib1_2.fa
+
+                                         The file format is automatically detected.
+                                         see "***** NOTE ***** Paired-end (Mate-pair) input" below
+
+
+    OTHER OPTIONS
+
+    -a{INT1} INT2 : lib_id average_insert_size
+                    Fixed average insert size (INT) is used instead of auto estimation.
+
+    -d{INT1} INT2 : lib_id SD_insert_size
+                    Fixed SD of insert size (INT) is used instead of auto estimation.
+
+    -s INT        : Mapping seed length (default 32)
+                    Seed length must not be larger than reads length. Smaller INT
+                    decrease speed.
+
+    -v INT        : Minimum overlap length (default 32)
+                    If adjacent contigs have overlap (length >= INT) and properly
+                    close to each other, the contigs are joined.
+
+    -l INT        : Minimum number of link (default 3)
+                    Platanus first estimates the threshold of link (number) and
+                    makes scaffolds, then decreases the threshold to INT and
+                    extends scaffolds.
+
+    -u FLOAT      : Maximum difference for bubble crush (identity, default 0.1)
+                    Larger FLOAT increases the number of bubbles merged. If
+                    heterozygosity of the sample is high, large FLOAT may be
+                    suitable (Ex. -u 0.2).
+
+
+    OUTPUT FILES
+
+    PREFIX_scaffold.fa           : assembled sequences that include gaps('N's mean gaps)
+
+    PREFIX_scaffoldBubble.fa     : removed bubble sequences
+
+    PREFIX_scaffoldComponent.tsv : the information about composition of scaffolds
+                                   (i.e. which contigs constitute a scaffold)
+
+--------------------------------------------------------------------------------
+
+
+        """
+
+
+        self.execute(options, cmd="platanus scaffold")
+
+    def gap_close(self, tool_options):
         options = ""
         options += " "
         options += " "
@@ -22,7 +135,5 @@ class ToolTemplate(Tool):
         options += " "
         options += " "
         self.execute(options, cmd="custom command if necessary ex: tool_command subcommand")
-
-
 if __name__ == "__main__":
     pass
---
+++ examples/desaminases/rename_run_7.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import sys
+import argparse
+
+import os
+
+samples_dict = {"Sample_1": "N085-LAN210-Can-PmCDA1-NA-RUN7-D1",
+                "Sample_2": "N086-LAN210-Can-PmCDA1-NA-RUN7-D1",
+                "Sample_3": "N087-LAN210-Can-PmCDA1-NA-RUN7-D1",
+                "Sample_4": "N088-LAN210-Can-PmCDA1-NA-RUN7-D1",
+                "Sample_5": "N089-LAN210-Can-PmCDA1-NA-RUN7-D1",
+                "Sample_6": "N090-LAN210-Can-PmCDA1-NA-RUN7-D1",
+                "Sample_7": "N091-LAN210-Can-PmCDA1-NA-RUN7-D3",
+                "Sample_8": "N092-LAN210-Can-PmCDA1-NA-RUN7-D6",
+                "Sample_9": "N093-LAN210-Can-PmCDA1-NA-RUN7-D6",
+                "Sample_10": "N094-LAN210-Can-PmCDA1-NA-RUN7-D6",
+                "Sample_11": "N095-LAN210-Can-PmCDA1-NA-RUN7-D6",
+                "Sample_12": "N096-LAN210-Can-PmCDA1-NA-RUN7-D6",
+                "Sample_13": "N097-LAN210-Can-AID-NA-RUN7-D1",
+                "Sample_14": "N098-LAN210-Can-AID-NA-RUN7-D1",
+                "Sample_15": "N099-LAN210-Can-AID-NA-RUN7-D1",
+                "Sample_16": "N100-LAN210-Can-AID-NA-RUN7-D3",
+                "Sample_17": "N101-LAN210-Can-AID-NA-RUN7-D3",
+                "Sample_18": "N102-LAN210-Can-AID-NA-RUN7-D3",
+                "Sample_19": "N103-LAN210-Can-AID-NA-RUN7-D6",
+                "Sample_20": "N104-LAN210-Can-AID-NA-RUN7-D6",
+                "Sample_21": "N105-LAN210-Can-AID-NA-RUN7-D6",
+                "Sample_22": "N106-LAN210-Can-AID-NA-RUN7-D6",
+                "Sample_23": "N107-LAN210-Can-A1-NA-RUN7-D1",
+                "Sample_24": "N108-LAN210-Can-A1-NA-RUN7-D1",
+                "Sample_25": "N109-LAN210-Can-A1-NA-RUN7-D1",
+                "Sample_26": "N110-LAN210-Can-A1-NA-RUN7-D1",
+                "Sample_27": "N111-LAN210-Can-A1-NA-RUN7-D1",
+                "Sample_28": "N112-LAN210-Can-A1-NA-RUN7-D1",
+                "Sample_29": "N113-LAN210-Can-A1-NA-RUN7-D3",
+                "Sample_30": "N114-LAN210-Can-A1-NA-RUN7-D3",
+                "Sample_31": "N115-LAN210-Can-A1-NA-RUN7-D3",
+                "Sample_32": "N116-LAN210-Can-A1-NA-RUN7-D3",
+                "Sample_33": "N117-LAN210-Can-A1-NA-RUN7-D3",
+                "Sample_34": "N118-LAN210-Can-A1-NA-RUN7-D3",
+                "Sample_35": "N119-LAN210-Can-A1-NA-RUN7-D3",
+                "Sample_36": "N120-LAN210-Can-A1-NA-RUN7-D6",
+                "Sample_37": "N121-LAN210-Can-A1-NA-RUN7-D6",
+                "Sample_38": "N122-LAN210-Can-A1-NA-RUN7-D6",
+                "Sample_39": "N123-LAN210-Can-A1-NA-RUN7-D6",
+                "Sample_40": "N124-LAN210-Can-A1-NA-RUN7-D6"}
+
+raw_dir = "raw_names/"
+workdir = "./"
+
+for sample in samples_dict:
+    os.system("mkdir -p %s" % samples_dict[sample])
+    sample_files = os.listdir(raw_dir + sample)
+    for filename in sample_files:
+        if "R1" in filename:
+            os.system("cp %s%s %s/%s_1.fastq" % (raw_dir, filename, samples_dict[sample], samples_dict[sample]))
+        elif "R2" in filename:
+            os.system("cp %s%s %s/%s_2.fastq" % (raw_dir, filename, samples_dict[sample], samples_dict[sample]))
+        else:
+            os.system("cp %s%s %s/" % (raw_dir, filename, samples_dict[sample]))
