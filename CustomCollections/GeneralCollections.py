6e71e04073ea08ae78443e789e694b201f97183b
Sergei Kliver <mahajrod@gmail.com>
2015-06-24 18:04:18 +0200

    Added OrderedSet implementation

--- CustomCollections/GeneralCollections.py
+++ CustomCollections/GeneralCollections.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import numpy as np
-from collections import OrderedDict
+from collections import OrderedDict, MutableSet
 
 
 class TwoLvlDict(OrderedDict):
@@ -33,3 +33,71 @@ class TwoLvlDict(OrderedDict):
         with open(out_filename, "w") as out_fd:
             out_fd.write(self.table_form(absent_symbol=absent_symbol))
 
+
+class OrderedSet(MutableSet):
+
+    def __init__(self, iterable=None):
+        self.end = end = []
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
+
+    @staticmethod
+    def union(*sets):
+        union = OrderedSet()
+        union.union(*sets)
+        return union
+
+    def union(self, *sets):
+        for set in sets:
+            self |= set
