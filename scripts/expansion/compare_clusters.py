c4df29971639f4c34a62eeef71ee9e1f918b3762
Sergei Kliver <mahajrod@gmail.com>
2015-07-30 06:53:04 +0200

    small changes in compare_clusters.py

--- scripts/expansion/compare_clusters.py
+++ scripts/expansion/compare_clusters.py
@@ -99,7 +99,7 @@ for ref_cluster_id in ref_clusters_dict:
             if len(include_dict[ref_cluster_id]) == 1 and (ref_cluster_id not in contained_in_dict):
                 print ref_cluster_id
                 synonym_dict[ref_cluster_id] = include_dict.pop(ref_cluster_id)
-
+                print synonym_dict[ref_cluster_id]
 number_of_common_families = len(synonym_dict)
 contained_fully_in_number = len(contained_fully_in_dict)
 contained_in_number = len(contained_in_dict)
