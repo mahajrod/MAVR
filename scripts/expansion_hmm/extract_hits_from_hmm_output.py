f375e6073161570673ff3343ebbceeb8a796bcb9
mahajrod <mahajrod@gmail.com>
2015-07-30 17:25:47 +0300

    added extract_hits_from_hmm_output.py

---
+++ scripts/expansion_hmm/extract_hits_from_hmm_output.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import argparse
+
+from Bio import SearchIO
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_file", action="store", dest="input", required=True,
+                    help="Input file with mm output")
+parser.add_argument("-f", "--format", action="store", dest="format", required=True,
+                    help="Format of input hmm file.")
+parser.add_argument("-o", "--output_file", action="store", dest="output", default="stdout",
+                    help="Output file")
+args = parser.parse_args()
+
+hmm_dict = SearchIO.index_db("temp.idx", args.input, args.format)
+out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
+out_fd.write("#family\tgene\tevalue\tbitscore\n")
+for family in hmm_dict:
+    #print hmm_dict[key]
+    for hit in hmm_dict[family]:
+        if hit.is_included:
+            out_fd.write("%s\t%s\t%s\t%s\n" % (family, hit.id, hit.evalue, hit.bitscore))
+if args.output != "stdout":
+    out_fd.close()
