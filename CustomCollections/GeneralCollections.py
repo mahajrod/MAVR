718778b51451b2e61091fe1fc79e43290c6b9ee7
Sergei Kliver <mahajrod@gmail.com>
2015-07-30 05:52:15 +0200

    small changes in compare_clusters.py

--- CustomCollections/GeneralCollections.py
+++ CustomCollections/GeneralCollections.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import numpy as np
-import igraph as ig
+#import igraph as ig
 from collections import OrderedDict, MutableSet
 
-
+"""
 class Graph(ig.Graph):
     def read(self, in_file, format="ncol"):
         self.Read(in_file, format=format)
-
+"""
 
 class TwoLvlDict(OrderedDict):
 
