dbafddef08d976aef9781de91ea74f9ff07705b1
mahajrod <mahajrod@gmail.com>
2015-07-31 18:07:21 +0300

    added extract_hits_from_hmm_output.py

--- scripts/expansion_hmm/extract_top_hits_from_hmm_output.py
+++ scripts/expansion_hmm/extract_top_hits_from_hmm_output.py
@@ -12,23 +12,37 @@ parser.add_argument("-i", "--input_file", action="store", dest="input", required
                     help="Input file with hmm output")
 parser.add_argument("-f", "--format", action="store", dest="format", required=True,
                     help="Format of input hmm file.")
+
 parser.add_argument("-o", "--output_file", action="store", dest="output", default="stdout",
                     help="Output file")
+parser.add_argument("-n", "--not_found", action="store", dest="not_found", default="not_found.ids",
+                    help="File to write ids of not found queries")
+parser.add_argument("-i", "--not_significant", action="store", dest="not_significant", default="not_significant.hits",
+                    help="File to write ids of quaries with not significant hits")
 args = parser.parse_args()
 
 hmm_dict = SearchIO.index_db("temp.idx", args.input, args.format)
 out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
 out_fd.write("#query\thit\tevalue\tbitscore\n")
-for family in hmm_dict:
+
+nf_fd = open(args.not_found, "w")
+ns_fd = open(args.not_significant, "w")
+for query in hmm_dict:
     #print hmm_dict[key]
-    try:
-        if hmm_dict[family][0].is_included:
-            out_fd.write("%s\t%s\t%s\t%s\n" % (family, hmm_dict[family][0].id, hmm_dict[family][0].evalue,
-                                               hmm_dict[family][0].bitscore))
-    except:
-        print hmm_dict[family]    
+    if hmm_dict[query]:
+        if hmm_dict[query][0].is_included:
+            out_fd.write("%s\t%s\t%s\t%s\n" % (query, hmm_dict[query][0].id, hmm_dict[query][0].evalue,
+                                               hmm_dict[query][0].bitscore))
+        else:
+            ns_fd.write("%s\t%s\t%s\t%s\n" % (query, hmm_dict[query][0].id, hmm_dict[query][0].evalue,
+                                               hmm_dict[query][0].bitscore))
+    else:
+        out_fd.write("%s\n" % query)
 
 if args.output != "stdout":
     out_fd.close()
 
 os.remove("temp.idx")
+
+nf_fd.close()
+ns_fd.close
