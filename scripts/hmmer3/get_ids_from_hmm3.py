3f08e747ce284d04faaa8d3a8cc5488d0e30c666
Sergei Kliver <mahajrod@gmail.com>
2015-08-06 06:58:30 +0200

    additions in scripts

---
+++ scripts/hmmer3/get_ids_from_hmm3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+
+from Tools.HMMER import HMMER3
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_hmm", action="store", dest="input",
+                    help="Input hmm3 file")
+parser.add_argument("-o", "--output_file", action="store", dest="output", default="stdout",
+                    help="Output file with ids. Default: stdout")
+
+args = parser.parse_args()
+
+HMMER3.get_ids_from_hmm3(args.input, ids_file=None if args.output == 'stdout' else args.output, return_ids_list=False)
