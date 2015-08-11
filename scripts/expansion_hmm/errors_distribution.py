0b7f175a047d9d25a023677204dcff19a4234b1c
Sergei Kliver <mahajrod@gmail.com>
2015-08-12 01:17:33 +0200

    additions in scripts

---
+++ scripts/expansion_hmm/errors_distribution.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import sys
+import argparse
+
+from collections import OrderedDict
+from CustomCollections.GeneralCollections import TwoLvlDict, IdList, IdSet, SynDict
+
+from Routines.File import check_path
+from Routines.File import read_synonyms_dict
+
+
+def is_assembled(families):
+    fam_set = list(set(families))
+    if len(fam_set) > 1 or "_" in fam_set[0]:
+        return False
+    return True
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="File with families assembly information")
+parser.add_argument("-e", "--header", action="store_true", dest="header",
+                    help="Header is present in input file")
+parser.add_argument("-o", "--output_file", action="store", dest="output", default="stdout",
+                    help="Output file. Default: stdout")
+
+args = parser.parse_args()
+
+out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
+
+species_syn_dict = TwoLvlDict()
+out_fd.write("#family\tspecies_with_family\tspecies_with_errors\tspecies_with_correct_fam\n")
+with open(args.input, "r") as in_fd:
+    if args.header:
+        in_fd.readline()
+    for line in in_fd:
+        species_with_errors = 0
+        species_with_fam = 0
+        tmp = line.strip().split("\t")
+        family_name = tmp[0]
+        for fam in tmp[1:]:
+            if fam != ".":
+                species_with_fam += 1
+            if "_" in fam:
+                species_with_errors += 1
+        species_with_correct_fam = species_with_fam - species_with_errors
+        out_fd.write("%s\t%i\t%i\t%i\n" % (family_name, species_with_fam, species_with_errors, species_with_correct_fam))
+
+if args.output != "stdout":
+    out_fd.close()
