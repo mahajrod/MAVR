558162618f40b36275de51fdd1c193d52c806a6d
Sergei Kliver <mahajrod@gmail.com>
2015-08-11 23:42:51 +0200

    additions in scripts

--- examples/treefam_check/compare_families.bash
+++ examples/treefam_check/compare_families.bash
@@ -9,15 +9,13 @@ REFERENCE_FAM_DIR="/home/skliver/data/TreeFam_new/fam/"
 COMPARE_SCRIPT=${SCRIPTS_DIR}expansion/compare_clusters.py
 FIND_CORRECT_FAMS_SCRIPT=${SCRIPTS_DIR}expansion_hmm/find_correctly_assembled_families_in_all_species.py
 
-
-
 SPECIES=(rattus_norvegicus macaca_mulatta echinops_telfairi ornithorhynchus_anatinus monodelphis_domestica tupaia_belangeri erinaceus_europaeus sorex_araneus microcebus_murinus pongo_abelii equus_caballus felis_catus ochotona_princeps cavia_porcellus choloepus_hoffmanni procavia_capensis tursiops_truncatus tarsius_syrichta dipodomys_ordii vicugna_pacos pteropus_vampyrus dasypus_novemcinctus homo_sapiens macropus_eugenii loxodonta_africana oryctolagus_cuniculus ailuropoda_melanoleuca nomascus_leucogenys callithrix_jacchus myotis_lucifugus sarcophilus_harrisii bos_taurus gorilla_gorilla otolemur_garnettii pan_troglodytes ictidomys_tridecemlineatus sus_scrofa mus_musculus canis_familiaris mustela_putorius_furo)
 
 cd ${WORK_DIR}
 
 for species in ${SPECIES[@]};
     do
-    ${COMPARE_SCRIPT} -r ${REFERENCE_FAM_DIR}${SPECIES}.fam -c ${SPECIES_FAM_DIR}${SPECIES} -n -o compare_dir/
+    ${COMPARE_SCRIPT} -r ${REFERENCE_FAM_DIR}${SPECIES}.fam -c ${SPECIES_FAM_DIR}${SPECIES}.fam -n -o compare_dir/
     done
 
 SPECIES_STR=`echo ${SPECIES[@]} | tr " " ","`
