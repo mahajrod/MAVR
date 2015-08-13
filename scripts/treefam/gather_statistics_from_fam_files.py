bd55907e86381cfd69ee54e72f029dadbf401273
mahajrod <mahajrod@gmail.com>
2015-08-13 15:11:48 +0300

    additions

--- scripts/treefam/gather_statistics_from_fam_files.py
+++ scripts/treefam/gather_statistics_from_fam_files.py
@@ -7,16 +7,17 @@ import argparse
 
 from collections import OrderedDict
 
-from Routines.File import check_path
+from Routines.File import check_path, split_filename
 from CustomCollections.GeneralCollections import SynDict, TwoLvlDict, IdList
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--input_dir", action="store", dest="input", required=True, type=check_path,
+parser.add_argument("-i", "--input_dir", action="store", dest="input", default="./", type=check_path,
                     help="Input directory with fam files")
 parser.add_argument("-s", "--species_set", action="store", dest="species_set", type=lambda s: s.split(","),
-                    required=True,
                     help="Comma separated set of species.")
+parser.add_argument("-b", "--use_basename", action="store_true", dest="use_basename",
+                    help="Use basenames of files in input directory as species")
 parser.add_argument("-u", "--suffix", action="store", dest="suffix", required=True, default=".fam",
                     help="Suffix of fam files. Default: .fam")
 parser.add_argument("-o", "--output", action="store", dest="output", default="stdout",
@@ -25,12 +26,21 @@ parser.add_argument("-o", "--output", action="store", dest="output", default="st
 args = parser.parse_args()
 
 out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
-species_list = sorted(args.species_set)
+species_list = []
+suffix_list = []
+if args.use_basename:
+    for filename in sorted(os.listdir(args.input)):
+        dir, basename, ext = split_filename(filename)
+        species_list.append(basename)
+        suffix_list.append(".%s" % ext)
+else:
+    species_list = sorted(args.species_set)
+    suffix_list = [args.suffix for i in range(0, len(species_list))]
 
 out_fd.write("#species\tnumber_of_families\tnumber_of_proteins\n")
-for species in species_list:
+for species, suffix in zip(species_list, suffix_list):
     fam_dict = SynDict()
-    fam_dict.read("%s%s" % (species, args.suffix), separator="\t", split_values=False, values_separator=",",
+    fam_dict.read("%s%s" % (species, suffix), separator="\t", split_values=False, values_separator=",",
                   key_index=0, value_index=1)
     out_fd.write("%s\t%i\t%i\n" % (species, len(fam_dict), fam_dict.count_all_synonyms()))
 
