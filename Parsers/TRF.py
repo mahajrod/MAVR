fd448beccb0bbaf11d4bad526cc4425ee68161ca
Sergei Kliver <mahajrod@gmail.com>
2015-09-19 09:00:32 +0200

    additions

--- Parsers/TRF.py
+++ Parsers/TRF.py
@@ -52,12 +52,12 @@ class CollectionTRF():
                     #line = fd.readline()
                     while tmp[0:8] != "Sequence":
                         tmp = next(fd)
-                        print tmp
+                        #print tmp
                     chrom = tmp.strip().split()[1]
                     while tmp[0:10] != "Parameters":
                         tmp = next(fd)
-                        print tmp
-                    print tmp
+                        #print tmp
+                    #print tmp
                     self.parameters = list(map(lambda x: int(x), tmp.strip().split()[1:]))
                     tmp = next(fd)
                     while tmp == "\n":
