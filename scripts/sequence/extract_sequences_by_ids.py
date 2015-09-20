83049157c0ad18489150de625735d0ea54fd3426
Sergei Kliver <mahajrod@gmail.com>
2015-09-20 09:00:18 +0200

    additions

--- scripts/sequence/extract_sequences_by_ids.py
+++ scripts/sequence/extract_sequences_by_ids.py
@@ -13,7 +13,7 @@ from Routines.Sequence import record_by_id_generator
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input_file_list", action="store", dest="input", required=True,
-                    type=lambda s: s.split(","),
+                    type=lambda s: make_list_of_path_to_files(s.split(",")),
                     help="Comma-separated list of input files/directories with sequences")
 parser.add_argument("-o", "--output_file", action="store", dest="output", default="stdout",
                     help="Output file with sequences")
@@ -25,13 +25,13 @@ parser.add_argument("-d", "--id_file", action="store", dest="id_file",
 args = parser.parse_args()
 
 out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
-args.input = make_list_of_path_to_files(args.input)
+
 tmp_index_file = "temp.idx"
 
 id_list = read_ids(args.id_file)
 
-print("Parsing %s..." % args.input_file)
-sequence_dict = SeqIO.index_db(tmp_index_file, args.input_file, format=args.format)
+print("Parsing %s..." % args.input)
+sequence_dict = SeqIO.index_db(tmp_index_file, args.input, format=args.format)
 SeqIO.write(record_by_id_generator(sequence_dict, id_list), out_fd, format=args.format)
 os.remove(tmp_index_file)
 
