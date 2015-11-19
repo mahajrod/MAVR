bf4847a6327da33374546a2bb2b2fff91e71bfc1
mahajrod <mahajrod@gmail.com>
2015-11-19 18:36:19 +0300

    added paralleled augustus

--- projects/lizard_genomes/download_anolis_transcriptome_reads.sh
+++ projects/lizard_genomes/download_anolis_transcriptome_reads.sh
@@ -5,4 +5,5 @@
 mkdir -p /mnt/guatemala/skliver/data/Anolis_carolinensis /mnt/guatemala/skliver/data/Anolis_carolinensis/transcriptomes
 cd /mnt/guatemala/skliver/data/Anolis_carolinensis/transcriptomes
 
+
 ~/Soft/MAVR/scripts/download/download_from_sra_by_ids.py -i SRR579556,SRR579558,SRR579557,SRR579559
