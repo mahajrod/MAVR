aa312ff64cae73efde1d87c47aefc7b26dcb952d
Sergei Kliver <mahajrod@gmail.com>
2015-12-01 19:15:29 +0100

    added paralleled augustus

---
+++ scripts/treefam/label_fam_file_with_species_name.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+FAM_DIR=$1
+LABELED_FAM_DIR=$2
+
+#SPECIES_NAME_FILE="species_names_handled.t"
+mkdir -p ${LABELED_FAM_DIR}
+
+for SPECIES_FAM_FILE in `ls ${FAM_DIR}`;
+	do
+	SPECIES=`basename ${SPECIES_FAM_FILE} .fam`
+	echo "Labeling ${SPECIES}"
+	FAM_FILE=${FAM_DIR}${SPECIES}".fam"
+	LABELED_FAM_FILE=${LABELED_FAM_DIR}${SPECIES}".fam"
+	sed "s/\t/\t${SPECIES}\./;s/,/,${SPECIES}\./g" ${FAM_FILE} > ${LABELED_FAM_FILE}
+	done
+
+
+
+
