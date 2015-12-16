8e1b569947edc37c4b195e71d6a728469223e166
mahajrod <mahajrod@gmail.com>
2015-12-16 11:55:43 +0300

    aaaa

--- scripts/annotation/ncbi/analyze_ncbi_pep.py
+++ scripts/annotation/ncbi/analyze_ncbi_pep.py
@@ -23,7 +23,7 @@ pep_uniq_description_file = "%s.pep.description.uniq" % args.output_prefix
 pep_uniq_description_no_isoform_versions = "%s.pep.sorted.description.no_isoform_versions" % args.output_prefix
 pep_description_collapsed_isoforms = "%s.pep.collapsed_isoforms.description" % args.output_prefix
 
-get_pep_decription_str = "grep -P '^>' %s | sed 's/^>//;s/\[%ss\]//;s/ /\\t/' | sort -t $'\\t' -k 2 -k 1 > %s" % (args.input,
+get_pep_decription_str = "grep -P '^>' %s | sed 's/^>//;s/\[%s\]//;s/ /\\t/' | sort -t $'\\t' -k 2 -k 1 > %s" % (args.input,
                                                                                                                   args.species_name,
                                                                                                                   pep_description_file)
 get_uniq_descriptions_str = "uniq -f 1 %s > %s" % (pep_description_file, pep_uniq_description_file)
@@ -31,6 +31,7 @@ remove_isoform_versions_str = "sed s/isoform.*// %s > %s" % (pep_uniq_descriptio
                                                              pep_uniq_description_no_isoform_versions)
 
 for exe_string in get_pep_decription_str, get_uniq_descriptions_str, remove_isoform_versions_str:
+    print(exe_string)
     os.system(exe_string)
 
 syn_dict = SynDict()
