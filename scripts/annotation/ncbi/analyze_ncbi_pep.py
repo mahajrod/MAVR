21e74aa984d797f62c5c8e530477d0dba4a6f840
mahajrod <mahajrod@gmail.com>
2015-12-16 12:26:19 +0300

    aaaa

--- scripts/annotation/ncbi/analyze_ncbi_pep.py
+++ scripts/annotation/ncbi/analyze_ncbi_pep.py
@@ -12,7 +12,8 @@ parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix
                     help="Prefix of output files. By default - species name, with spaces replaced by underline")
 parser.add_argument("-s", "--species_name", action="store", dest="species_name", required=True,
                     help="Species name as listed in pep file. Example: 'Felis catus'")
-
+parser.add_argument("-r", "--remove_predicted", action="store_true", dest="remove_predicted",
+                    help="Remove predicted proteins")
 args = parser.parse_args()
 
 if not args.output_prefix:
@@ -23,12 +24,18 @@ pep_uniq_description_file = "%s.pep.description.uniq" % args.output_prefix
 pep_uniq_description_no_isoform_versions = "%s.pep.sorted.description.no_isoform_versions" % args.output_prefix
 pep_description_collapsed_isoforms = "%s.pep.collapsed_isoforms.description" % args.output_prefix
 
-get_pep_decription_str = "grep -P '^>' %s | sed 's/^>//;s/\[%s\]//;s/ /\\t/' | sort -t '\t' -k 2 -k 1 > %s" % (args.input,
-                                                                                                                  args.species_name,
-                                                                                                                  pep_description_file)
+if args.remove_predicted:
+    get_pep_decription_str = "grep -P '^>' %s | grep -v 'PREDICTED' | sed 's/^>//;s/\[%s\]//;s/ /\\t/' | sort -t '\t' -k 2 -k 1 > %s" % (args.input,
+                                                                                                                                         args.species_name,
+                                                                                                                                         pep_description_file)
+else
+    get_pep_decription_str = "grep -P '^>' %s | sed 's/^>//;s/\[%s\]//;s/ /\\t/' | sort -t '\t' -k 2 -k 1 > %s" % (args.input,
+                                                                                                                   args.species_name,
+                                                                                                                   pep_description_file)
 get_uniq_descriptions_str = "uniq -f 1 %s > %s" % (pep_description_file, pep_uniq_description_file)
 remove_isoform_versions_str = "sed s/isoform.*// %s > %s" % (pep_uniq_description_file,
                                                              pep_uniq_description_no_isoform_versions)
+
 
 for exe_string in get_pep_decription_str, get_uniq_descriptions_str, remove_isoform_versions_str:
     print(exe_string)
