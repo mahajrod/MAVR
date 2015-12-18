62be8a6f67a063fa80027691026e4131fde740de
mahajrod <mahajrod@gmail.com>
2015-12-18 15:01:59 +0300

    added paralleled exonerate

--- scripts/annotation/get_transcript_len_from_gff.py
+++ scripts/annotation/get_transcript_len_from_gff.py
@@ -27,10 +27,11 @@ out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
 
 for record in GFF.parse(open(args.input_gff)):
     for feature in record.features:
-        print feature
+        #print feature
         if feature.type == "gene":
             for subfeature in feature.sub_features:
                 #print subfeature
+                print(subfeature.type)
                 if subfeature.type == "mRNA" or subfeature.type == "transcript":
                     out_fd.write("%s\t%s\t%i\n" % (feature.id, subfeature.id, len(subfeature)))
 
