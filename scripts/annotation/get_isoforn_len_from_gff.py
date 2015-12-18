b231a39a23d5ad8fc8c7a33490168b866e66381a
mahajrod <mahajrod@gmail.com>
2015-12-18 16:20:11 +0300

    added paralleled exonerate

---
+++ scripts/annotation/get_isoforn_len_from_gff.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import argparse
+import os
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+from BCBio import GFF
+
+
+from Routines.Sequence import record_generator
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_gff", action="store", dest="input_gff",
+                    help="Gff file with annotations to extract")
+parser.add_argument("-o", "--output_file", action="store", dest="output", default="stdout",
+                    help="Output file with information about transcripts")
+
+args = parser.parse_args()
+
+out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
+
+for record in GFF.parse(open(args.input_gff)):
+    for feature in record.features:
+        #print feature
+        if feature.type == "gene":
+            transcript_id_list = []
+            transcript_len_list = []
+            CDS_len_list = []
+            for subfeature in feature.sub_features:
+                #print subfeature
+                #print(subfeature.type)
+                if subfeature.type == "mRNA" or subfeature.type == "transcript":
+                    transcript_id_list.append(subfeature.id)
+                    transcript_len_list.append(len(subfeature))
+                    CDS_len = 0
+                    for subsubfeature in subfeature.sub_features:
+                        if subsubfeature.type == "CDS":
+                            CDS_len += len(subsubfeature)
+                    CDS_len_list.append(CDS_len)
+
+
+            out_fd.write("%s\t%s\t%s\t%s\t%s\n" % (feature.id, ",".join(transcript_id_list),
+                                                   ",".join(map(str, transcript_len_list)),
+                                                   ",".join(map(str, CDS_len_list)),
+                                                   ",".join(map(str, map(lambda x: x/3, CDS_len_list)))))
+
+out_fd.close()
+
+
+
