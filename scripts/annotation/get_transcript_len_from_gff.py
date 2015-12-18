f2b3dd60859c29803329002e445b2a55ff90919a
mahajrod <mahajrod@gmail.com>
2015-12-18 14:23:26 +0300

    added paralleled exonerate

---
+++ scripts/annotation/get_transcript_len_from_gff.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import argparse
+import os
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+from BCBio import GFF
+
+
+from Routines.Sequence import record_generator
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_gff", action="store", dest="input_gff",
+                    help="Gff file with annotations to extract")
+parser.add_argument("-o", "--output_file", action="store", dest="output", default="stdout",
+                    help="Output file with transcript lengths")
+
+args = parser.parse_args()
+
+out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
+
+for record in GFF.parse(open(args.input_gff)):
+    new_record = record
+    record.features = []
+    for feature in record.features:
+        if feature.type == "gene":
+            for subfeature in feature.sub_features:
+                if subfeature.type == "mRNA" or subfeature.type == "transcript":
+                    out_fd.write("%s\t%s\t%i\n" % (feature.id, subfeature.id, len(subfeature)))
+
+out_fd.close()
+
+
+
