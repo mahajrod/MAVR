6666fa93994271a59ee2a2319e1e9cf8e906b20f
Sergei Kliver <mahajrod@gmail.com>
2015-12-27 09:43:58 +0100

    added paralleled exonerate

--- Tools/Abstract.py
+++ Tools/Abstract.py
@@ -26,6 +26,7 @@ class Tool():
         self.path = check_path(path)
         self.cmd = cmd
         self.threads = max_threads
+        print(jar_path)
         self.jar_path = check_path(jar_path) if jar_path else None
         self.jar = jar
         self.max_memory = max_memory
