f50c81cefc9050d0ad013123c271d9eb0bf5cf19
Sergei Kliver <mahajrod@gmail.com>
2016-02-02 22:28:08 +0100

    augustus tools

---
+++ scripts/treefam/extract_selected_families_from_fam_file.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import argparse
+
+from CustomCollections.GeneralCollections import SynDict, IdSet
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input fam file")
+parser.add_argument("-d", "--id_file", action="store", dest="id_file", required=True,
+                    help="File with ids of families to extract")
+parser.add_argument("-o", "--output", action="store", dest="output", default="stdout",
+                    help="File to write extracted families. Default - stdout")
+parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
+                    help="Print not found ids. Default - no")
+
+args = parser.parse_args()
+
+out_file = sys.stdout if args.output == "stdout" else open(args.output, "w")
+
+fam_dict = SynDict()
+fam_dict.read(args.input)
+
+id_set = IdSet()
+id_set.read(args.id_file)
+
+extracted_dict = SynDict()
+for id_entry in id_set:
+    if id_entry in fam_dict:
+        extracted_dict[id_entry] = fam_dict[id_entry]
+    else:
+        if args.verbose:
+            print("%s was not found" % id_entry)
+
+
+
+
