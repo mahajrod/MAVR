38a53fa8cd7758c5e4066090115a31ee00f31715
Sergei Kliver <mahajrod@gmail.com>
2016-02-04 23:23:36 +0100

    augustus tools

--- scripts/annotation/prepare_hints_from_exonerate_target_output.py
+++ scripts/annotation/prepare_hints_from_exonerate_target_output.py
@@ -16,7 +16,7 @@ parser.add_argument("-d", "--white_id_file", action="store", dest="white_id_file
                     help="File with ids from white list. If set other ids are ignored")
 
 parser.add_argument("-m", "--max_hits_per_query", action="store", dest="max_hits_per_query", type=int, default=3,
-                    help="Maximum hits per query")
+                    help="Maximum hits per query. Default - 3")
 parser.add_argument("-e", "--augustus_script_dir", action="store", dest="augustus_script_dir", default="",
                     help="Directory with augustus scripts(not set by default)")
 
