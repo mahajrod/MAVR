b71b371d691b6d3f9fd281f67418bc3caefba0f9
mahajrod <mahajrod@gmail.com>
2016-02-10 18:29:25 +0300

    augustus tools

---
+++ Tools/Expression/Cufflinks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+
+from Tools.Abstract import Tool
+
+
+class Gffread(Tool):
+    def __init__(self, path="", max_threads=4):
+        Tool.__init__(self, "gffread", path=path, max_threads=max_threads)
+
+    @staticmethod
+    def parse_common_options(input_gff_file, genomic_fasta_file=None, output_cds_file=None,
+                             output_transcripts_file=None):
+
+        options = " %s" % input_gff_file
+        options += " -g %s" if genomic_fasta_file else ""
+        options += " -x %s" if output_cds_file else ""
+        options += " -w %s" if output_transcripts_file else ""
+
+        return options
+
+    def extract_transcript_sequences(self, input_gff_file, genomic_fasta_file, output_prefix):
+
+        output_cds_file = "%s.cds" % output_prefix
+        output_transcripts_file = "%s.transcript" % output_prefix
+        options = self.parse_common_options(input_gff_file, genomic_fasta_file=genomic_fasta_file,
+                                            output_cds_file=output_cds_file,
+                                            output_transcripts_file=output_transcripts_file)
+
+        self.execute(options=options)
+
+if __name__ == "__main__":
+    pass
---
+++ Tools/Expression/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'mahajrod'
+
+from Tools.Expression.Cufflinks import Gffread
+
+Gffread = Gffread()
