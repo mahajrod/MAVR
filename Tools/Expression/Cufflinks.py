d885f9a2b2587dc0a36422d7763e8dbac9da45ac
Sergei Kliver <mahajrod@gmail.com>
2016-05-08 16:02:31 +0200

    sequence clusters

--- Tools/Expression/Cufflinks.py
+++ Tools/Expression/Cufflinks.py
@@ -10,23 +10,44 @@ class Gffread(Tool):
 
     @staticmethod
     def parse_common_options(input_gff_file, genomic_fasta_file=None, output_cds_file=None,
-                             output_transcripts_file=None):
+                             output_transcripts_file=None, coding_only=False):
 
         options = " %s" % input_gff_file
+        options += " -C" if coding_only else ""
         options += " -g %s" % genomic_fasta_file if genomic_fasta_file else ""
         options += " -x %s" % output_cds_file if output_cds_file else ""
         options += " -w %s" % output_transcripts_file if output_transcripts_file else ""
 
         return options
 
-    def extract_transcript_sequences(self, input_gff_file, genomic_fasta_file, output_prefix):
+    def extract_transcript_sequences(self, input_gff_file, genomic_fasta_file, output_prefix, coding_only=False):
 
         output_cds_file = "%s.cds" % output_prefix
         output_transcripts_file = "%s.transcript" % output_prefix
         options = self.parse_common_options(input_gff_file,
+                                            coding_only=coding_only,
                                             genomic_fasta_file=genomic_fasta_file,
                                             output_cds_file=output_cds_file,
                                             output_transcripts_file=output_transcripts_file)
+        #print options
+        self.execute(options=options)
+
+    def extract_cds(self, input_gff_file, genomic_fasta_file, output_file):
+
+        options = self.parse_common_options(input_gff_file,
+                                            genomic_fasta_file=genomic_fasta_file,
+                                            output_cds_file=output_file,
+                                            coding_only=True,
+                                            output_transcripts_file=None)
+        #print options
+        self.execute(options=options)
+
+    def extract_mRNA(self, input_gff_file, genomic_fasta_file, output_file, coding_only=False):
+
+        options = self.parse_common_options(input_gff_file,
+                                            coding_only=coding_only,
+                                            genomic_fasta_file=genomic_fasta_file,
+                                            output_transcripts_file=output_file)
         #print options
         self.execute(options=options)
 
---
+++ scripts/annotation/extract_cds_by_gff.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+
+from Tools.Expression import Cufflinks
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input gff with annotation")
+parser.add_argument("-g", "--genome", action="store", dest="genome", required=True,
+                    help="Fasta with genome")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file to write cds")
+
+args = parser.parse_args()
+
+Cufflinks.extract_cds(args.input, args.genome, args.output)
