776f60e7a15164d8efbfb20d007d6df8410902cf
Sergei Kliver <mahajrod@gmail.com>
2016-05-11 01:34:33 +0200

    sequence clusters

--- Tools/Samtools/Samtools.py
+++ Tools/Samtools/Samtools.py
@@ -12,10 +12,20 @@ class SamtoolsV1(Tool):
         Tool.__init__(self, "samtools", path=path, max_threads=max_threads)
 
         # bam/sam flag values:
-        self.bam_flag_values = {"unaligned": 4,
-                                "supplementary_alignment": 2048,
-                                "non_primary_alignment": 256
-                                }
+        self.bam_flags = {
+                          "read_paired": 1,
+                          "read_mapped_in_proper_pair": 2,
+                          "read_unmapped": 4,
+                          "mate_unmapped": 8,
+                          "read_reverse_strand": 16,
+                          "mate_reverse_strand": 32,
+                          "first_in_pair": 64,
+                          "second_in_pair": 128,
+                          "not_primary_alignment": 256,
+                          "read_fails_platform/vendor_quality_checks": 512,
+                          "read_is_PCR_or_optical_duplicate": 1024,
+                          "supplementary_alignment": 2048
+                          }
 
     def rmdup(self, input_bam, output_bam, remove_dup_for_se_reads=False, treat_both_pe_and_se_reads=False):
 
@@ -26,30 +36,63 @@ class SamtoolsV1(Tool):
 
         self.execute(options, cmd="samtools rmdup")
 
-    def view(self, input_file, output_file, include_header_in_output=True, output_uncompressed_bam=False,
-             output_bam=False, white_flag_value=None, black_flag_value=None, bed_file_with_regions_to_output=None):
-
+    def parse_view_options(self, include_header_in_output=True, output_uncompressed_bam=False,
+                           output_bam=False, white_flag_value=None, black_flag_value=None,
+                           bed_file_with_regions_to_output=None):
         options = " -@ %i" % self.threads
         options += " -h" if include_header_in_output else ""
         options += " -u" if output_uncompressed_bam else ""
         options += " -b" if output_bam else ""
-        options += " -o %s" % output_file
         options += " -f %i" % white_flag_value if white_flag_value else ""
         options += " -F %i" % black_flag_value if black_flag_value else ""
         options += " -L %s" % bed_file_with_regions_to_output if bed_file_with_regions_to_output else ""
+        return options
+
+    def view(self, input_file, output_file, include_header_in_output=True, output_uncompressed_bam=False,
+             output_bam=False, white_flag_value=None, black_flag_value=None, bed_file_with_regions_to_output=None):
+
+        options = self.parse_view_options(include_header_in_output=include_header_in_output,
+                                          output_uncompressed_bam=output_uncompressed_bam,
+                                          output_bam=output_bam, white_flag_value=white_flag_value,
+                                          black_flag_value=black_flag_value,
+                                          bed_file_with_regions_to_output=bed_file_with_regions_to_output)
+
+        options += " -o %s" % output_file
         options += " %s" % input_file
 
         self.execute(options, "samtools view")
 
-    def sort(self, input_bam, output_bam, temp_file_prefix="temp_bam", sort_by_name=False, max_memory_per_thread="1G"):
-
+    def parse_sort_options(self, temp_file_prefix="temp_bam", sort_by_name=False, max_memory_per_thread="1G"):
         options = " -@ %i" % self.threads
         options += " -n" if sort_by_name else ""
         options += " -m %s" % max_memory_per_thread if max_memory_per_thread else ""
-        options += " -o %s" % output_bam
         options += " -T %s" % temp_file_prefix
+        return options
+
+    def sort(self, input_bam, output_bam, temp_file_prefix="temp_bam", sort_by_name=False, max_memory_per_thread="1G"):
+        options = self.parse_sort_options(temp_file_prefix=temp_file_prefix, sort_by_name=sort_by_name,
+                                          max_memory_per_thread=max_memory_per_thread)
+        options += " -o %s" % output_bam
         options += " %s" % input_bam
+
         self.execute(options, "samtools sort")
+
+    def get_info_from_flags(self, flag_list):
+        info_field = 0
+        for flag in flag_list:
+            info_field |= self.bam_flags[flag]
+
+        return info_field
+
+    def prepare_bam_for_read_extraction(self, input_bam, output_bam, temp_file_prefix="temp_bam",
+                                        max_memory_per_thread="1G"):
+        black_list_flags = ["not_primary_alignment", "supplementary_alignment"]
+        info_field = self.get_info_from_flags(black_list_flags)
+
+        view_options = " -b -u -F %i %s" % (info_field, input_bam)
+        sort_options = " -n -m %s -T %s -o %s -" % (max_memory_per_thread, temp_file_prefix, output_bam)
+        cmd = "samtools view %s | samtools sort %s" % (view_options, sort_options)
+        self.execute(options="", cmd=cmd)
 
     def faidx(self, fasta_file):
 
--- scripts/alignment/extract_fastq_from_bam.py
+++ scripts/alignment/extract_fastq_from_bam.py
@@ -14,14 +14,15 @@ parser.add_argument("-i", "--input", action="store", dest="input", required=True
                     help="Input bam file")
 parser.add_argument("-t", "--threads", action="store", dest="threads", type=int, default=1,
                     help="Number of threads to use. Default - 1")
-parser.add_argument("-n", "--sort_by_name", action="store_true", dest="sort_by_name",
-                    help="Input bam is not sorted by reads name")
-parser.add_argument("-e", "--sorted_bam", action="store", dest="sorted_bam",
-                    help="File to write sorted bam file. Required if -n/--sort_by_name option is set")
+parser.add_argument("-p", "--prepare_bam", action="store_true", dest="prepare_bam",
+                    help="Prepare bam for reads extraction(filter out supplementary and not primary alignments"
+                         "and sort by name)")
+parser.add_argument("-e", "--prepared_bam", action="store", dest="prepared_bam",
+                    help="File to write sorted bam file. Required if -p/--prepare_bam option is set")
 parser.add_argument("-d", "--temp_dir", action="store", dest="temp_dir",
-                    help="Directory to use for temporary files. Required if -n/--sort_by_name option is set")
+                    help="Directory to use for temporary files. Required if -p/--prepare_bam option is set")
 parser.add_argument("-o", "--out_prefix", action="store", dest="out_prefix", required=True,
-                    help="Prefix of output fatsq files")
+                    help="Prefix of output fastq files")
 parser.add_argument("-s", "--single_ends", action="store_false", dest="paired", default=True,
                     help="Reads are SE")
 parser.add_argument("-m", "--max_memory_per_thread", action="store", dest="max_memory_per_thread", default="1G",
@@ -29,14 +30,14 @@ parser.add_argument("-m", "--max_memory_per_thread", action="store", dest="max_m
 args = parser.parse_args()
 
 if args.sort_by_name and ((not args.sorted_bam) or (not args.temp_dir)):
-    raise ValueError("Options -p/--sorted_bam and -m/--temp_dir must be set if -n/--sort_by_name option is used")
+    raise ValueError("Options -e/--prepared_bam and -m/--temp_dir must be set if -p/--prepare_bam option is used")
 
 SamtoolsV1.threads = args.threads
 
-if args.sort_by_name:
+if args.prepare_bam:
     FileRoutines.save_mkdir(FileRoutines.check_path(args.temp_dir))
-    SamtoolsV1.sort(args.input, args.sorted_bam, temp_file_prefix=args.temp_dir, sort_by_name=args.sort_by_name,
-                    max_memory_per_thread=args.max_memory_per_thread)
+    SamtoolsV1.prepare_bam_for_read_extraction(args.input, args.prepared_bam, temp_file_prefix=args.temp_dir,
+                                               max_memory_per_thread=args.max_memory_per_thread)
 
 if args.paired:
     left_fastq = "%s_1.fastq" % args.out_prefix
@@ -45,6 +46,6 @@ else:
     left_fastq = "%s.fastq" % args.out_prefix
     right_fastq = None
 
-BamToFastq.convert(args.sorted_bam if args.sort_by_name else args.input, left_fastq, out_right_fastq=right_fastq)
+BamToFastq.convert(args.prepared_bam if args.prepare_bam else args.input, left_fastq, out_right_fastq=right_fastq)
 
 
