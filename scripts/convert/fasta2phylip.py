78bfa35febda13844357e481588d7cbae06693cd
mahajrod <mahajrod@gmail.com>
2016-06-09 19:39:23 +0300

    sequence clusters

---
+++ scripts/convert/fasta2phylip.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+
+import argparse
+from Converters import SequenceConverters
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input fasta file")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output phylip file")
+
+args = parser.parse_args()
+
+SequenceConverters.fasta2phylip(args.input, args.output, mode="name_in_same")
+
