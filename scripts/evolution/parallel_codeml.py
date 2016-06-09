90650d2f4c016f5e1c2bdd28503bce8fff6f465a
mahajrod <mahajrod@gmail.com>
2016-06-09 18:35:00 +0300

    sequence clusters

--- scripts/evolution/parallel_codeml.py
+++ scripts/evolution/parallel_codeml.py
@@ -40,7 +40,7 @@ Codeml.threads = args.threads
 Codeml.path = args.path
 Codeml.parallel_codeml(args.input_dir, args.tree, args.output_dir, seq_type=args.seq_type,
                        codon_frequency=args.codon_frequency, noisy=3, verbose="concise", runmode=0, clock=0,
-                       aminoacid_distance=1, model=args.model, nssites=0,
+                       aminoacid_distance=None, model=args.model, nssites=0,
                        genetic_code=args.genetic_code, fix_kappa=False, kappa=5, fix_omega=False, omega=0.2, getSE=0,
                        RateAncestor=0, small_difference=args.small_difference, clean_data=args.clean_data, method=0)
 
