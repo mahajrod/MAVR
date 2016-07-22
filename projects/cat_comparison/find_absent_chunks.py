aca78d8a56c8dede0b6843bcd6611c2cde9c1039
mahajrod <mahajrod@gmail.com>
2016-07-22 16:39:55 +0300

    changes

--- projects/cat_comparison/find_absent_chunks.py
+++ projects/cat_comparison/find_absent_chunks.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input_dir", action="store", dest="input_file", required=True,
                     help="Input dir with exonerate output named in style *_<chunk_number>.out")
-parser.add_argument("-t", "--total_number_of_chunks", action="store", dest="number_of_chunks", type=int,
+parser.add_argument("-n", "--total_number_of_chunks", action="store", dest="number_of_chunks", type=int,
                     required=True,
                     help="Total number of chunks")
 
