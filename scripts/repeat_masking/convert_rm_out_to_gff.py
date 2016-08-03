2e4ce9f1f99095e2b488fd29c855e4687608a530
mahajrod <mahajrod@gmail.com>
2016-08-03 16:42:27 +0300

    changes

--- scripts/repeat_masking/convert_rm_out_to_gff.py
+++ scripts/repeat_masking/convert_rm_out_to_gff.py
@@ -9,13 +9,20 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input_file", action="store", dest="input", required=True,
                     help="Input file with RepeatMasker output")
+parser.add_argument("-p", "--output_prefix", action="store", dest="output_prefix", required=True,
+                    help="Prefix of output files")
+"""
 parser.add_argument("-o", "--output_gff", action="store", dest="output_gff", required=True,
                     help="Output gff file")
 parser.add_argument("-a", "--repeat_classes", action="store", dest="repeat_classes", required=True,
                     help="File to write annotated repeat classes")
 parser.add_argument("-f", "--repeat_families", action="store", dest="repeat_families", required=True,
                     help="File to write annotated repeat families")
-
+"""
 args = parser.parse_args()
 
-RepeatMasker.convert_rm_out_to_gff(args.input, args.output_gff, args.repeat_classes, args.repeat_families)
+output_gff = "%s.gff" % args.output_prefix
+repeat_classes = "%s.repeat_classes" % args.output_prefix
+repeat_families = "%s.repeat_families" % args.output_prefix
+
+RepeatMasker.convert_rm_out_to_gff(args.input, output_gff, repeat_classes, repeat_families)
