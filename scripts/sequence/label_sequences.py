2277e9d8d357d80d6ffa435785ef45823d9017d1
mahajrod <mahajrod@gmail.com>
2016-08-20 20:43:41 +0300

    changes

---
+++ scripts/sequence/label_sequences.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import sys
+import argparse
+
+from Routines import TreeFamRoutines
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input file with sequences")
+parser.add_argument("-l", "--label", action="store", dest="label", required=True,
+                    help="Label to use")
+parser.add_argument("-s", "--separator", action="store", dest="separator", default="@",
+                    help="Separator to use. default - '@'")
+parser.add_argument("-o", "--output", action="store", dest="output", default="stdout",
+                    help="File to write labeled sequences. Default - stdout")
+
+args = parser.parse_args()
+
+out_file = "" if args.output == "stdout" else " > %s" % args.output
+
+sed_string = "sed 's/^>/>%s%s/' %s %s" % (args.label, args.separator, args.input, out_file)
+
+os.system(sed_string)
