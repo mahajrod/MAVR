e2f490855ce3473494066bc7d54937fd97fcb95f
mahajrod <mahajrod@gmail.com>
2016-09-12 20:43:52 +0300

    changes

--- scripts/snpcall/filter_vcf.py
+++ scripts/snpcall/filter_vcf.py
@@ -18,26 +18,27 @@ parser.add_argument("-g", "--gatk_directory", action="store", dest="gatk_dir", d
 parser.add_argument("--snp_filter_name", action="store", dest="snp_filter_name", type=str,
                     default="ambiguous_snp", help="SNP filter name")
 parser.add_argument("--snp_QD", action="store", dest="snp_QD", type=float, default=2.0,
-                    help="SNP QD threshold")
+                    help="SNP QD threshold. Default -  2.0")
 parser.add_argument("--snp_FS", action="store", dest="snp_FS", type=float, default=60.0,
-                    help="SNP FS threshold")
+                    help="SNP FS threshold. Default -   60.0")
 parser.add_argument("--snp_MQ", action="store", dest="snp_MQ", type=float, default=40.0,
-                    help="SNP MQ threshold")
+                    help="SNP MQ threshold. Default -  40.0")
 parser.add_argument("--snp_HaplotypeScore", action="store", dest="snp_HaplotypeScore", type=float, default=13.0,
-                    help="SNP HaplotypeScore threshold")
+                    help="SNP HaplotypeScore threshold. Default -  13.0")
 parser.add_argument("--snp_MappingQualityRankSum", action="store", dest="snp_MappingQualityRankSum", type=float,
-                    default=-12.5, help="SNP MappingQualityRankSum threshold")
+                    default=-12.5, help="SNP MappingQualityRankSum threshold. Default -")
 parser.add_argument("--snp_ReadPosRankSum", action="store", dest="snp_ReadPosRankSum", type=float, default=-8.0,
-                    help="SNP ReadPosRankSum threshold")
+                    help="SNP ReadPosRankSum threshold. Default -   -8.0")
 parser.add_argument("--indel_filter_name", action="store", dest="indel_filter_name", type=str,
                     default="ambiguous_indel", help="Indel filter name")
 parser.add_argument("--indel_QD", action="store", dest="indel_QD", type=float, default=2.0,
-                    help="Indel QD threshold")
+                    help="Indel QD threshold. Default - 2.0")
 parser.add_argument("--indel_ReadPosRankSum", action="store", dest="indel_ReadPosRankSum", type=float, default=-20.0,
-                    help="Indel ReadPosRankSum threshold")
+                    help="Indel ReadPosRankSum threshold. Default -   -20.0")
 parser.add_argument("--indel_InbreedingCoeff", action="store", dest="indel_InbreedingCoeff", type=float, default=-0.8,
-                    help="Indel InbreedingCoeff threshold")
-parser.add_argument("--indel_FS", action="store", dest="indel_FS", type=float, default=200.0, help="Indel FS threshold")
+                    help="Indel InbreedingCoeff threshold. Default -   -0.8")
+parser.add_argument("--indel_FS", action="store", dest="indel_FS", type=float, default=200.0,
+                    help="Indel FS threshold. Default - 200.0")
 
 args = parser.parse_args()
 
