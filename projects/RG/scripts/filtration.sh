8041d664a24f689fe12d7eb2bb96bd124a032529
mahajrod <mahajrod@gmail.com>
2016-10-28 14:12:26 +0300

    changes

--- projects/RG/scripts/filtration.sh
+++ projects/RG/scripts/filtration.sh
@@ -58,8 +58,8 @@ for SAMPLE in ${SAMPLE_LIST[@]};
     echo "Filtering reads by mean quality"
     echo "    ${NUMBER_OF_FILES} files"
 
-    FACUT_STRING="${FACUT_BIN_DIR}/filter_by_mean_quality -t ${QUALITY_THRESHOLD} -q ${PHRED_SCORE_TYPE} -n ${READ_NAME_TYPE}  -f ${FILES[0]} -r ${FILES[1]} -p ${OUTPUT_PREFIX} > ${OUTPUT_STAT}"
-    echo ${FACUT_STRING}
+    FACUT_STRING="${FACUT_BIN_DIR}/filter_by_mean_quality -t ${QUALITY_THRESHOLD} -q ${PHRED_SCORE_TYPE} -n ${READ_NAME_TYPE}  -f ${FILES[0]} -r ${FILES[1]} -p ${OUTPUT_PREFIX}"
+    echo "${FACUT_STRING}  > ${OUTPUT_STAT}"
 
-    ${FACUT_STRING}
+    ${FACUT_STRING}  > ${OUTPUT_STAT}
     done
