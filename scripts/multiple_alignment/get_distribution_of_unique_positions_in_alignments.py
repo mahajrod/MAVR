b0f15351a6ad8a605ba354c3f58ea59c77f6bd06
mahajrod <mahajrod@gmail.com>
2016-11-01 16:10:44 +0300

    changes

--- scripts/multiple_alignment/get_distribution_of_unique_positions_in_alignments.py
+++ scripts/multiple_alignment/get_distribution_of_unique_positions_in_alignments.py
@@ -45,4 +45,4 @@ for species in species_list:
 
 data_list = [data_dict[species] for species in data_dict]
 
-MatplotlibRoutines.percent_histogram(data_list, "tratata")
+MatplotlibRoutines.percent_histogram(data_list, "tratata", input_mode="fraction")
