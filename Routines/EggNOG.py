2097097a4224a62f960050e3c1940751a82dc12e
mahajrod <mahajrod@gmail.com>
2016-12-17 13:29:07 +0300

    changes

--- Routines/EggNOG.py
+++ Routines/EggNOG.py
@@ -41,7 +41,7 @@ class EggNOGRoutines(SequenceClusterRoutines):
 
         print type(FileRoutines)
 
-        input_files = FileRoutines.make_list_of_path_to_files([dir_with_alignments] if isinstance(dir_with_alignments, str) else dir_with_alignments)
+        input_files = make_list_of_path_to_files([dir_with_alignments] if isinstance(dir_with_alignments, str) else dir_with_alignments)
 
         FileRoutines.save_mkdir(out_dir)
         from Routines import MultipleAlignmentRoutines
@@ -52,7 +52,7 @@ class EggNOGRoutines(SequenceClusterRoutines):
 
     @staticmethod
     def split_proteins_per_species(dir_with_proteins, output_dir, input_format="fasta", output_format="fasta"):
-        input_files = make_list_of_path_to_files([dir_with_proteins] if isinstance(dir_with_proteins, str) else dir_with_proteins)
+        input_files = FileRoutines.make_list_of_path_to_files([dir_with_proteins] if isinstance(dir_with_proteins, str) else dir_with_proteins)
 
         out_dir = FileRoutines.check_path(output_dir)
         FileRoutines.save_mkdir(out_dir)
