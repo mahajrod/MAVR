9a87bd32e50bb7c4ae25b1679b6fa4b75ea4ee25
Sergei Kliver <mahajrod@gmail.com>
2016-12-19 22:24:04 +0100

    changes

--- Parsers/Coockiecutter.py
+++ Parsers/Coockiecutter.py
@@ -3,6 +3,7 @@ __author__ = 'mahajrod'
 from collections import OrderedDict
 
 
+# adapter for 2 versions of Coockiecutter
 class CoockiecutterReport():
     def __init__(self, coockiecutter_report_file):
         self.stat = OrderedDict()
@@ -24,7 +25,7 @@ class CoockiecutterReport():
                         self.stat[read_position][tmp[0]] = int(tmp[1])
                     tmp = in_fd.readline()
 
-        match_key = "match" if "match" in self.stat["left"] else "adapter"
-
-        self.input_pairs = self.stat["left"]["ok"] + self.stat["left"][match_key] + self.stat["left"]["n"]
-        self.retained_pairs = self.stat["left"]["pe"]
+        self.match_key = "match" if "match" in self.stat["left"] else "adapter"
+        self.retained_pairs_key = "pe" if "pe" in self.stat["left"] else "paired-end reads"
+        self.input_pairs = self.stat["left"]["ok"] + self.stat["left"][self.match_key] + self.stat["left"]["n"]
+        self.retained_pairs = self.stat["left"][self.retained_pairs_key]
