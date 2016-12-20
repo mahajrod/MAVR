48621303989a4fefea3fbc8b901da38d05b78598
Sergei Kliver <mahajrod@gmail.com>
2016-12-20 15:06:23 +0100

    changes

---
+++ scripts/convert/fastq2fasta.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#INPUT_FILES=$1
+#replace comma by space
+#INPUT_FILES=${INPUT_FILES//,/ }
+
+#cat ${INPUT_FILES} | awk '{if(NR%4==1 || NR%4==2) print}' | sed 's/^@/\>/'
+
+cat $@ | awk '{if(NR%4==1 || NR%4==2) print}' | sed 's/^@/\>/'
