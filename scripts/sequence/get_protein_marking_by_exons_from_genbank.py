f07b6e33c02ea6e719c0dd9afc352a5ad392f14a
mahajrod <mahajrod@gmail.com>
2016-12-26 17:26:01 +0300

    changes

--- scripts/sequence/get_protein_marking_by_exons_from_genbank.py
+++ scripts/sequence/get_protein_marking_by_exons_from_genbank.py
@@ -10,8 +10,8 @@ from Routines import SequenceRoutines
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--input", action="store", dest="input", type=SequenceRoutines.make_list_of_path_to_files,
-                    help="Comma-separated list of genbank files/directories with annotations")
+parser.add_argument("-i", "--input", action="store", dest="input",
+                    help="Genbank file with annotations")
 parser.add_argument("--fast_parsing", action="store_true", dest="fast_parsing",
                     help="Fast parsing mode - high memory consumption. Default: false")
 parser.add_argument("-o", "--output", action="store", dest="output",
@@ -21,7 +21,7 @@ args = parser.parse_args()
 
 out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
 
-record_dict = SeqIO.to_dict(SeqIO.parse(args.input, format="genbank")) if args.fast_parsing else SeqIO.index_db("temp_index.idx", args.input, format="genbank")
+record_dict = SeqIO.to_dict(SeqIO.parse(args.input, format="genbank")) if args.fast_parsing else SeqIO.index_db("temp_index.idx", [args.input], format="genbank")
 
 SequenceRoutines.get_protein_marking_by_exons_from_genbank(record_dict, args.output,
                                                            protein_id_field_in_cds_feature="protein_id")
