6b0f2573d56cfcd99b61943590866450469dbdad
mahajrod <mahajrod@gmail.com>
2017-02-13 18:26:24 +0300

    changes

--- scripts/download/download_from_sra_by_ids.py
+++ scripts/download/download_from_sra_by_ids.py
@@ -6,6 +6,11 @@ import argparse
 
 from CustomCollections.GeneralCollections import IdList
 
+from multiprocessing import Pool
+
+from Tools.Abstract import Tool
+
+
 def path_from_id(entry_id):
     ncbi_ftp = "ftp://ftp-trace.ncbi.nlm.nih.gov/"
     sra_reads_dir = "sra/sra-instant/reads/"
@@ -40,6 +45,9 @@ parser.add_argument("-i", "--ids", action="store", dest="ids",
                     help="Comma-separated list of SRA ids to download")
 parser.add_argument("-f", "--id_file", action="store", dest="id_file",
                     help="File with SRA ids(one per line) to download")
+parser.add_argument("-t", "--threads", action="store", dest="threads", type=int, default=1,
+                    help="Number of simultaneous downloads")
+
 args = parser.parse_args()
 
 if (not args.ids) and (not args.id_file):
@@ -48,6 +56,12 @@ if (not args.ids) and (not args.id_file):
 loader = IdList()
 id_list = loader.read(args.id_file) if args.id_file else args.ids
 
+options_list = []
 for entry_id in id_list:
     ftp_path = path_from_id(entry_id)
-    os.system("wget --no-host-directories -rc -t 500 %s" % ftp_path)
+    options_list.append("--no-host-directories -rc -t 500 %s" % ftp_path)
+
+
+tool = Tool(cmd="wget", max_threads=args.threads)
+
+tool.parallel_execute(options_list)
