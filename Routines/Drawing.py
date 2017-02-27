064ce675343c1a2c9944cad90fbc84c2383e2247
Sergei Kliver <mahajrod@gmail.com>
2017-02-27 03:23:57 +0100

    changes

--- Routines/Drawing.py
+++ Routines/Drawing.py
@@ -120,7 +120,7 @@ class DrawingRoutines(MatplotlibRoutines):
                 prev_x = gap_coords[1]
                 print [gap_coords[0], gap_coords[0] + int(gap_len/2), gap_coords[1]]
                 plt.plot([gap_coords[0], gap_coords[0] + int(gap_len/2), gap_coords[1]], #plt.plot([gap_coords[0] + 2, gap_coords[0] + int(gap_len/2) + 1, gap_coords[1] - 1],
-                         [gap_y_start, gap_y_jump, gap_y_start], color="black", linewidth=1)
+                         [gap_y_start, gap_y_jump, gap_y_start], color="black")
 
             if not gap_coords_list:
                 fragment = Rectangle((prev_x, start_y), alignment_length, protein_height, fill=False,
