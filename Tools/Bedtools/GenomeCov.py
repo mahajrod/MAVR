b3559bbc075b0097d35550cbce30c604ff45f312
mahajrod <mahajrod@gmail.com>
2017-02-28 19:12:36 +0300

    changes

--- Tools/Bedtools/GenomeCov.py
+++ Tools/Bedtools/GenomeCov.py
@@ -2,9 +2,10 @@
 __author__ = 'Sergei F. Kliver'
 
 import os
+import numpy as np
 
 from Tools.Abstract import Tool
-
+from Routines import MathRoutines
 
 class GenomeCov(Tool):
     def __init__(self, path="", max_threads=4):
@@ -22,9 +23,66 @@ class GenomeCov(Tool):
 
         self.execute(options)
 
-    def convert_coverage_file(self, coverage_file, output_file):
-        pass
+    def collapse_coverage_file(self, coverage_file, output_file):
+
+        awk_string = "awk -F'\\t' 'BEGIN {SCAF=\"\"; LEN=\"\"; COV=\"\"} {if ($1 != SCAF) {printf \"%s\\t%s\\t%s\\n\",SCAF,LEN,COV; SCAF=$1; LEN=$2; COV=$3} else {LEN=$2; COV=COV\",\"$3}} ; END {printf \"%s\\t%s\\t%s\\n\",SCAF,LEN,COV}' %s > %s" % (coverage_file, output_file)
+
+        self.execute(options="", cmd=awk_string)
+
+    @staticmethod
+    def analyze_collapsed_coverage_file(collapsed_file, output_file):
+        line_number = 0
+        with open(collapsed_file, "w") as in_fd:
+            with open(output_file, "r") as out_fd:
+
+                output_header = "#Scaffold\tLength\tMean_coverage\tMedian_coverage\tMin_coverage\tMax_coverage\tCoveraged_positions\tZero_coverage_position_number\tZero_coveraged_region_number\tLeading_zero_covarage_len\tTrailing_zero_covarage_len\tZero_coverage_region_coordinates\n"
+
+                out_fd.write(output_header)
+                for line in in_fd:
+                    line_number += 1
+                    if line == "\n":    # skip blank lines
+                        continue
+                    tmp = line.strip().split("\t")
+                    record_id = tmp[0]
+                    record_len = int(tmp[1])
+                    coverage_array = map(int, tmp[2].split(","))
+
+                    if record_len != len(coverage_array):
+                        raise ValueError("Malformed line %i" % line_number)
+
+                    mean_covarage = float(np.mean(coverage_array))
+                    median_coverage = float(np.median(coverage_array))
+                    min_coverage = float(np.min(coverage_array))
+                    max_coverage = float(np.max(coverage_array))
+                    coveraged_position_number = np.count_nonzero(coverage_array)
+
+                    zero_coverage_position_number, zero_coverage_regions_list = MathRoutines.find_flat_regions_in_array(coverage_array, value=0)
+
+                    if zero_coverage_position_number > 0:
+                        leading_zero_covarage_len = zero_coverage_regions_list[0][1]
+                        trailing_zero_covarage_len = zero_coverage_regions_list[-1][1]
+                        zero_coveraged_region_number = len(zero_coverage_regions_list)
+                        zero_coverage_coordinates_list = []
+                        for (start, length) in zero_coverage_regions_list:
+                            zero_coverage_coordinates_list.append("%i-%i" % (start + 1, start + length))
+
+                    else:
+                        leading_zero_covarage_len = 0
+                        trailing_zero_covarage_len = 0
+                        zero_coveraged_region_number = 0
+                        zero_coverage_coordinates = "."
+
+                    out_fd.write("%s\t%i\t%.2f\t%.2f\t%i\t%i\t%i\t%i\t%i\t%i\t%i\t%s\n" % (record_id,
+                                                                                           record_len,
+                                                                                           mean_covarage,
+                                                                                           median_coverage,
+                                                                                           min_coverage,
+                                                                                           max_coverage,
+                                                                                           coveraged_position_number,
+                                                                                           zero_coverage_position_number,
+                                                                                           zero_coveraged_region_number,
+                                                                                           leading_zero_covarage_len,
+                                                                                           trailing_zero_covarage_len,
+                                                                                           zero_coverage_coordinates))
 
 
-if __name__ == "__main__":
-    pass
