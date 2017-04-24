a7ead27a21ed204f204d831a6ae3856a7e4f918c
Sergei Kliver <mahajrod@gmail.com>
2017-04-25 00:31:06 +0300

    changes

--- scripts/annotation/exonerate/repare_annotation_file_from_transcript_and_cds.py
+++ scripts/annotation/exonerate/repare_annotation_file_from_transcript_and_cds.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'Sergei F. Kliver'
-
 import argparse
-
 from Tools.Annotation import Exonerate
-
 
 parser = argparse.ArgumentParser()
 
@@ -13,7 +10,7 @@ parser.add_argument("-t", "--transcript_file", action="store", dest="transcript_
 parser.add_argument("-c", "--cds_file", action="store", dest="cds_file", required=True,
                     help="File with sequences of CDS")
 parser.add_argument("-r", "--correspondence_file", action="store", dest="correspondence_file", required=True,
-                    help="File with correspondence of CDS to transcripts")
+                    help="File with correspondence of transcripts to CDS")
 parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
                     help="Output prefix")
 parser.add_argument("-f", "--format", action="store", dest="format", default="fasta",
