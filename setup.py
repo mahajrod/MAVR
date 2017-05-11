560d7051d74fe0b95ed57f7725fc865b74c5027d
mahajrod <mahajrod@gmail.com>
2017-05-11 11:30:59 +0300

    changes

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def get_list_of_files(list_of_dirs_and_files, expression=None, recursive=True):
 
 
 setup(name='MAVR',
-      version='0.55',
+      version='0.56',
       packages=find_packages(),
       author='Sergei F. Kliver',
       author_email='mahajrod@gmail.com',
