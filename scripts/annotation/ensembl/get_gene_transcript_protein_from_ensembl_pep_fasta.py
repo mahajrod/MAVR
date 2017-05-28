f061695eccf6b7103bec2d0583ff257ab92a6da1
mahajrod <mahajrod@gmail.com>
2017-05-28 15:15:55 +0300

    changes

---
+++ scripts/annotation/ensembl/get_gene_transcript_protein_from_ensembl_pep_fasta.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+
+import argparse
+from Routines import EnsemblRoutines
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input file with protein sequences")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file")
+
+args = parser.parse_args()
+
+EnsemblRoutines.get_gene_transcript_protein_from_ensembl_pep_fasta(args.input, args.output)
