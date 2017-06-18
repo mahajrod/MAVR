137614b13fce2ac8a4f0ff2d63c5af98bb9b1c76
Sergei Kliver <mahajrod@gmail.com>
2017-06-19 01:18:00 +0300

    changes

--- Tools/Expression/Subread.py
+++ Tools/Expression/Subread.py
@@ -247,7 +247,8 @@ class Subread(Tool):
         print similar_feature_number_dict
 
         for feature_id in no_multimapped_read_count_dict:
-            all_adjusted_read_count_dict[feature_id] = float(no_multimapped_read_count_dict[feature_id]) + float(with_multimapped_read_count_dict[feature_id]) / float(similar_feature_number_dict[feature_id])
+            all_adjusted_read_count_dict[feature_id] = float(no_multimapped_read_count_dict[feature_id]) + \
+                                                       (float(with_multimapped_read_count_dict[feature_id]) - float(no_multimapped_read_count_dict[feature_id])) / float(similar_feature_number_dict[feature_id])
 
         all_adjusted_read_count_dict.write(all_adjusted_read_counts)
 
