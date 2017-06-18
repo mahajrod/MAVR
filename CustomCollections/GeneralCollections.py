5f10454d3da3df121ea0d040a23c1b3984d822ac
Sergei Kliver <mahajrod@gmail.com>
2017-06-19 01:23:10 +0300

    changes

--- CustomCollections/GeneralCollections.py
+++ CustomCollections/GeneralCollections.py
@@ -371,9 +371,12 @@ class SynDict(OrderedDict):
             for line in in_fd:
                 if comments_prefix:
                     if line[: com_pref_len] == comments_prefix:
+                        print "BBBBBBBBBBB"
                         continue
+
                 else:
                     self.header = line.strip()
+                    print "AAAAAAAA"
                     break
         else:
             self.header = None
