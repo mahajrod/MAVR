ac5e4f46efd7539785d65117f76eafdad8403de8
Sergei Kliver <mahajrod@gmail.com>
2017-08-07 05:25:36 +0200

    changes

--- scripts/repeat_masking/extract_repeats_for_gene_annotation_from_gff.py
+++ scripts/repeat_masking/extract_repeats_for_gene_annotation_from_gff.py
@@ -11,6 +11,12 @@ parser.add_argument("-i", "--input_file", action="store", dest="input", required
                     help="Input file with RepeatMasker output")
 parser.add_argument("-p", "--output_prefix", action="store", dest="output_prefix", required=True,
                     help="Prefix for output files")
+parser.add_argument("-e", "--expanded_set", action="store_true", dest="expanded_set", default=False,
+                    help="Use expanded set of repeats. Includes 'Unknown', 'Other' and 'ARTEFACT' classes."
+                         "Default: False")
+parser.add_argument("-r", "--retain_unknown", action="store_true", dest="unknown", default=False,
+                    help="Retain repeats from 'Unknown' class."
+                         "Default: False")
 
 args = parser.parse_args()
 
@@ -21,4 +27,5 @@ extracted_gff = "%s.selected_repeat_classes.gff" % args.output_prefix
 
 RepeatMasker.convert_rm_out_to_gff(args.input, output_gff, repeat_classes, repeat_families)
 
-RepeatMasker.extract_repeats_used_for_gene_annotation(output_gff, extracted_gff)
+RepeatMasker.extract_repeats_used_for_gene_annotation(output_gff, extracted_gff, use_expanded_set=args.expanded_set,
+                                                      retain_unknown_repeats=args.unknown)
