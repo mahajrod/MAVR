865c02d0fe9bfc28bdf99b4f647ada79ee0351d2
Sergei Kliver <mahajrod@gmail.com>
2017-08-18 22:21:34 +0200

    changes

--- Routines/Evolution.py
+++ Routines/Evolution.py
@@ -1,5 +1,8 @@
 __author__ = 'mahajrod'
 
+import os
+from collections import OrderedDict
+from CustomCollections.GeneralCollections import IdList
 from Routines.Phylogenetics import PhylogeneticsRoutines
 
 
@@ -26,3 +29,39 @@ class EvolutionRoutines(PhylogeneticsRoutines):
                         out_fd.write(bootstrap_fd.next())
                 bootstrap_fd.next()  # read empty line between samples
                 counter += 1
+
+    def combine_ds_dn_w_from_bootstrap_data(self, input_dir, output_dir, use_node_names_if_possible=True):
+
+        dn_dir = "%s/dN/" % output_dir
+        ds_dir = "%s/dS/" % output_dir
+        w_dir = "%s/W/" % output_dir
+
+        for directory in output_dir, dn_dir, ds_dir, w_dir:
+            self.safe_mkdir(directory)
+
+        input_files = map(lambda s: "%s/%s" % (input_dir, s), os.listdir(input_dir))
+
+        data_dict = OrderedDict()
+        for filename in input_files:
+            with open(filename, "r") as in_fd:
+                in_fd.readline()  # read header
+                for line in in_fd:
+                    node_id, node_name, dn, ds, w = line.strip().split("\t")
+
+                    if use_node_names_if_possible:
+                        node = node_id if node_name == "." else node_name
+                    else:
+                        node = node_id
+
+                    if node not in data_dict:
+                        data_dict[node] = OrderedDict()
+                        for parameter in "dN", "dS", "W":
+                            data_dict[node][parameter] = IdList()
+                    data_dict[node]["dN"].append(dn)
+                    data_dict[node]["dS"].append(ds)
+                    data_dict[node]["W"].append(w)
+
+        for node in data_dict:
+            for parameter in "dN", "dS", "W":
+                out_file = "%s/%s/%s.tsv" % (output_dir, parameter, node)
+                data_dict[node][parameter].write(out_file)
