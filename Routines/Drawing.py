8126c62a9d3f52881475183fabcb6a80f39ea370
Sergei Kliver <mahajrod@gmail.com>
2017-08-19 01:47:34 +0200

    changes

--- Routines/Drawing.py
+++ Routines/Drawing.py
@@ -505,7 +505,7 @@ class DrawingRoutines(MatplotlibRoutines, SequenceRoutines):
         print xmax
         print bins
 
-        colors = plt.cm.jet(np.linspace(0, 1, 2*len(filelist)))
+        colors = plt.cm.jet(np.linspace(0, 0.5, len(filelist)))
 
         plt.hist(data, label=labels, bins=bins, histtype=histtype, color=colors)
 
