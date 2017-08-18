bd82afbb784f55dfc63432054688939be60faf83
Sergei Kliver <mahajrod@gmail.com>
2017-08-18 22:21:12 +0200

    changes

---
+++ scripts/evolution/combine_ds_dn_w_from_bootstrap_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+
+import argparse
+from Routines import EvolutionRoutines
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_dir", action="store", dest="input_dir", required=True,
+                    help="Input directory with files containing extracted data")
+parser.add_argument("-o", "--output_dir", action="store", dest="output_dir", required=True,
+                    help="Output directory")
+
+args = parser.parse_args()
+
+EvolutionRoutines.combine_ds_dn_w_from_bootstrap_data(args.input_dir, args.output_dir, use_node_names_if_possible=True)
