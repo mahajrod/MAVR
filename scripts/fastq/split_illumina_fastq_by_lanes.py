02fa03e7d5b54853a54b6c41e433ad6caff00937
Sergei Kliver <mahajrod@gmail.com>
2017-09-13 22:25:57 +0200

    changes

--- scripts/fastq/split_illumina_fastq_by_lanes.py
+++ scripts/fastq/split_illumina_fastq_by_lanes.py
@@ -12,7 +12,11 @@ parser.add_argument("-o", "--output_dir", action="store", dest="output_dir", req
                     help="Output directory")
 parser.add_argument("-p", "--output_prefix", action="store", dest="output_prefix",
                     help="Prefix of output files. Default: not set")
+parser.add_argument("-s", "--output_suffix", action="store", dest="output_suffix", default=".fastq",
+                    help="Suffix of output files. Default: .fastq")
 
 args = parser.parse_args()
 
-FastQRoutines.split_illumina_fastq_by_lanes(args.input, args.output_dir, output_prefix=args.output_prefix)
+FastQRoutines.split_illumina_fastq_by_lanes(args.input, args.output_dir,
+                                            output_prefix=args.output_prefix,
+                                            output_suffix=args.output_suffix)
