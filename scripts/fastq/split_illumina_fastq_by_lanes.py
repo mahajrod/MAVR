d780e9cc105ad37828e1580c59ab268a55b1d49e
Sergei Kliver <mahajrod@gmail.com>
2017-09-13 21:58:39 +0200

    changes

---
+++ scripts/fastq/split_illumina_fastq_by_lanes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+
+from Routines import FastQRoutines
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_file", action="store", dest="input", required=True,
+                    help="Input fastq file")
+parser.add_argument("-o", "--output_dir", action="store", dest="output_dir", required=True,
+                    help="Output directory")
+parser.add_argument("-p", "--output_prefix", action="store", dest="output_prefix",
+                    help="Prefix of output files. Default: not set")
+
+args = parser.parse_args()
+
+FastQRoutines.split_illumina_fastq_by_lanes(args.input, args.output_dir, output_prefix=args.output_prefix)
