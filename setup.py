f7e1e03a5c41ffade1ae1aa797af216c3218ba8c
mahajrod <mahajrod@gmail.com>
2017-09-25 20:26:09 +0300

    changes

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(name='MAVR',
       packages=find_packages(),
       author='Sergei F. Kliver',
       author_email='mahajrod@gmail.com',
-      install_requires=['scipy', 'numpy', 'matplotlib', 'ete2', 'biopython', 'xmltodict', 'bcbio-gff'],
+      install_requires=['scipy', 'numpy', 'matplotlib', 'ete2', 'biopython', 'xmltodict', 'bcbio-gff', 'statsmodels'],
       scripts=get_list_of_files("scripts/"),
       long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),)
