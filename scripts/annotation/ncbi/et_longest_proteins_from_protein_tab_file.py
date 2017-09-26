e5e2574d5bea4c18b062f62205e96dd637850975
Sergei Kliver <mahajrod@gmail.com>
2017-09-26 13:52:03 +0200

    changes

---
+++ scripts/annotation/ncbi/et_longest_proteins_from_protein_tab_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import argparse
+
+from Routines import NCBIRoutines
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input NCBI protein tab file")
+parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
+                    help="Prefix of output files")
+
+args = parser.parse_args()
+
+NCBIRoutines.et_longest_proteins_from_protein_tab_file(args.input, args.output_prefix)
