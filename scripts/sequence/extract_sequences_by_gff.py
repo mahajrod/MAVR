f6561f3db719e82aa113e4c4b460e10d6f0d6b21
mahajrod <mahajrod@gmail.com>
2017-10-23 02:36:57 +0300

    changes

--- scripts/sequence/extract_sequences_by_gff.py
+++ scripts/sequence/extract_sequences_by_gff.py
@@ -16,9 +16,9 @@ from Routines.Sequence import record_generator
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--input_file", action="store", dest="input_file",
+parser.add_argument("-i", "--input_file", action="store", dest="input",
                     help="Input file with sequences")
-parser.add_argument("-o", "--output_file", action="store", dest="output_file",
+parser.add_argument("-o", "--output_file", action="store", dest="output",
                     help="Output file with sequences")
 parser.add_argument("-f", "--format", action="store", dest="format", default="fasta",
                     help="Format of input and output files. Allowed formats genbank, fasta(default)")
@@ -40,7 +40,7 @@ print("Parsing %s..." % args.input_file)
 sequence_dict = SequenceRoutines.parse_seq_file(args.input, args.parsing_mode, args.format, index_file=tmp_index_file ) # SeqIO.index_db(tmp_index_file, args.input_file, format=args.format)
 annotations_dict = SeqIO.to_dict(GFF.parse(open(args.gff_file)))
 
-SeqIO.write(record_generator(annotations_dict, sequence_dict, args.type), args.output_file, format=args.format)
+SeqIO.write(record_generator(annotations_dict, sequence_dict, args.type), args.output, format=args.format)
 os.remove(tmp_index_file)
 
 
