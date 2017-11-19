5bc1164c9102bb2108595e028cd239a0dc12dcc9
mahajrod <mahajrod@gmail.com>
2017-11-19 16:19:46 +0300

    fixes

--- scripts/math/get_stats_from_numer_data.py
+++ scripts/math/get_stats_from_numer_data.py
@@ -20,7 +20,7 @@ parser.add_argument("-c", "--comments_prefix", action="store", dest="comments_pr
 parser.add_argument("-d", "--delimiter", action="store", dest="delimiter",
                     help="Value delimiter. Default - any space symbol")
 parser.add_argument("-l", "--columns", action="store", dest="columns", type=lambda s: s.split(","),
-                    help="Comma-separated list of columns with data. Default - all")
+                    help="Comma-separated list of columns(0-based) with data. Default - all")
 
 args = parser.parse_args()
 MathRoutines.get_stats_from_file(args.input, minimum=args.min, maximum=args.max, dtype=float,
