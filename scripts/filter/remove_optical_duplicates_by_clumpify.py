1ffdaa183d66763522a295b36d42fb4b14eff2a0
mahajrod <mahajrod@gmail.com>
2017-12-15 17:48:40 +0300

    fixes

--- scripts/filter/remove_optical_duplicates_by_clumpify.py
+++ scripts/filter/remove_optical_duplicates_by_clumpify.py
@@ -15,10 +15,13 @@ parser.add_argument("-r", "--reverse_reads", action="store", dest="reverse_reads
 parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
                     help="Prefix of output files")
 parser.add_argument("-g", "--gzip_output", action="store_true", dest="gzip_output", default=False,
-                    help="gzip output. Default: False")
+                    help="Compress output by gzip. Default: False")
+parser.add_argument("-m", "--memory_limit", action="store", dest="memory_limit", default="2g",
+                    help="Memory limit. Default: 2g")
 
 args = parser.parse_args()
 
 Clumpify.remove_optical_duplicates(args.forward_reads, args.output_prefix,
-                                   reverse_reads=args.reverse_reads, gzip_output=args.gzip_output)
+                                   reverse_reads=args.reverse_reads, gzip_output=args.gzip_output,
+                                   memory_limit="args.memory_limit")
 
