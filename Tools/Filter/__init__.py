ed4131f97568b39391037e8d70acb9d733edd8b6
mahajrod <mahajrod@gmail.com>
2017-12-15 17:44:24 +0300

    fixes

--- Tools/Filter/__init__.py
+++ Tools/Filter/__init__.py
@@ -4,6 +4,7 @@ from Tools.Filter.FaCut import FaCut
 from Tools.Filter.FastQC import FastQC
 from Tools.Filter.Adapters import adapters_PE
 from Tools.Filter.Cutadapt import Cutadapt
+from Tools.Filter.Clumpify import Clumpify
 from Tools.Filter.TrimGalore import TrimGalore
 from Tools.Filter.Trimmomatic import Trimmomatic
 from Tools.Filter.Cookiecutter import Cookiecutter
@@ -12,6 +13,7 @@ from Tools.Filter.Cookiecutter import CookiecutterOld
 FaCut = FaCut()
 FastQC = FastQC()
 Cutadapt = Cutadapt()
+Clumpify = Clumpify()
 TrimGalore = TrimGalore()
 Trimmomatic = Trimmomatic()
 Cookiecutter = Cookiecutter()
