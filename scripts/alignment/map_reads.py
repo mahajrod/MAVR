552bd47692d0fec366a8080993724f854cc539bd
mahajrod <zmeenosez72>
2018-02-07 15:45:05 +0300

    fixes

--- scripts/alignment/map_reads.py
+++ scripts/alignment/map_reads.py
@@ -8,7 +8,7 @@ from numpy import mean, median, array
 
 from Tools.Alignment import *
 from Tools.Samtools import SamtoolsV1, SamtoolsV0
-from Tools.Picard import AddOrReplaceReadGroups
+from Tools.Picard import AddOrReplaceReadGroups, MarkDuplicates
 from Tools.Bedtools import GenomeCov
 
 from CustomCollections.GeneralCollections import SynDict
@@ -19,38 +19,40 @@ def make_list_from_comma_sep_string(s):
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--aligner_index", action="store", dest="index",
-                    help="Aligner-specific index")
-parser.add_argument("-a", "--aligner", action="store", dest="aligner", default="bowtie2",
-                    help="Aligner to use. Possible aligners: bowtie2. Default: bowtie2")
-parser.add_argument("-t", "--threads", action="store", dest="threads", default=4, type=int,
-                    help="Number of threads. Default: 4")
-parser.add_argument("-b", "--bed_with_regions", action="store", dest="bed",
-                    help="Bed file with regions to output.")
-parser.add_argument("-r", "--right_reads", action="store", dest="right_reads",
+parser.add_argument("-p", "--prefix", action="store", dest="prefix", required=True,
+                    help="Prefix of output files")
+
+parser.add_argument("-r", "--forward_reads", action="store", dest="forward_reads",
                     type=make_list_from_comma_sep_string,
-                    help="Comma-separated list of files with right reads")
-parser.add_argument("-l", "--left_reads", action="store", dest="left_reads",
+                    help="Comma-separated list of files with forward reads")
+parser.add_argument("-l", "--reverse_reads", action="store", dest="reverse_reads",
                     type=make_list_from_comma_sep_string,
-                    help="Comma-separated list of files with left reads")
+                    help="Comma-separated list of files with reverse reads")
 parser.add_argument("-u", "--unpaired_reads", action="store", dest="unpaired_reads",
                     type=make_list_from_comma_sep_string,
                     help="Comma-separated list of files with unpaired reads")
+
+parser.add_argument("-i", "--aligner_index", action="store", dest="index",
+                    help="Aligner-specific index")
+
+parser.add_argument("-a", "--aligner", action="store", dest="aligner", default="bwa",
+                    help="Aligner to use. Possible aligners: bwa(default), bowtie2")
+parser.add_argument("-t", "--threads", action="store", dest="threads", default=4, type=int,
+                    help="Number of threads. Default: 4")
+
 parser.add_argument("-q", "--quality", action="store", dest="quality", default="phred33",
                     help="Quality type. Possible variants phred33, phred64. Default: phred33")
-parser.add_argument("-p", "--prefix", action="store", dest="prefix", default="alignment",
-                    help="Prefix of output files")
-parser.add_argument("-c", "--black_flag_value", action="store", dest="black_flag_value", type=int,
-                    help="Black bam flag value. By default unaligned, supplementary alignments and "
-                         "nonprimary alignments will be removed")
-parser.add_argument("-e", "--white_flag_value", action="store", dest="white_flag_value", type=int,
-                    help="White flag value")
-parser.add_argument("-g", "--dont_add_read_groups", action="store_true", dest="dont_add_read_groups", default=False,
-                    help="Don't add read groups to final bam")
+parser.add_argument("-a", "--alignment_format", action="store", dest="alignment_format", default="bam",
+                    help="Format of output alignments. Allowed: bam(default), sam, cram")
+
+
+parser.add_argument("-g", "--add_read_groups_by_picard", action="store_true", dest="add_read_groups_by_picard", default=False,
+                    help="Add read groups to final bam using PICARD. Use this option if aligner don't support adding readgroups itself")
 parser.add_argument("-d", "--picard_dir", action="store", dest="picard_dir",
                     help="Path to Picard directory. Required to add read groups")
 parser.add_argument("-n", "--retain_intermediate_files", action="store_true", dest="retain_temp", default=False,
                     help="Retain intermediate files")
+
 parser.add_argument("-y", "--coverage_bed", action="store", dest="coverage_bed", default="coverage.bed",
                     help="Bed file with coverage")
 parser.add_argument("-z", "--calculate_median_coverage", action="store_true", dest="calculate_median_coverage",
@@ -62,63 +64,81 @@ parser.add_argument("-x", "--calculate_mean_coverage", action="store_true", dest
 parser.add_argument("-f", "--flanks_size", action="store", dest="flanks_size",
                     default=0, type=int,
                     help="Size of flanks to remove when calculating mean/median coverage")
+
+
+
+parser.add_argument("-b", "--bed_with_regions", action="store", dest="bed",
+                    help="Bed file with regions to output.")
+
+parser.add_argument("-c", "--black_flag_value", action="store", dest="black_flag_value", type=int,
+                    help="Black bam flag value. By default unaligned, supplementary alignments and "
+                         "nonprimary alignments will be removed")
+parser.add_argument("-e", "--white_flag_value", action="store", dest="white_flag_value", type=int,
+                    help="White flag value")
+
+
+
+
+
+
 args = parser.parse_args()
 
 black_flag_value = args.black_flag_value if args.black_flag_value else \
     SamtoolsV0.bam_flag_values["unaligned"] + SamtoolsV0.bam_flag_values["supplementary_alignment"] \
     + SamtoolsV0.bam_flag_values["non_primary_alignment"]
 
-raw_alignment = "%s_raw_alignment.sam" % args.prefix
-filtered_alignment = "%s_filtered.bam" % args.prefix
-sorted_filtered_alignment_prefix = "%s_filtered_sorted" % args.prefix
-sorted_filtered_alignment = "%s_filtered_sorted.bam" % args.prefix
-rmdup_sorted_filtered_alignment = "%s_final.bam" % args.prefix
-rmdup_sorted_filtered_alignment_with_groups = "%s_final_with_groups.bam" % args.prefix
+sorted_alignment = "%s.%s" % (args.prefix, args.alignment_format)
+sorted_alignment_picard_groups = None
+
+final_alignment = "%s.mkdup.%s" % (args.prefix, args.alignment_format)
+duplicates_stat_file = "%s.duplicates.stat" % args.prefix
+
 if args.aligner == "bowtie2":
     aligner = Bowtie2
+elif args.aligner == "bwa":
+    aligner = BWA
+    
+else:
+    raise ValueError("")
 
 aligner.threads = args.threads
 
 
-aligner.align(args.index, right_reads_list=args.right_reads, left_reads_list=args.left_reads,
-              unpaired_reads_list=args.unpaired_reads, quality_score=args.quality, output_file=raw_alignment)
+aligner.align(args.index, forward_reads_list=args.forward_reads, reverse_reads_list=args.reverse_reads,
+              unpaired_reads_list=args.unpaired_reads, quality_score=args.quality, output_prefix=args.prefix,
+              output_format=args.alignment_format,
+              read_group_name="reads",
+              PU="x",
+              SM="sample",
+              platform="Illumina",
+              LB="x",
+              sort_by_coordinate=True,
+              sort_by_name=False,
+              max_per_sorting_thread_memory=None)
 
 
-"""
-# Samtools version 1+.  Rmdup doesnt work with bams containing reads from several libraries
-SamtoolsV1.threads = args.threads
-SamtoolsV1.view(raw_alignment, output_file=filtered_alignment, include_header_in_output=True,
-              output_uncompressed_bam=True, output_bam=True, white_flag_value=args.white_flag_value,
-              black_flag_value=black_flag_value, bed_file_with_regions_to_output=args.bed)
-SamtoolsV1.sort(filtered_alignment, sorted_filtered_alignment, temp_file_prefix="temp_bam")
-SamtoolsV1.rmdup(sorted_filtered_alignment, rmdup_sorted_filtered_alignment, treat_both_pe_and_se_reads=False)
-"""
-
-# Samtools v 0.1.19
-
-SamtoolsV0.view(raw_alignment, output_file=filtered_alignment, include_header_in_output=True,
-                output_uncompressed_bam=True, output_bam=True, white_flag_value=args.white_flag_value,
-                black_flag_value=black_flag_value, bed_file_with_regions_to_output=args.bed,
-                sam_input=True)
-SamtoolsV0.sort(filtered_alignment, sorted_filtered_alignment_prefix)
-
-
-SamtoolsV0.rmdup(sorted_filtered_alignment, rmdup_sorted_filtered_alignment, treat_both_pe_and_se_reads=False)
-
-if not args.dont_add_read_groups:
+if args.add_read_groups_by_picard:
+    sorted_alignment_picard_groups = "%s.picard_groups.%s" % (args.prefix, args.alignment_format)
     AddOrReplaceReadGroups.jar_path = args.picard_dir
-    AddOrReplaceReadGroups.add_read_groups(rmdup_sorted_filtered_alignment, rmdup_sorted_filtered_alignment_with_groups,
+    AddOrReplaceReadGroups.add_read_groups(sorted_alignment, sorted_alignment_picard_groups,
                                            RGID=args.prefix, RGLB=args.prefix, RGPL=args.prefix,
                                            RGSM=args.prefix, RGPU=args.prefix)
-    #os.remove(rmdup_sorted_filtered_alignment)
-    #os.rename("temp.bam", rmdup_sorted_filtered_alignment)
 
-SamtoolsV0.index(rmdup_sorted_filtered_alignment_with_groups if not args.dont_add_read_groups else rmdup_sorted_filtered_alignment)
-GenomeCov.get_coverage(rmdup_sorted_filtered_alignment_with_groups if not args.dont_add_read_groups else rmdup_sorted_filtered_alignment, args.coverage_bed)
+if args.alignment_format == "bam":
+    SamtoolsV1.index(sorted_alignment_picard_groups if sorted_alignment_picard_groups else sorted_alignment)
+
+MarkDuplicates.run(sorted_alignment_picard_groups if sorted_alignment_picard_groups else sorted_alignment,
+                   final_alignment,
+                   duplicates_stat_file)
+
+if args.alignment_format == "bam":
+    SamtoolsV1.index(final_alignment)
+
+GenomeCov.get_coverage(final_alignment, args.coverage_bed)
 if not args.retain_temp:
-    os.remove(raw_alignment)
-    os.remove(filtered_alignment)
-    os.remove(sorted_filtered_alignment)
+    os.remove(sorted_alignment)
+    if args.add_read_groups_by_picard:
+        os.remove(sorted_alignment_picard_groups)
 
 if args.calculate_median_coverage or args.calculate_mean_coverage:
     coverage_dict = SynDict()
