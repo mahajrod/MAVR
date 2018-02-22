094d9d1cf2818d4d714f540b3ff5d9cf8cd3deba
mahajrod <mahajrod@gmail.com>
2018-02-22 12:19:55 +0300

    fixes

---
+++ scripts/primers/get_alignments.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import argparse
+
+from Parsers.Primer3 import CollectionPrimer3
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-o", "--primer3_output", action="store", dest="primer3_output", required=True,
+                    help="File with Primer3 output")
+parser.add_argument("-a", "--alignments_file", action="store", dest="alignments_file", required=True,
+                    help="File to write alignments of primers to target sequence")
+
+args = parser.parse_args()
+
+primer3_collection = CollectionPrimer3(from_file=True, primer3_file=args.primer3_output)
+primer3_collection.write_alignments(args.alignments_file, segment_length=120, left_primer_symbol=">",
+                         target_symbol="*", right_primer_symbol="<")
+
