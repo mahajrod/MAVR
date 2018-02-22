dd931a5554a3ce149d33b3cadc8f4dce886aa70e
mahajrod <mahajrod@gmail.com>
2018-02-22 07:54:30 +0300

    fixes

--- CustomCollections/GeneralCollections.py
+++ CustomCollections/GeneralCollections.py
@@ -381,7 +381,10 @@ class SynDict(OrderedDict):
             #print line
             tmp = line.strip().split(separator) if separator else line.strip().split()
             #print tmp
+            print line
+            print tmp
             key, value = tmp[key_index], tmp[value_index]
+            print key, value
             if split_values or allow_repeats_of_key:
                 value = value.split(values_separator)
             #print key, value
