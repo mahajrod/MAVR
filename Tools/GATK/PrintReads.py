86c51596f6c34e04ae0c6c2739cdd587de20b6c2
mahajrod <mahajrod@gmail.com>
2018-02-26 14:45:27 +0300

    fixes

--- Tools/GATK/PrintReads.py
+++ Tools/GATK/PrintReads.py
@@ -7,4 +7,11 @@ from Tools.GATK.Abstract import GATKTool
 
 class PrintReads(GATKTool):
 
-    pass
+    def get_recalled_reads(self, reference, input_bam, recal_table, output_bam):
+        options = " -nct %i" % self.threads
+        options += " -R %s" % reference
+        options += " -I %s" % input_bam
+        options += " -BQSR %s" % recal_table
+        options += " -o %s" % output_bam
+        
+        self.execute(options, cmd="-T PrintReads")
