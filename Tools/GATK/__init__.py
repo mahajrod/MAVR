798b9927c6e947da571698a943dd684717d49fc1
mahajrod <mahajrod@gmail.com>
2018-02-26 14:34:59 +0300

    fixes

--- Tools/GATK/__init__.py
+++ Tools/GATK/__init__.py
@@ -3,6 +3,7 @@ __author__ = 'mahajrod'
 from Tools.GATK.IndelRealigner import IndelRealigner
 from Tools.GATK.SelectVariants import SelectVariants
 from Tools.GATK.CombineVariants import CombineVariants
+from Tools.GATK.BaseRecalibrator import BaseRecalibrator
 from Tools.GATK.UnifiedGenotyper import UnifiedGenotyper
 from Tools.GATK.VariantFiltration import VariantFiltration
 from Tools.GATK.ApplyRecalibration import ApplyRecalibration
@@ -14,6 +15,7 @@ from Tools.GATK.FastaAlternateReferenceMaker import FastaAlternateReferenceMaker
 IndelRealigner = IndelRealigner()
 SelectVariants = SelectVariants()
 CombineVariants = CombineVariants()
+BaseRecalibrator = BaseRecalibrator()
 UnifiedGenotyper = UnifiedGenotyper()
 VariantFiltration = VariantFiltration()
 ApplyRecalibration = ApplyRecalibration()
