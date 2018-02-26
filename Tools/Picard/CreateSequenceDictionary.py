ae6f6b46c076c7913c068964ae3fdd6993b9ff25
mahajrod <mahajrod@gmail.com>
2018-02-26 16:10:34 +0300

    fixes

--- Tools/Picard/CreateSequenceDictionary.py
+++ Tools/Picard/CreateSequenceDictionary.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 __author__ = 'Sergei F. Kliver'
-
+import os
 from Tools.Abstract import JavaTool
 
 
 class CreateSequenceDictionary(JavaTool):
 
     def __init__(self, java_path="", max_threads=4, jar_path="", max_memory="1g"):
-        jar = "CreateSequenceDictionary.jar"
-        JavaTool.__init__(self, jar, java_path=java_path, max_threads=max_threads,
+        JavaTool.__init__(self, "CreateSequenceDictionary.jar", java_path=java_path, max_threads=max_threads,
                           jar_path=jar_path, max_memory=max_memory)
 
     def make_fasta_dict(self, fasta_file, dict_name):
@@ -17,6 +16,15 @@ class CreateSequenceDictionary(JavaTool):
         options += " O= %s" % dict_name
 
         self.execute(options)
+
+    def check_for_fasta_dict(self, fasta_file):
+
+        fasta_path_list = self.split_filename(fasta_file)
+        dict_file = "%s/%s.dict" % (fasta_path_list[0], fasta_path_list[1])
+
+        if not os.path.exists(dict_file):
+            self.make_fasta_dict(fasta_file, dict_file)
+
 
 if __name__ == "__main__":
     pass
