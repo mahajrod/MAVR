943161c7abeb3662d8aa476e318c3b128283302f
mahajrod <mahajrod@gmail.com>
2018-02-26 15:27:20 +0300

    fixes

--- Pipelines/__init__.py
+++ Pipelines/__init__.py
@@ -1,10 +1,12 @@
 
+from Pipelines.SNPCall import SNPCallPipeline
 from Pipelines.Primers import STRPrimerPipeline
 from Pipelines.Abstract import Pipeline
 from Pipelines.Filtering import FilteringPipeline
 from Pipelines.DiffExpression import DiffExpressionPipeline
 
 Pipeline = Pipeline()
+SNPCallPipeline = SNPCallPipeline()
 FilteringPipeline = FilteringPipeline()
 STRPrimerPipeline = STRPrimerPipeline()
 DiffExpressionPipeline = DiffExpressionPipeline()
