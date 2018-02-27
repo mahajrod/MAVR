c67c2ec328d2c3ba21bcb50abd16de1756e45a61
mahajrod <mahajrod@gmail.com>
2018-02-27 18:16:39 +0300

    fixes

--- scripts/10x/prepare_reference.py
+++ scripts/10x/prepare_reference.py
@@ -15,9 +15,12 @@ parser.add_argument("-o", "--output", action="store", dest="output",
                     help="File to write prepared reference")
 parser.add_argument("-c", "--coord_file", action="store", dest="coord_file",
                     help="File to write coordinates of sequences in merged record")
+parser.add_argument("-l", "--longranger_dir", action="store", dest="longranger_dir", default="",
+                    help="Directory with longranger binary")
 
 args = parser.parse_args()
 
 
+LongRanger.path = args.longranger_dir
 LongRanger.prepare_reference(args.input, args.output, max_scaffold_length=527000000, max_scaffold_number=500,
                              polyN_len=500, coord_file=args.coord_file)
