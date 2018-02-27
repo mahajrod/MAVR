a0eb9f6b4c00a85769915351e6d2aa333817775b
mahajrod <mahajrod@gmail.com>
2018-02-27 17:41:57 +0300

    fixes

--- scripts/10x/prepare_reference.py
+++ scripts/10x/prepare_reference.py
@@ -11,12 +11,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input", action="store", dest="input", required=True,
                     help="File with input reference sequences")
-
 parser.add_argument("-o", "--output", action="store", dest="output",
                     help="File to write prepared reference")
+parser.add_argument("-c", "--coord_file", action="store", dest="coord_file",
+                    help="File to write coordinates of sequences in merged record")
 
 args = parser.parse_args()
 
 
 LongRanger.prepare_reference(args.input, args.output, max_scaffold_length=527000000, max_scaffold_number=500,
-                             polyN_len=500)
+                             polyN_len=500, coord_file=args.coord_file)
