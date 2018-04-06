6a4d5670f0d7c90d1e13a312d5fce486b2d7405d
mahajrod <mahajrod@gmail.com>
2018-04-06 17:00:10 +0300

    fixes

--- scripts/annotation/gff/convert_gff_to_simple_bed.py
+++ scripts/annotation/gff/convert_gff_to_simple_bed.py
@@ -17,10 +17,14 @@ parser.add_argument("-t", "--feature_types", action="store", dest="feature_types
 """
 parser.add_argument("-d", "--id_entry", action="store", dest="id_entry", default="ID",
                     help="Id entry. Default: ID")
-parser.add_argument("-p", "--parental_id_entry", action="store", dest="parental_id_entry", default="Parent",
-                    help="Parental id entry. Default: Parent")
 """
+
+parser.add_argument("-s", "--scaffold_id_file", action="store", dest="scaffold_id_file", default=None,
+                    help="File with IDs of scaffolds to include. Default: All")
+
 
 args = parser.parse_args()
 
-AnnotationsRoutines.convert_gff_to_simple_bed(args.input_gff, args.output_bed, feature_type_list=args.feature_types)
+AnnotationsRoutines.convert_gff_to_simple_bed(args.input_gff, args.output_bed,
+                                              feature_type_list=args.feature_types,
+                                              scaffold_id_file=args.scaffold_id_file)
