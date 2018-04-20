de406dc9f66695a9b92252d831045b61e2bfd848
mahajrod <mahajrod@gmail.com>
2018-04-20 08:00:55 +0300

    fixes

---
+++ scripts/file/exchange_key_and_value_in_syn_file.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import argparse
+from CustomCollections.GeneralCollections import SynDict
+from Routines import FileRoutines
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input syn file")
+parser.add_argument("-o", "--output", action="store", dest="output", default=sys.stdout,
+                    help="Output syn file")
+parser.add_argument("-s", "--separator", action="store", dest="separator", default="\t",
+                    help="Separator used in input file. Default: TAB")
+parser.add_argument("-l", "--split_values", action="store_true", dest="split_values", default=False,
+                    help="Split values in input file")
+parser.add_argument("-e", "--value_separator", action="store", dest="value_separator", default=",",
+                    help="Value separator in input file. Default: ,")
+
+args = parser.parse_args()
+
+input_dict = SynDict(filename=args.input, separator=args.separator, split_values=args.split_values,
+                     values_separator=args.value_separator)
+
+output_dict = input_dict.exchange_key_and_value()
+output_dict.write(args.output, splited_values=True)
