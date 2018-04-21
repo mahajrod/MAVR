6953dbe63f86c4115ed6cd5ad83d2b79c993813b
mahajrod <mahajrod@gmail.com>
2018-04-21 05:30:30 +0300

    fixes

---
+++ scripts/sequence_clusters/eggnog/extract_eggnogmapper_annotations_by_protein_ids.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import sys
+import shutil
+import argparse
+
+from Tools.Annotation import Emapper
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="File with emapper annotations")
+parser.add_argument("-d", "--protein_id_file", action="store", dest="protein_id_file", required=True,
+                    help="File with protein ids to extract")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file")
+
+args = parser.parse_args()
+
+Emapper.extract_eggnogmapper_annotations_by_protein_ids(args.input, args.protein_id_file, args.output)
