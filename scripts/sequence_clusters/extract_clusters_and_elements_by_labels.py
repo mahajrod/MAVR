9a6068bae4ff26f27af13acef11fc8e98a2cdc1c
mahajrod <mahajrod@gmail.com>
2018-04-21 07:03:46 +0300

    fixes

--- scripts/sequence_clusters/extract_clusters_and_elements_by_labels.py
+++ scripts/sequence_clusters/extract_clusters_and_elements_by_labels.py
@@ -10,17 +10,26 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input", action="store", dest="input", required=True,
                     help="File with clusters")
-parser.add_argument("-l", "--label_file", action="store", dest="label_file", required=True,
-                    help="File with labels to extract")
+parser.add_argument("-f", "--label_file", action="store", dest="label_file",
+                    help="File with element labels to extract")
 parser.add_argument("-o", "--output", action="store", dest="output", required=True,
-                    help="File to write extracted_clusters")
+                    help="File to write extracted clusters")
 parser.add_argument("-p", "--label position", action="store", dest="label_position", default="first",
                     help="Position of label. Allowed - first, last. Default - first")
 parser.add_argument("-s", "--separator", action="store", dest="separator", default="@",
                     help="Separator to use. default - '@'")
+parser.add_argument("-l", "--label_list", action="store", dest="label_list", type=lambda s: s.split(","),
+                    help="Comma-separated list of element labels to extract")
 
 args = parser.parse_args()
 
-SequenceClusterRoutines.extract_clusters_and_elements_by_labels_from_files(args.input, args.label_file, args.output,
+if args.label_file and args.label_list:
+    raise ValueError("Both --label_file and --label_list were set")
+elif (not args.label_file) and (not args.label_list):
+    raise ValueError("Neither --label_file or --label_list was set")
+
+SequenceClusterRoutines.extract_clusters_and_elements_by_labels_from_files(args.input,
+                                                                           args.label_file if args.label_file else args.label_list,
+                                                                           args.output,
                                                                            separator=args.separator,
                                                                            label_position=args.label_position)
