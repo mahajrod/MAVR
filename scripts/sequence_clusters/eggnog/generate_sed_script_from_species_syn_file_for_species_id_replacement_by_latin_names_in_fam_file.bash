064338c36d70e9bbe5a5542b1eff0db6a1658713
mahajrod <mahajrod@gmail.com>
2018-04-21 03:59:58 +0300

    fixes

--- scripts/sequence_clusters/eggnog/generate_sed_script_from_species_syn_file_for_species_id_replacement_by_latin_names_in_fam_file.bash
+++ scripts/sequence_clusters/eggnog/generate_sed_script_from_species_syn_file_for_species_id_replacement_by_latin_names_in_fam_file.bash
@@ -2,4 +2,4 @@
 
 sed 's/\(.*\)\t\(.*\)/g;s\/\1\.\/\2.\//g' $1
 
-#sed 's/\(.*\)\t\(.*\)/s\/>\1\.\/>\2.\//' species.syn  > species.sed
+#sed 's/\(.*\)\t\(.*\)/g;s\/\1\.\/\2.\//g' species.syn  > species.sed
