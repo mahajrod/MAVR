331584240ab6867d072289337223b31b42ef9dfc
mahajrod <mahajrod@gmail.com>
2018-04-26 15:58:07 +0300

    fixes

--- scripts/sequence_clusters/extract_selected_families_from_fam_file.py
+++ scripts/sequence_clusters/extract_selected_families_from_fam_file.py
@@ -12,6 +12,8 @@ parser.add_argument("-i", "--input", action="store", dest="input", required=True
                     help="Input fam file")
 parser.add_argument("-d", "--id_file", action="store", dest="id_file", required=True,
                     help="File with ids of families to extract")
+parser.add_argument("-c", "--id_column_index", action="store", dest="id_column_index", type=int,
+                    help="Index(0-based) of columns in ID file with ids of families to extract")
 parser.add_argument("-o", "--output", action="store", dest="output", default="stdout",
                     help="File to write extracted families. Default - stdout")
 parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
@@ -21,11 +23,9 @@ args = parser.parse_args()
 
 out_file = sys.stdout if args.output == "stdout" else open(args.output, "w")
 
-fam_dict = SynDict()
-fam_dict.read(args.input)
+fam_dict = SynDict(filename=args.input)
 
-id_set = IdSet()
-id_set.read(args.id_file)
+id_set = IdSet(filename=args.id_file, column_number=args.id_column_index)
 
 extracted_dict = SynDict()
 for id_entry in id_set:
