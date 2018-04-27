2d40f978040856f9007db4a326d6940f74c4fd3c
mahajrod <mahajrod@gmail.com>
2018-04-27 16:31:28 +0300

    fixes

--- scripts/sequence_clusters/label_cluster_elements.py
+++ scripts/sequence_clusters/label_cluster_elements.py
@@ -18,11 +18,16 @@ parser.add_argument("-p", "--label position", action="store", dest="label_positi
                     help="Position of label. Allowed - first, last. Default - first")
 parser.add_argument("-s", "--separator", action="store", dest="separator", default="@",
                     help="Separator to use. default - '@'")
-
+parser.add_argument("-k", "--key_column_index", action="store", dest="key_column_index", type=int, default=0,
+                    help="Index of key column in synonym file. Default: 0")
+parser.add_argument("-v", "--value_column_index", action="store", dest="value_column_index", type=int, default=1,
+                    help="Index of value column in synonym file.Default: 1")
 args = parser.parse_args()
 
 label = args.label if args.label else FileRoutines.split_filename(args.cluster_file)[1]
 
 SequenceClusterRoutines.label_cluster_elements_from_file(args.cluster_file, label, args.output,
                                                          separator=args.separator,
-                                                         label_position=args.label_position)
+                                                         label_position=args.label_position,
+                                                         key_index=args.key_column_index,
+                                                         value_index=args.value_column_index)
