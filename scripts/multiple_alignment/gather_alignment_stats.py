d4870385b79d7bd4d7917b1b4add670fcafd009e
mahajrod <mahajrod@gmail.com>
2018-05-07 06:08:32 +0300

    fixes

---
+++ scripts/multiple_alignment/gather_alignment_stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+
+from Parsers.MultipleAlignment import MultipleAlignmentStatCollection
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", type=lambda x: x.split(","), required=True,
+                    help="Comma-separated list of files or directory with files containing sequences with alignment")
+"""
+parser.add_argument("-o", "--output_directory", action="store", dest="output", type=FileRoutines.check_path,
+                    help="Output directory")
+"""
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file with results")
+
+"""
+parser.add_argument("-m", "--mode", action="store", dest="mode", default="globalpair",
+                    help="Alignment mode. Default: 'globalpair'. Allowed: globalpair, localpair, genafpair")
+parser.add_argument("-q", "--quiet", action="store_true", dest="quiet",
+                    help="Quiet output")
+parser.add_argument("-x", "--maxiterate", action="store", dest="maxiterate", type=float,
+                    help="Maximum number of iterations")
+parser.add_argument("-f", "--offset", action="store", dest="offset", type=float,
+                    help="Offset (works like gap extension penalty)")
+parser.add_argument("-g", "--gap_open_penalty", action="store", dest="gap_open_penalty", type=float,
+                    help="Gap open penalty")
+"""
+args = parser.parse_args()
+
+stat_collection = MultipleAlignmentStatCollection(input_file=args.input)
+stat_collection.write_general_stats(args.output)
