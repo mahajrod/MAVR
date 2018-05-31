8dc85b37acce1f5656b09a1dc6ff3237abb20e3e
mahajrod <mahajrod@gmail.com>
2018-05-31 18:57:00 +0300

    fixes

---
+++ scripts/annotation/gff/extract_transcripts_by_gff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+
+from Tools.Expression import Gffread
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_gff", action="store", dest="input_gff", required=True,
+                    help="Input GFF file")
+parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
+                    help="Prefix of output files")
+parser.add_argument("-g", "--genomic_fasta", action="store", dest="genomic_fasta", required=True,
+                    help="Fasta with genome sequence")
+
+args = parser.parse_args()
+
+Gffread.extract_transcript_sequences(args.input_gff, args.genomic_fasta, args.output_prefix, coding_only=False)
