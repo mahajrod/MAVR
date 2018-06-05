1fde2966c2b926ae0c60e4e0747ba32d97cdf679
mahajrod <mahajrod@gmail.com>
2018-06-05 15:57:34 +0300

    fixes

---
+++ scripts/annotation/VEP/prepare_gff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import sys
+import argparse
+
+from Tools.Annotation import VEP
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_gff", action="store", dest="input", required=True, type=lambda s: s.split(","),
+                    help="List of input .gff file")
+parser.add_argument("-p", "--output_prefix", action="store", dest="output_prefix", required=True,
+                    help="Prefix of output files")
+
+args = parser.parse_args()
+
+VEP.prepare_gff(args.input, args.output_prefix)
