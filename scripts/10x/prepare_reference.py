b17d295640f37c82d274f678d00a625dadd5ddef
mahajrod <mahajrod@gmail.com>
2018-06-18 02:56:04 +0300

    fixes

--- scripts/10x/prepare_reference.py
+++ scripts/10x/prepare_reference.py
@@ -17,10 +17,16 @@ parser.add_argument("-c", "--coord_file", action="store", dest="coord_file",
                     help="File to write coordinates of sequences in merged record")
 parser.add_argument("-l", "--longranger_dir", action="store", dest="longranger_dir", default="",
                     help="Directory with longranger binary")
-
+parser.add_argument("-e", "--renamed_scaffolds_syn_file", action="store", dest="renamed_scaffolds_syn_file",
+                    required=True,
+                    help="File with synonyms to renamed scaffolds")
+parser.add_argument("-r", "--symbols_to_replace_list", action="store", dest="symbols_to_replace_list", default=[":"],
+                    type=lambda s: s.split(","),
+                    help="Comma-separated list of symbols to replace in scaffold ids. Default: :")
 args = parser.parse_args()
 
 
 LongRanger.path = args.longranger_dir
-LongRanger.prepare_reference(args.input, args.output, max_scaffold_length=527000000, max_scaffold_number=500,
-                             polyN_len=500, coord_file=args.coord_file)
+LongRanger.prepare_reference(args.input, args.output, args.renamed_scaffolds_syn_file,
+                             max_scaffold_length=527000000, max_scaffold_number=500,
+                             polyN_len=500, coord_file=args.coord_file, symbols_to_replace_list=args.symbols_to_replace_list)
