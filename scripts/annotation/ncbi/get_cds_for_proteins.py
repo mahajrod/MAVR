02c68945b1d2096d591edb4e166b10fb03513991
mahajrod <mahajrod@gmail.com>
2018-07-03 07:58:49 +0300

    fixes

--- scripts/annotation/ncbi/get_cds_for_proteins.py
+++ scripts/annotation/ncbi/get_cds_for_proteins.py
@@ -12,7 +12,7 @@ parser.add_argument("-i", "--input", action="store", dest="input", required=True
                     help="Input file with ids of ncbi peptides")
 parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
                     help="Prefix of output files")
-parser.add_argument("-c", "--chunk_size", action="store", dest="chunk_size", default=300, type=int,
+parser.add_argument("-c", "--chunk_size", action="store", dest="chunk_size", default=150, type=int,
                     help="Download chunk size. Default: 300")
 args = parser.parse_args()
 
