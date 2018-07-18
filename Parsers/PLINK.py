e92a243f9f28ea0e0431a2cc83c8a3690c608c08
mahajrod <mahajrod@gmail.com>
2018-07-18 11:44:46 +0300

    fixes

--- Parsers/PLINK.py
+++ Parsers/PLINK.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
+import pandas
 __author__ = 'mahajrod'
 from collections import OrderedDict
 from CustomCollections.GeneralCollections import TwoLvlDict
 
 
 class PLINKReport(OrderedDict):
-    def __init__(self, plink_report_file, report_type="ROH"):
+    def __init__(self, plink_report_file, report_type="ROH", separator="\s+"):
         OrderedDict.__init__(self)
+        self.report_type = report_type
+        self.families = None
+        self.samples = None
+        self.roh_lengths = None
 
+        index_col = None
+        if report_type == "ROH":
+            index_col = (0, 1)
+
+        self.data = pandas.read_table(plink_report_file, sep=separator, index_col=index_col)
+
+        if report_type == "ROH":
+            self.families = self.data.index.levels[0]
+            self.samples = self.data.index.levels[1]
+        """
         with open(plink_report_file, "r") as in_fd:
             if report_type == "ROH":
+
+
                 header_list = in_fd.readline().strip().split()
 
             for line in in_fd:
@@ -24,6 +41,16 @@ class PLINKReport(OrderedDict):
                     self[tmp[0]][tmp[1]][header_list[j]] = float(self[tmp[0]][tmp[1]][header_list[j]])
                 for j in 6, 7, 9:
                     self[tmp[0]][tmp[1]][header_list[j]] = int(self[tmp[0]][tmp[1]][header_list[j]])
+            """
+
+    def get_roh_lengths(self, save=False):
+        """
+        return: pandas Series
+        """
+        if save:
+            self.roh_lengths = self.data['POS2'] - self.data['POS1'] + 1
+        return self.roh_lengths if self.roh_lengths is not None else self.data['POS2'] - self.data['POS1'] + 1
+
 
 
 class PLINKReportCollection(OrderedDict):
