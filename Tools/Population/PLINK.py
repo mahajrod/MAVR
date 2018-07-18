f0258d11963bab03a18d99e93cd8291a83ad85a0
mahajrod <mahajrod@gmail.com>
2018-07-18 11:44:51 +0300

    fixes

--- Tools/Population/PLINK.py
+++ Tools/Population/PLINK.py
@@ -1,12 +1,13 @@
 
 import numpy as np
+from collections import OrderedDict
 
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
 from Tools.Abstract import Tool
-
+from Parsers.PLINK import PLINKReport
 
 class PLINK(Tool):
     def __init__(self, path="", max_threads=4, max_memory="100G", max_per_thread_memory="5G"):
@@ -18,6 +19,148 @@ class PLINK(Tool):
 
         # constans for PLINK bim file
         self.allel_columns_in_bim_file = (4, 5)
+
+
+    def parse_common_options(self, output_prefix, input_vcf_file=None, allow_noncanonical_chromosome_names=None, keep_autoconverted_files=None):
+
+        options = " --threads %i" % self.threads
+        options += " --out %s" % output_prefix
+        options += " --allow-extra-chr" if allow_noncanonical_chromosome_names else ""
+        options += " --keep-autoconv" if keep_autoconverted_files else ""
+        options += " --vcf %s" % input_vcf_file if input_vcf_file else ""
+
+        return options
+
+    def find_runs_of_homozygosity_options(self,
+                                          roh_calling_method=None,
+                                          window_length_in_kb=None,
+                                          min_homozygous_snps_per_window=None,
+                                          max_heterozygous_snps_per_window=None,
+                                          max_missing_snps_per_window=None,
+                                          max_inverse_density_of_homozygous_snps_in_kb_per_snp=None,
+                                          max_internal_gap_in_kb=None,
+                                          min_roh_length=None,
+                                          min_homozygous_snps_in_roh=None,
+                                          min_scanning_window_hit_rate=None,
+                                          generate_overlapping_segments=False,
+                                          max_heterozygous_snps=None,
+                                          min_concordance_across_jointly_homozygous_variants=None,
+                                          homozygous_verbose=False):
+        """
+        ROH calling methods:
+        <group | group-verbose> <consensus-match> <extend> <subtract-1-from-lengths>
+
+        """
+        options = " --homozyg %s" % roh_calling_method if roh_calling_method else " --homozyg"
+        options += " --homozyg-window-snp %i" % min_homozygous_snps_per_window if min_homozygous_snps_per_window else ""                      # 50
+        options += " --homozyg-window-het %i" % max_heterozygous_snps_per_window if max_heterozygous_snps_per_window else ""                  # 1
+        options += " --homozyg-window-missing %i" % max_missing_snps_per_window if max_missing_snps_per_window else ""                        # 5
+        options += " --homozyg-window-kb %i" % window_length_in_kb if window_length_in_kb else ""                                             # 5000
+        options += " --homozyg-window-threshold %f" % min_scanning_window_hit_rate if min_scanning_window_hit_rate else ""                    # 0.05
+
+        options += " --homozyg-density %i" % max_inverse_density_of_homozygous_snps_in_kb_per_snp if max_inverse_density_of_homozygous_snps_in_kb_per_snp else "" # 50
+        options += " --homozyg-gap %i" % max_internal_gap_in_kb if max_internal_gap_in_kb else ""                                                                 # 1000
+        options += " --homozyg-snp %i" % min_homozygous_snps_in_roh if min_homozygous_snps_in_roh else ""                                                         # 100
+        options += " --homozyg-kb %i" % min_roh_length if min_roh_length else ""                                                                                  # 1000
+        options += " --homozyg-match %f" % min_concordance_across_jointly_homozygous_variants if min_concordance_across_jointly_homozygous_variants else ""       # 0.95
+        options += " --homozyg-het %i" % max_heterozygous_snps if max_heterozygous_snps else ""                                                                 # unlimited
+        options += " --homozyg-group" if generate_overlapping_segments else ""
+        options += " --homozyg-verbose" if homozygous_verbose else "" #
+
+        return options
+
+    def find_runs_of_homozygosity(self,
+                                  output_prefix,
+                                  input_vcf_file=None,
+                                  allow_noncanonical_chromosome_names=None,
+                                  keep_autoconverted_files=None,
+                                  roh_calling_method=None,
+                                  window_length_in_kb=None,
+                                  min_homozygous_snps_per_window=None,
+                                  max_heterozygous_snps_per_window=None,
+                                  max_missing_snps_per_window=None,
+                                  max_inverse_density_of_homozygous_snps_in_kb_per_snp=None,
+                                  max_internal_gap_in_kb=None,
+                                  min_roh_length=None,
+                                  min_homozygous_snps_in_roh=None,
+                                  min_scanning_window_hit_rate=None,
+                                  generate_overlapping_segments=False,
+                                  max_heterozygous_snps=None,
+                                  min_concordance_across_jointly_homozygous_variants=None,
+                                  homozygous_verbose=False):
+        options = self.parse_common_options(output_prefix,
+                                            input_vcf_file=input_vcf_file,
+                                            allow_noncanonical_chromosome_names=allow_noncanonical_chromosome_names,
+                                            keep_autoconverted_files=keep_autoconverted_files)
+
+        options += self.find_runs_of_homozygosity_options(roh_calling_method=roh_calling_method,
+                                                          window_length_in_kb=window_length_in_kb,
+                                                          min_homozygous_snps_per_window=min_homozygous_snps_per_window,
+                                                          max_heterozygous_snps_per_window=max_heterozygous_snps_per_window,
+                                                          max_missing_snps_per_window=max_missing_snps_per_window,
+                                                          max_inverse_density_of_homozygous_snps_in_kb_per_snp=max_inverse_density_of_homozygous_snps_in_kb_per_snp,
+                                                          max_internal_gap_in_kb=max_internal_gap_in_kb,
+                                                          min_roh_length=min_roh_length,
+                                                          min_homozygous_snps_in_roh=min_homozygous_snps_in_roh,
+                                                          min_scanning_window_hit_rate=min_scanning_window_hit_rate,
+                                                          generate_overlapping_segments=generate_overlapping_segments,
+                                                          max_heterozygous_snps=max_heterozygous_snps,
+                                                          min_concordance_across_jointly_homozygous_variants=min_concordance_across_jointly_homozygous_variants,
+                                                          homozygous_verbose=homozygous_verbose)
+        self.execute(options=options)
+
+    def test_roh_parameters(self,
+                            output_dir,
+                            output_prefix,
+                            input_vcf_file,
+                            allow_noncanonical_chromosome_names=None,
+                            keep_autoconverted_files=None,
+                            window_length_in_kb=None,
+                            min_homozygous_snps_per_window=(1, 51, 1),
+                            min_homozygous_snps_in_roh=(1, 101, 1),
+                            max_heterozygous_snps_per_window=(1, 11, 1),
+                            max_heterozygous_snps=(1, 21, 1),
+                            max_inverse_density_of_homozygous_snps_in_kb_per_snp=(50, 1000, 50),
+                            ):
+        self.safe_mkdir(output_dir)
+        plink_report_dict = OrderedDict()
+
+        for i in range(*min_homozygous_snps_per_window):
+            plink_report_dict[i] = OrderedDict()
+            for j in range(*min_homozygous_snps_in_roh):
+                plink_report_dict[j] = OrderedDict()
+                for k in range(*max_heterozygous_snps_per_window):
+                    plink_report_dict[k] = OrderedDict()
+                    for l in range(*max_heterozygous_snps):
+                        plink_report_dict[l] = OrderedDict()
+                        for m in range(*max_inverse_density_of_homozygous_snps_in_kb_per_snp):
+
+                            dir_name = "%s/%i_%i_%i_%i_%i/" % (output_dir, i, j, k, l, m)
+                            description_text = "Minimum homozygous SNPs per window:\t%i\n" % i
+                            description_text += "Minimum homozygous SNPs in ROh:\t%i\n" % j
+                            description_text += "Maximum heterozygous SNPs per window:\t%i\n" % k
+                            description_text += "Max heterozygous SNPs:\t%i\n" % l
+                            description_text += "Max inverse density of homozygous SNPs(kb/SNP)" % m
+                            self.safe_mkdir(dir_name, description_text=description_text, description_filename="DESCRIPTION")
+                            self.find_runs_of_homozygosity("%s/%s" % (output_dir, output_prefix),
+                                                           input_vcf_file=input_vcf_file,
+                                                           allow_noncanonical_chromosome_names=allow_noncanonical_chromosome_names,
+                                                           keep_autoconverted_files=keep_autoconverted_files,
+                                                           roh_calling_method=None,
+                                                           window_length_in_kb=window_length_in_kb,
+                                                           min_homozygous_snps_per_window=i,
+                                                           max_heterozygous_snps_per_window=k,
+                                                           max_missing_snps_per_window=None,
+                                                           max_inverse_density_of_homozygous_snps_in_kb_per_snp=m,
+                                                           max_internal_gap_in_kb=None,
+                                                           min_roh_length=None,
+                                                           min_homozygous_snps_in_roh=j,
+                                                           min_scanning_window_hit_rate=None,
+                                                           generate_overlapping_segments=False,
+                                                           max_heterozygous_snps=l,
+                                                           min_concordance_across_jointly_homozygous_variants=None,
+                                                           homozygous_verbose=False)
+                            plink_report_dict[m] = PLINKReport("%s/%s" % (output_dir, output_prefix), report_type="ROH")
 
     @staticmethod
     def get_samples_list_from_plink_fam_file(plink_fam_file, verbose=False, sample_id_column=1):
