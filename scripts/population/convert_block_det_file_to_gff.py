6dc12c69fa914e785c4bfab686b72f44ac5b7488
mahajrod <mahajrod@gmail.com>
2018-07-20 18:16:44 +0300

    fixes

--- scripts/population/convert_block_det_file_to_gff.py
+++ scripts/population/convert_block_det_file_to_gff.py
@@ -14,7 +14,10 @@ parser.add_argument("-o", "--output", action="store", dest="output", required=Tr
                     help="Output GFF file with block")
 parser.add_argument("-f", "--output_format", action="store", dest="output_format", default="gff",
                     help="Format of output file. Default: gff, bed")
+parser.add_argument("-b", "--haploblock_id_prefix", action="store", dest="haploblock_id_prefix", default="HB",
+                    help="Prefix of haploblock ids. Default: HB")
 
 args = parser.parse_args()
 
-PLINK.convert_blocks_det_file_to_gff(args.block_det_file, args.output, output_format=args.output_format)
+PLINK.convert_blocks_det_file_to_gff(args.block_det_file, args.output, output_format=args.output_format,
+                                     haploblock_id_prefix=args.haploblock_id_prefix)
