d04438311fbfd6aca50d49e8a1e330cc18be65f6
mahajrod <mahajrod@gmail.com>
2018-07-30 16:04:12 +0300

    fixes

--- Tools/GATK/CatVariants.py
+++ Tools/GATK/CatVariants.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 __author__ = 'mahajrod'
-
+import shutil
+import numpy as np
 from Tools.Abstract import JavaTool
-
-import os
-from Routines.Functions import check_path
 
 
 class CatVariants(JavaTool):
@@ -33,7 +31,8 @@ class CatVariants(JavaTool):
         return options
 
     def combine_gvcf(self, reference, gvcf_list, output, input_is_sorted=False, extension_list=["g.vcf",],
-                     tmp_dir="tmp_combine_gvcf", max_files_per_merge=50):
+                     tmp_dir="./tmp_combine_gvcf/", max_files_per_merging=50, iteration=0, threads=None,
+                     remove_intermediate_files=False):
         """
         java -jar GenomeAnalysisTK.jar \
            -T GenotypeGVCFs \
@@ -42,9 +41,39 @@ class CatVariants(JavaTool):
            --variant sample2.g.vcf \
            -o output.vcf
         """
-        if len(gvcf_list) <= max_files_per_merge:
+        if len(gvcf_list) <= max_files_per_merging:
             options = self.parse_options(reference, gvcf_list, output, input_is_sorted, extension_list=extension_list)
             self.execute(options, runtype="cp")
+            if remove_intermediate_files:
+                shutil.rmtree(tmp_dir, ignore_errors=True)
+
         else:
-            pass
+            self.safe_mkdir(tmp_dir)
+            iteration_dir = "%s/iteration_%i/" % (tmp_dir, iteration)
+            self.safe_mkdir(iteration_dir)
+
+            number_of_files = len(gvcf_list)
+
+            bins = np.arange(0, number_of_files, 1)
+            if bins[-1] != number_of_files:
+                bins = np.append(bins, number_of_files, 1)
+
+            output_file_list = []
+            options_list = []
+            for i in range(0, len(bins)-1):
+                output_file = "%s/%i.g.vcf" % (iteration_dir, i)
+                output_file_list.append(output_file)
+
+                options_list.append(self.parse_options(reference,
+                                                       gvcf_list[bins[i]:bins[i+1]],
+                                                       output_file,
+                                                       input_is_sorted, extension_list=[]))
+
+            self.parallel_execute(options_list, threads=threads)
+
+            self.combine_gvcf(reference, output_file_list, output, input_is_sorted=input_is_sorted,
+                              extension_list=extension_list,
+                              tmp_dir=tmp_dir,
+                              max_files_per_merging=max_files_per_merging, iteration=iteration+1)
+
 
