745b363b0bf8748711929dbc6f3f7fb5e8184690
mahajrod <mahajrod@gmail.com>
2018-07-30 17:49:23 +0300

    fixes

--- Tools/GATK/CatVariants.py
+++ Tools/GATK/CatVariants.py
@@ -23,6 +23,8 @@ class CatVariants(JavaTool):
                                                                       recursive=False, return_absolute_paths=True)
 
         for gvcf in gvcf_file_list:
+            if ".idx" in gvcf:
+                print gvcf
             options += " -V %s" % gvcf
 
         options += " -out %s" % output
