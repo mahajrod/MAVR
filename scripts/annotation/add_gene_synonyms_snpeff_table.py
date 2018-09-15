6defc6c71a23e10d8b182a3ae85e1a03dc309db3
mahajrod <mahajrod@gmail.com>
2018-09-15 03:38:24 +0300

    fixes

---
+++ scripts/annotation/add_gene_synonyms_snpeff_table.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import sys
+import argparse
+
+from Tools.Annotation import SNPeff
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input table")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output table with common name")
+parser.add_argument("-s", "--synonym_file", action="store", dest="synonym_file", required=True,
+                    help="File with synonyms")
+parser.add_argument("-e", "--header_name_for_synonym", action="store", dest="header_name_for_synonym",
+                    default="Common_name",
+                    help="Header name for synonym column")
+parser.add_argument("-k", "--key_index", action="store", dest="key_index", type=int, default=0,
+                    help="Key column in file with synonyms(0-based). Default: 0")
+parser.add_argument("-v", "--value_index", action="store", dest="value_index", type=int, default=1,
+                    help="Value column in file with synonyms(0-based). Default: 1")
+
+args = parser.parse_args()
+
+SNPeff.jar_path = args.snpeff_dir
+SNPeff.max_memory = args.memory
+SNPeff.add_gene_synonyms(args.input, args.output, args.synonym_file,
+                         key_column=args.key_index, value_column=args.value_index,
+                         header_name_for_synonym=args.header_name_for_synonym)
