897e3bd2775348bfb5cd34b2ddc08f81d8ec8d9b
mahajrod <mahajrod@gmail.com>
2018-09-15 05:04:52 +0300

    fixes

--- Tools/Annotation/SNPeff.py
+++ Tools/Annotation/SNPeff.py
@@ -75,20 +75,24 @@ class SNPeff(JavaTool):
                                key_index=key_column,
                                value_index=value_column,
                                comments_prefix="#")
-        print synonym_dict
+        #print synonym_dict
         with open(input_file, "r") as in_fd, open(output_file, "w") as out_fd:
             header = in_fd.readline().strip() + "\t%s\n" % header_name_for_synonym
             out_fd.write(header)
 
             for line in in_fd:
                 tmp = line.strip().split("\t")
-                print tmp
+                #print tmp
                 gene_name = tmp[snpeff_tab_column_id_column]
                 print gene_name
+
                 if gene_name in synonym_dict:
                     print gene_name, synonym_dict[gene_name]
                 tmp.append(synonym_dict[gene_name] if gene_name in synonym_dict else "")
                 out_fd.write("\t".join(tmp) + "\n")
+
+            if "MUSNIGT00040372" in synonym_dict:
+                print "AAAAAAAAAAAAAAAAAAA"
 
 if __name__ == "__main__":
     import os
