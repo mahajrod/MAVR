3a63128deb9de4dca79414dcce843267e52ece74
mahajrod <mahajrod@gmail.com>
2018-09-16 16:48:37 +0300

    fixes

--- scripts/repeat_masking/mask_repeats_for_gene_annotation_from_gff.py
+++ scripts/repeat_masking/mask_repeats_for_gene_annotation_from_gff.py
@@ -15,6 +15,12 @@ parser.add_argument("-g", "--genome_fasta", action="store", dest="genome_fasta",
 parser.add_argument("-p", "--output_prefix", action="store", dest="output_prefix", required=True,
                     help="Prefix for output files")
 
+parser.add_argument("-r", "--retain_unknown", action="store_true", dest="retain_unknown",
+                    help="Retain unknown class of repeats. Default: False")
+parser.add_argument("-e", "--use_extended_set", action="store_true", dest="use_extended_set",
+                    help="Use extended set of repeats (i.e retain also unknow, other and artefact classes). "
+                         "Default: False")
+
 args = parser.parse_args()
 
 output_gff = "%s.gff" % args.output_prefix
@@ -25,5 +31,7 @@ output_fasta = "%s.selected_repeat_classes.masked.fasta" % args.output_prefix
 
 RepeatMasker.convert_rm_out_to_gff(args.input, output_gff, repeat_classes, repeat_families)
 
-RepeatMasker.extract_repeats_used_for_gene_annotation(output_gff, extracted_gff)
+RepeatMasker.extract_repeats_used_for_gene_annotation(output_gff, extracted_gff,
+                                                      use_expanded_set=args.use_extended_set,
+                                                      retain_unknown_repeats=args.retain_unknown)
 MaskFasta.mask(args.genome_fasta, output_fasta, extracted_gff, softmasking=True)
