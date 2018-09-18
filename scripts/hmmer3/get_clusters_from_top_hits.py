9bb1455567025e4fcd5df17796d2b00e6a6af0be
mahajrod <mahajrod@gmail.com>
2018-09-18 10:08:13 +0300

    fixes

---
+++ scripts/hmmer3/get_clusters_from_top_hits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+
+from Tools.HMMER import HMMER3
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input",
+                    help="File with HMMER top hits")
+parser.add_argument("-o", "--output", action="store", dest="output",
+                    help="Output file")
+args = parser.parse_args()
+
+HMMER3.get_families_from_top_hits(args.input, args.output)
