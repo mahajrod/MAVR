a939deea9e13b1d391cf0e564e28508bcd1e8f0b
mahajrod <mahajrod@gmail.com>
2018-09-18 12:43:06 +0300

    fixes

---
+++ scripts/fastq/count_reads_and_bases_in_fastq.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+
+import sys
+import argparse
+
+from Routines import FastQRoutines
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    type=FastQRoutines.make_list_of_path_to_files_from_string,
+                    help="Comma-separated list of fastq files")
+parser.add_argument("-o", "--output", action="store", dest="output",
+                    help="Output file. Default: stdout only")
+
+args = parser.parse_args()
+
+FastQRoutines.count_reads_and_bases(args.input, args.output)
