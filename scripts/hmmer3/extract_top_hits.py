ca8eed9b417c8d727420779702e5f3571080f664
mahajrod <mahajrod@gmail.com>
2018-09-18 07:42:13 +0300

    fixes

---
+++ scripts/hmmer3/extract_top_hits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+
+import argparse
+
+from Tools.HMMER import HMMER3
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="File with hmm hits")
+#parser.add_argument("-f", "--format", action="store", dest="format",
+#                    help="Format of input hmm file.")
+parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix",
+                    help="Prefix of output file")
+parser.add_argument("-a", "--parsing_mode", action="store", dest="parsing_mode", default="index_db",
+                    help="Parsing mode for hmmer hits file. Allowed: parse, index, index_db(default)")
+
+args = parser.parse_args()
+
+HMMER3.extract_top_hits(args.input, args.output_prefix, parsing_mode=args.parsing_mode)
