d1e79d08c8caa3e4775fc1429415324d116e09f8
mahajrod <mahajrod@gmail.com>
2018-09-19 12:26:23 +0300

    fixes

--- Routines/Annotations.py
+++ Routines/Annotations.py
@@ -695,7 +695,7 @@ class AnnotationsRoutines(SequenceRoutines):
                 else: # d <= b
                     pass
                 i += 1
-
+        print unified_dict
         print merged_dict
         if output_file:
             with self.metaopen(output_file, "w") as out_fd:
