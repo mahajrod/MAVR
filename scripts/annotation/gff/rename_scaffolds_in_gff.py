8ff3d2125098a0982f6350bb6e23cbc168a15d49
mahajrod <mahajrod@gmail.com>
2018-09-20 10:53:07 +0300

    fixes

---
+++ scripts/annotation/gff/rename_scaffolds_in_gff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+
+from Routines import AnnotationsRoutines
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_gff", action="store", dest="input_gff", required=True,
+                    help="Input GFF file")
+parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
+                    help="Prefix of output files")
+parser.add_argument("-s", "--syn_file", action="store", dest="syn_file", required=True,
+                    help="File with scaffold synonyms")
+
+args = parser.parse_args()
+
+AnnotationsRoutines.rename_scaffolds_in_gff(args.nput_gff, args.syn_file, args.output_prefix)
