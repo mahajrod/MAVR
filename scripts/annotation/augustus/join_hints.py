920622f02067aaa1474357c18f3941d504fe776c
mahajrod <mahajrod@gmail.com>
2018-09-29 02:49:45 +0300

    fixes

---
+++ scripts/annotation/augustus/join_hints.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import sys
+import argparse
+
+from Tools.Annotation import AUGUSTUS
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Comma-separated list of input files with hints in gff")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file with merged hints")
+
+
+args = parser.parse_args()
+
+AUGUSTUS.join_multiple_hints(args.input, args.output)
