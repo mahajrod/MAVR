6d913203ebd2be05d86132848efcc2321b982329
mahajrod <mahajrod@gmail.com>
2018-10-01 11:38:04 +0300

    fixes

---
+++ scripts/annotation/gff/get_features_from_gff.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+
+from Routines import AnnotationsRoutines
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_gff", action="store", dest="input_gff", required=True,
+                    help="Input .gff file")
+parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
+                    help="Output .gff file")
+
+parser.add_argument("-t", "--feature_types", action="store", dest="feature_types",
+                    type=lambda s: s.split(","), default=["CDS"],
+                    help="Comma-separated list of feature types to extract. "
+                         "Default: all feature types")
+
+parser.add_argument("-u", "--unification_key", action="store",
+                    dest="unification_key",
+                    default="Parent",
+                    help="Annotation entry to use for unification. Default: Parent")
+
+args = parser.parse_args()
+
+AnnotationsRoutines.get_feature_dict(args.input_gff,
+                                     output_prefix=args.output_prefix,
+                                     feature_type_list=args.feature_types,
+                                     unification_key=args.unification_key)
