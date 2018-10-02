4221aafdfcc6791b726c87132a8fe5f182f28834
mahajrod <mahajrod@gmail.com>
2018-10-02 03:10:01 +0300

    fixes

--- scripts/alignment/remove_duplicates_and_poorly_aligned_reads.py
+++ scripts/alignment/remove_duplicates_and_poorly_aligned_reads.py
@@ -13,9 +13,13 @@ parser.add_argument("-q", "--min_map_quality", action="store", dest="min_map_qua
                     help="Minimum mapping quality to retain read. Default: 20")
 parser.add_argument("-o", "--output_bam", action="store", dest="output_bam",
                     help="Output bam. Default: stdout")
+parser.add_argument("-t", "--threads", action="store", dest="threads", default=1, type=int,
+                    help="Number of threads. Default: 1")
 
 args = parser.parse_args()
 
+SamtoolsV1.threads = args.threads
 SamtoolsV1.remove_duplicates_and_poorly_aligned_reads(input_bam=args.input_bam,
                                                       output_bam=args.output_bam,
-                                                      min_mapping_quality=args.min_map_quality)
+                                                      min_mapping_quality=args.min_map_quality,
+                                                      )
