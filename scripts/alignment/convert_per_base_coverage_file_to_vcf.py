137022358f6495f0040ca12b3aa54281dae4646a
mahajrod <mahajrod@gmail.com>
2018-10-03 08:48:27 +0300

    fixes

---
+++ scripts/alignment/convert_per_base_coverage_file_to_vcf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+__author__ = 'mahajrod'
+
+import argparse
+
+from Tools.Bedtools import GenomeCov
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input file with per base coverage")
+parser.add_argument("-s", "--sample_name", action="store", dest="sample_name", required=True,
+                    help="Sample name for vcf file")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output vcf")
+
+args = parser.parse_args()
+
+GenomeCov.convert_per_base_coverage_file_to_vcf(args.input, args.output, args.sample_name)
