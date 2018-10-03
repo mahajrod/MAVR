74666c2c91d948d3ff2bcb524a01cc68d63fbd98
mahajrod <mahajrod@gmail.com>
2018-10-03 04:22:33 +0300

    fixes

--- scripts/alignment/get_bam_coverage_stats.py
+++ scripts/alignment/get_bam_coverage_stats.py
@@ -11,10 +11,15 @@ parser.add_argument("-i", "--input_bam", action="store", dest="input_bam", requi
                     help="Input bam.")
 parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
                     help="Prefix of output files")
-
+parser.add_argument("-n", "--min_coverage", action="store", dest="min_coverage", type=float,
+                    help="Minimum coverage to use. Default - not set")
+parser.add_argument("-m", "--max_coverage", action="store", dest="max_coverage", type=float,
+                    help="Maximum coverage to use. Default - not set")
 
 args = parser.parse_args()
 
 #GenomeCov.threads = args.threads
-GenomeCov.get_bam_coverage_stats(args.input_bam, args.output_prefix, genome_bed=None)
+GenomeCov.get_bam_coverage_stats(args.input_bam, args.output_prefix, genome_bed=None,
+                                 max_coverage=args.max_coverage, min_coverage=args.min_coverage,
+                                 verbose=True)
 
