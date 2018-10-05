e94627d4fd09b183cc7d1aa38a773d02d978d72e
mahajrod <mahajrod@gmail.com>
2018-10-05 20:56:20 +0300

    fixes

--- scripts/annotation/trim_cds_and_remove_terminal_stop_codons.py
+++ scripts/annotation/trim_cds_and_remove_terminal_stop_codons.py
@@ -11,7 +11,8 @@ parser.add_argument("-i", "--input", action="store", dest="input", required=True
                     help="Input fasta with raw cDS")
 parser.add_argument("-s", "--stop_codons", action="store", dest="stop_codons_list", default=["TGA", "TAA", "TAG"],
                     type=lambda s: s.split(","),
-                    help="Comma-separated list of stop codons. Can be set using any case and both RNA and DNA alphabet")
+                    help="Comma-separated list of stop codons. Can be set using any case and both RNA and DNA alphabet."
+                         "Default: TGA, TAA, TAG")
 parser.add_argument("-o", "--output", action="store", dest="output", required=True,
                     help="Output file to write trimmed CDS")
 
