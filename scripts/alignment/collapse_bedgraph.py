5e4ab1b83e3a53316282f539e2c272be912ed347
mahajrod <mahajrod@gmail.com>
2018-10-05 13:43:24 +0300

    fixes

---
+++ scripts/alignment/collapse_bedgraph.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+__author__ = 'mahajrod'
+
+import argparse
+
+from Tools.Bedtools import GenomeCov
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input bedgraph file ")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file")
+
+args = parser.parse_args()
+
+GenomeCov.collapse_bedgraph(args.input, args.output)
