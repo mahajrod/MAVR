496e18ee31e042ebc866b234cb2b439fb6680932
mahajrod <mahajrod@gmail.com>
2018-10-05 14:36:07 +0300

    fixes

---
+++ scripts/alignment/convert_picard_dict_to_syn_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+__author__ = 'mahajrod'
+
+import argparse
+
+from Tools.GATK import FastaAlternateReferenceMaker
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input file with database")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file")
+
+args = parser.parse_args()
+
+FastaAlternateReferenceMaker.convert_picard_dict_to_syn_file(args.input, args.output)
