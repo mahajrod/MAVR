6ec89b1bef9758c4a12edbaa78ff6e336c6ccd8c
mahajrod <mahajrod@gmail.com>
2018-10-09 15:01:41 +0300

    fixes

--- Tools/MultipleAlignment/__init__.py
+++ Tools/MultipleAlignment/__init__.py
@@ -2,6 +2,8 @@ __author__ = 'mahajrod'
 
 from Tools.MultipleAlignment.PRANK import PRANK
 from Tools.MultipleAlignment.MAFFT import MAFFT
+from Tools.MultipleAlignment.GUIDANCE2 import GUIDANCE2
 
 PRANK = PRANK()
 MAFFT = MAFFT()
+GUIDANCE2 = GUIDANCE2()
