bef048dfd8c4425272957573b68f03eb071b9f95
mahajrod <mahajrod@gmail.com>
2018-10-11 21:08:25 +0300

    fixes

--- Tools/Population/PSMC.py
+++ Tools/Population/PSMC.py
@@ -15,7 +15,8 @@ class PSMC(Tool):
     def __init__(self, path="", max_threads=4, max_memory="100G", max_per_thread_memory="5G"):
         Tool.__init__(self, "plink", path=path, max_threads=max_threads, max_memory=max_memory, max_per_thread_memory=max_per_thread_memory)
 
-    def psmc_plot(self, sample_label_list, psmc_list, generation_time, absolute_mutation_rate, plot_grid=False):
+    def psmc_plot(self, sample_label_list, psmc_list, generation_time, absolute_mutation_rate,
+                  output_prefix, plot_grid=False):
         """
         -u FLOAT   absolute mutation rate per nucleotide [2.5e-08]
          -s INT     skip used in data preparation [100]
@@ -43,6 +44,7 @@ class PSMC(Tool):
         options += " -p" # TODO: find sence of this options
         options += " -G" if plot_grid else plot_grid
         options += " -M %s" % ",".join(sample_label_list)
+        options += " %s" % output_prefix
         options += " %s" % " ".join(psmc_list)
 
         self.execute(options=options, cmd="psmc_plot.pl")
