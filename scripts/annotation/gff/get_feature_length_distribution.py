845f344ff698e8a5d82eff680f6164c0cbc99b64
mahajrod <mahajrod@gmail.com>
2018-11-22 15:04:34 +0300

    fixes

--- scripts/annotation/gff/get_feature_length_distribution.py
+++ scripts/annotation/gff/get_feature_length_distribution.py
@@ -12,7 +12,7 @@ parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix
                     help="Output prefix")
 parser.add_argument("-t", "--feature_types", action="store", dest="feature_types",
                     type=lambda s: s.split(","), default=None,
-                    help="Comma-separated list of feature types to add aliases. "
+                    help="Comma-separated list of feature types to add aliases."
                          "Default: all feature types")
 
 args = parser.parse_args()
