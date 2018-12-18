5c676a31c8c489fbafaa548fac3d407838532345
mahajrod <mahajrod@gmail.com>
2018-12-18 22:49:19 +0300

    valgrind added to Tools

--- Tools/LinuxTools/__init__.py
+++ Tools/LinuxTools/__init__.py
@@ -2,7 +2,9 @@
 
 from Tools.LinuxTools.CGAS import CGAS
 from Tools.LinuxTools.Download import *
+from Tools.LinuxTools.Valgrind import Valgrind
 
 Axel = Axel()
 Wget = Wget()
 CGAS = CGAS()
+Valgrind = Valgrind()
