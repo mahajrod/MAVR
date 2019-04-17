f07adc4a54663c2915ceff90c8c9dd6700465597
mahajrod <mahajrod@gmail.com>
2019-04-17 14:48:49 +0700

    fixes

--- scripts/wga/align_by_lastal.py
+++ scripts/wga/align_by_lastal.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'Sergei F. Kliver'
 import argparse
-from RouToolPa.Tools.Alignment import LAST
+from RouToolPa.Tools.WGA import LAST
 
 
 parser = argparse.ArgumentParser()
