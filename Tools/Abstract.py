23d2c3adc6879f70233a5ed70353bfad8573a54e
mahajrod <mahajrod@gmail.com>
2019-04-17 13:56:24 +0700

    fixes

--- Tools/Abstract.py
+++ Tools/Abstract.py
@@ -21,7 +21,7 @@ def execute(exe_string):
     print_mutex.release()
 
     #os.system(exe_string)
-    subprocess.call(["/usr/bash", exe_string])
+    subprocess.call(["/usr/bash", '-c', exe_string])
 
 
 class Tool(SequenceRoutines, AlignmentRoutines):
