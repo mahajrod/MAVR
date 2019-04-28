177ae83a6e175fdcd563066098218f8799331f20
mahajrod <mahajrod@gmail.com>
2019-04-28 15:32:54 +0300

    fixes

---
+++ scripts/snpcall/index_variants.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+from RouToolPa.Tools.GATK import ValidateVariants
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_vcf", action="store", dest="input_vcf", required=True,
+                    help="Index variants")
+parser.add_argument("-r", "--reference", action="store", dest="reference", required=True,
+                    help="Fasta with reference genome")
+parser.add_argument("-c", "--gvcf_input", action="store_true", dest="gvcf_input", default=False,
+                    help="Input is gvcf")
+parser.add_argument("-g", "--gatk_directory", action="store", dest="gatk_dir", default="",
+                    help="Directory with GATK jar")
+
+args = parser.parse_args()
+
+
+ValidateVariants.jar_path = args.gatk_dir
+ValidateVariants.index_vcf(args.reference, args.input_vcf, input_is_gvcf=args.gvcf_input)
