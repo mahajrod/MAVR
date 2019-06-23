b665363671db1d9a9744bfcb253824997406f596
mahajrod <mahajrod@gmail.com>
2019-06-23 13:30:23 +0300

    fixes

--- scripts/snpcall/parallel_gvcf_call.py
+++ scripts/snpcall/parallel_gvcf_call.py
@@ -24,10 +24,10 @@ parser.add_argument("-q", "--variant_emit_quality", action="store", dest="emit_q
                     help="Minimum quality of variant to be emitted. Default: 30")
 parser.add_argument("-m", "--memory", action="store", dest="memory", default="10g",
                     help="Maximum memory to use. Default: 10g")
-parser.add_argument("-x", "--max_seq_per_region", action="store", dest="max_seq_per_region", default=100, type=int,
+parser.add_argument("-x", "--max_seq_per_region", action="store", dest="max_seq_per_region", default=200, type=int,
                     help="Maximum sequences per region. Default: 100")
-parser.add_argument("-l", "--max_region_len", action="store", dest="max_region_len", default=200000, type=int,
-                    help="Maximum region length. Default: 200000")
+parser.add_argument("-l", "--max_region_len", action="store", dest="max_region_len", default=2000000, type=int,
+                    help="Maximum region length. Default: 2000000")
 
 parser.add_argument("-d", "--handling_mode", action="store", dest="handling_mode", default="local",
                     help="Handling mode. Allowed: local(default), slurm")
