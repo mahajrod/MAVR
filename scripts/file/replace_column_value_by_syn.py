3c5581a87f9b15e40b6c32cb6a667852728a5839
mahajrod <mahajrod@gmail.com>
2019-08-15 12:13:29 +0700

    fixes

--- scripts/file/replace_column_value_by_syn.py
+++ scripts/file/replace_column_value_by_syn.py
@@ -11,6 +11,8 @@ parser.add_argument("-i", "--input", action="store", dest="input", required=True
                     help="Input file")
 parser.add_argument("-c", "--column_number", action="store", dest="column_number", type=int, required=True,
                     help="Column number with values to be replaced(0-based)")
+parser.add_argument("-m", "--comment_prefix", action="store", dest="comment_prefix", default="#",
+                    help="Prefix of comment lines. Default: #")
 parser.add_argument("-s", "--synonym_file", action="store", dest="syn_file", required=True,
                     help="Synonym file")
 parser.add_argument("-e", "--separator", action="store", dest="separator", default="\t",
@@ -21,7 +23,6 @@ parser.add_argument("-o", "--output", action="store", dest="output", default=Non
 args = parser.parse_args()
 
 FileRoutines.replace_column_value_by_syn(args.input, args.syn_file, args.output, column=args.column_number,
-                                         comment_prefix=None, separator=args.separator,
+                                         comment_prefix=args.comment_prefix, separator=args.separator,
                                          syn_header=False, syn_separator="\t",
                                          syn_key_index=0, syn_value_index=1, syn_comment_prefix=None)
-
