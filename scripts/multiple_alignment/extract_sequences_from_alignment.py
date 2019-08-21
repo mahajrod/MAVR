140eb93ca817feb27c3b3de1edd0cbf90b63d2d5
mahajrod <mahajrod@gmail.com>
2019-08-21 14:37:08 +0700

    fixes

--- scripts/multiple_alignment/extract_sequences_from_alignment.py
+++ scripts/multiple_alignment/extract_sequences_from_alignment.py
@@ -7,16 +7,15 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input", action="store", dest="input", required=True,
                     help="Input file with alignment")
-
 parser.add_argument("-o", "--output", action="store", dest="output", required=True,
                     help="Output file")
-
 parser.add_argument("-f", "--format", action="store", dest="format", default="fasta",
                     help="Format of the input alignment. Default: fasta")
 parser.add_argument("-a", "--output_format", action="store", dest="output_format", default="fasta",
                     help="Format of the output sequences. Default: fasta")
 parser.add_argument("-g", "--gap_symbol", action="store", dest="gap_symbol", default="-",
                     help="Gap symbol. Default: -")
+
 args = parser.parse_args()
 
 MultipleAlignmentRoutines.extract_sequences_from_alignment(args.input,
