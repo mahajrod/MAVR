a0a445afb8341331f6fcad608cbae2e91e5d4a4b
mahajrod <mahajrod@gmail.com>
2019-09-10 14:12:56 +0700

    fixes

--- scripts/alignment/collapse_per_base_covarage_mask.py
+++ scripts/alignment/collapse_per_base_covarage_mask.py
@@ -18,10 +18,14 @@ parser.add_argument("--scaf_col", action="store", dest="scaf_col", type=int,
 parser.add_argument("--pos_col", action="store", dest="pos_col", type=int,
                     default=1,
                     help="Index of position column in coverage file (0-based). Default: 1")
-
+parser.add_argument("-z", "--0-based", action="store_true", dest="zero_based_output",
+                    help="Write output in 0-based format and python notation, "
+                         "i.e last position is not included in the interval."
+                         "Required for bed format. Default: False")
 args = parser.parse_args()
 
 AlignmentRoutines.collapse_per_base_coverage_mask(args.mask_file, args.output,
                                                   scaffold_column=args.scaf_col,
                                                   position_column=args.pos_col,
+                                                  zero_based_output=args.zero_based_output,
                                                   comments_prefix="#")
