aac57b0ed56b00207bea58cbed5d16e83c1902e5
mahajrod <mahajrod@gmail.com>
2019-09-10 13:06:59 +0700

    fixes

---
+++ scripts/alignment/collapse_per_base_covarage_mask.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import argparse
+from RouToolPa.Routines import AlignmentRoutines
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-m", "--mask_file", action="store", dest="mask_file", required=True,
+                    help="File with per base mask")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file with collapsed(from bases to regions) mask")
+
+parser.add_argument("--scaf_col", action="store", dest="scaf_col", type=int,
+                    default=0,
+                    help="Index of scaffold column in coverage file (0-based). Default: 0")
+parser.add_argument("--pos_col", action="store", dest="pos_col", type=int,
+                    default=1,
+                    help="Index of position column in coverage file (0-based). Default: 1")
+
+args = parser.parse_args()
+
+AlignmentRoutines.collapse_per_base_coverage_mask(args.mask_file, args.output,
+                                                  scaffold_column=args.scaf_col,
+                                                  position_column=args.pos_col,
+                                                  comments_prefix="#")
