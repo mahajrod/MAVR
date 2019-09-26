ad3cc801b1fdadf0418d1b78eb6aa4d1e637c7ca
mahajrod <mahajrod@gmail.com>
2019-09-26 11:08:25 +0700

    fixes

--- Pipelines/__init__.py
+++ Pipelines/__init__.py
@@ -1,6 +1,7 @@
 from Pipelines.Sanger import SangerPipeline
 from Pipelines.SNPCall import SNPCallPipeline
 from Pipelines.Primers import STRPrimerPipeline
+from Pipelines.Primers import MitochondrialAmplificationPrimerPipeline
 from Pipelines.Abstract import Pipeline
 from Pipelines.Alignment import AlignmentPipeline
 from Pipelines.Filtering import FilteringPipeline
@@ -13,3 +14,4 @@ AlignmentPipeline = AlignmentPipeline()
 FilteringPipeline = FilteringPipeline()
 STRPrimerPipeline = STRPrimerPipeline()
 DiffExpressionPipeline = DiffExpressionPipeline()
+MitochondrialAmplificationPrimerPipeline = MitochondrialAmplificationPrimerPipeline()
