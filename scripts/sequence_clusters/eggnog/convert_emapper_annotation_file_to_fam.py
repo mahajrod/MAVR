23df69f9722981f269a36ea3bfafbc6543c0934c
mahajrod <mahajrod@gmail.com>
2019-12-05 12:54:07 +0700

    fixes

--- scripts/sequence_clusters/eggnog/convert_emapper_annotation_file_to_fam.py
+++ scripts/sequence_clusters/eggnog/convert_emapper_annotation_file_to_fam.py
@@ -14,9 +14,14 @@ parser.add_argument("-p", "--eggnogdb_prefix", action="store", dest="eggnogdb_pr
                     help="EggNOG database prefix for clusters. Default: None")
 parser.add_argument("-s", "--species_name", action="store", dest="species_name",
                     help="Species name to use for labeling of proteins. Default: not set")
-
-
+parser.add_argument("-d", "--diamond", action="store_true", dest="diamond",
+                    help="Annotation file was produced in diamond mode. Default: False")
+parser.add_argument("-db", action="store_true", dest="db",
+                    help="Name of database used for annotation. Required in diamond mode, ignored in hmmer mode. "
+                         "Default: not set")
 args = parser.parse_args()
 
 Emapper.convert_emapper_annotation_file_to_fam(args.input, args.output, eggnogdb_prefix=args.eggnogdb_prefix,
-                                               species_name=None, label_separator=args.species_name)
+                                               species_name=None, label_separator=args.species_name,
+                                               diamond_mode=args.diamond, database=args.db
+                                               )
