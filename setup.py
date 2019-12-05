4420135b53255a4ec1e78c6eecf9b15c422672db
mahajrod <mahajrod@gmail.com>
2019-12-05 15:24:32 +0700

    fixes

--- setup.py
+++ setup.py
@@ -33,13 +33,13 @@ dependencies = ['scipy', 'numpy', 'pandas', 'matplotlib', 'matplotlib-venn',
                 'biopython', 'xmltodict', 'bcbio-gff', 'statsmodels']
 if sys.version_info[0] == 3:
     dependencies += ["ete3"]
-elif sys.version_info[0] == 3:
+elif sys.version_info[0] == 2:
     dependencies += ["ete2"]
 else:
     raise ValueError("ERROR!!! Unsupported python version: %s" % str(sys.version_info[0]))
 
 setup(name='MAVR',
-      version='0.58',
+      version='0.93',
       packages=find_packages(),
       author='Sergei F. Kliver',
       author_email='mahajrod@gmail.com',
