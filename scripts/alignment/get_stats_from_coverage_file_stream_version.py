fe809277c03d22ce623a4c2ec1d5a8c52df74855
mahajrod <mahajrod@gmail.com>
2019-12-11 05:52:59 +0700

    fixes

--- scripts/alignment/get_stats_from_coverage_file_stream_version.py
+++ scripts/alignment/get_stats_from_coverage_file_stream_version.py
@@ -14,9 +14,12 @@ parser.add_argument("-s", "--scaffold_column", action="store", dest="scaffold_co
                     help="Column(0-based) with scaffold ids. Default: 0")
 parser.add_argument("-c", "--coverage_column", action="store", dest="coverage_column", default=1, type=int,
                     help="Column(0-based) with per base coverage values. Default: 1")
+parser.add_argument("-b", "--buffer_size", action="store", dest="buffer_size", default=100000000, type=int,
+                    help="Buffer size in bytes for reading file. Default: 100000000")
 args = parser.parse_args()
 
 GenomeCov.get_stats_from_coverage_file_stream_version(args.input, args.output, verbose=True,
                                                       scaffold_column=args.scaffold_column,
                                                       coverage_column=args.coverage_column,
-                                                      separator="\t")
+                                                      separator="\t",
+                                                      buffering=args.buffer_size)
