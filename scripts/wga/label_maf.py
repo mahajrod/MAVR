d498583d1de7f284faf379be0e3a450857570fb5
mahajrod <mahajrod@gmail.com>
2019-12-14 16:42:08 +0700

    fixes

---
+++ scripts/wga/label_maf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+from RouToolPa.Routines.WGA import WGARoutines
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input MAF file")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output MAF file")
+parser.add_argument("-l", "--label_list", action="store", dest="label_list", required=True,
+                    type=WGARoutines.split_string_by_comma,
+                    help="Comma-separated list of labels for species from MAF file")
+parser.add_argument("-s", "--separator", action="store", dest="separator", default=".",
+                    help="Separator between label and sequence id ")
+
+args = parser.parse_args()
+
+WGARoutines.label_maf(args.input, args.output, args.label_list, args.separator)
