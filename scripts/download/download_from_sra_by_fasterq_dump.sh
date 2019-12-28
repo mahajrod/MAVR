ce80ba08431351ee50a98430202ed88d0dcac011
mahajrod <mahajrod@gmail.com>
2019-12-28 08:30:32 +0700

    fixes

--- scripts/download/download_from_sra_by_fasterq_dump.sh
+++ scripts/download/download_from_sra_by_fasterq_dump.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 #TODO: adjust script. In fact it is a template
 for SP in $1; do mkdir -p ${SP}; cat ${SP}*ids | xargs -P $2 -I SRA fasterq-dump --progress --mem 10000MB -O ${SP} SRA & done
+
