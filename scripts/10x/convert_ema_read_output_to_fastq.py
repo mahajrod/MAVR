fa8001f0328241fb62ebbbc33fd431f47c0d8104
mahajrod <mahajrod@gmail.com>
2020-01-07 14:37:47 +0700

    fixes

---
+++ scripts/10x/convert_ema_read_output_to_fastq.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import argparse
+from RouToolPa.Tools.Alignment import EMA
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True, default=sys.stdin,
+                    help="Input file with reads in EMA format. Default: stdin")
+parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
+                    help="Prefix of output files")
+
+args = parser.parse_args()
+
+EMA.convert_ema_read_format_to_fastq(args.input, args.output_prefix, bufferig=1000000000)
