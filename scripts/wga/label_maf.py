c9ba2325b8ea48ff56ed74faa596f217a550274e
mahajrod <mahajrod@gmail.com>
2020-02-01 17:41:56 +0700

    fixes

--- scripts/wga/label_maf.py
+++ scripts/wga/label_maf.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 __author__ = 'Sergei F. Kliver'
+import sys
 import argparse
 from RouToolPa.Routines import WGARoutines
 
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+parser.add_argument("-i", "--input", action="store", dest="input", default=sys.stdin,
                     help="Input MAF file")
-parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+parser.add_argument("-o", "--output", action="store", dest="output", default=sys.stdout,
                     help="Output MAF file")
 parser.add_argument("-l", "--label_list", action="store", dest="label_list", required=True,
                     type=lambda s: s.split(","),
