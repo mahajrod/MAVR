b8f5621a3b5827a29e506908f808f6a77a942ec2
mahajrod <mahajrod@gmail.com>
2020-02-14 20:53:45 +0700

    fixes

--- scripts/annotation/gff/extract_annotations_from_gff.py
+++ scripts/annotation/gff/extract_annotations_from_gff.py
@@ -26,6 +26,6 @@ args = parser.parse_args()
 feature_ids = args.feature_ids.split(",") if args.feature_ids else IdList(filename=args.feature_id_file)
 
 AnnotationsRoutines.extract_annotation_from_gff(args.input_gff,
-                                                args.feature_ids,
+                                                feature_ids,
                                                 args.annotation_types,
                                                 args.output_gff)
