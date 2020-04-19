42943d93843b4b6d143f5205d27f144c0ae1ef5f
mahajrod <zmeenosez72>
2020-04-19 14:50:08 +0300

    fixes

---
+++ scripts/assembly/10x/generate_fasta_from_supernova_output.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import os
+import argparse
+import matplotlib
+
+from RouToolPa.Tools.Assemblers import Supernova
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-a", "--assembly_dir", action="store", dest="assembly_dir", required=True,
+                    help="Directory with Supernova output")
+parser.add_argument("-m", "--min_length", action="store", dest="min_length", default=1000, type=int,
+                    help="Minimum length of scaffold to output. Default: 1000")
+parser.add_argument("-e", "--header_style", action="store", dest="header_style", default="full",
+                    help="Fasta header style to use. Allowed: full(default), short")
+parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
+                    help="Output prefix")
+
+args = parser.parse_args()
+
+Supernova.generate_fasta(args.assembly_dir, args.out_prefix, min_length=args.min_length, header_style=args.header_style)
