91514fc9ff836f5070f1b3f211875bbb702e69c4
mahajrod <zmeenosez72>
2020-05-03 14:06:05 +0300

    fixes

--- scripts/sequence/get_sequence_lengths.py
+++ scripts/sequence/get_sequence_lengths.py
@@ -4,7 +4,7 @@ import os
 import sys
 import argparse
 from RouToolPa.Parsers.Sequence import CollectionSequence
-
+from RouToolPa.Routines import FileRoutines
 
 parser = argparse.ArgumentParser()
 
@@ -12,24 +12,21 @@ parser.add_argument("-f", "--format", action="store", dest="format",
                     default="fasta", help="format of file with sequences - default: fasta.")
 parser.add_argument("-i", "--input", action="store", dest="input", required=True,
                     help="Input file with sequences")
-parser.add_argument("-o", "--output", action="store", dest="output", default="out.t",
-                    help="Output file - default: out.t.")
-parser.add_argument("-m", "--mode", action="store", dest="mode", default="parse",
-                    help="Parsing mode. Allowed: parse(default), index, index_db")
+parser.add_argument("-o", "--output", action="store", dest="output", default=sys.stdout,
+                    type=lambda s: FileRoutines.metaopen(s, "w"),
+                    help="Output file - default: stdout")
 
 args = parser.parse_args()
 
-out_fd = sys.stdout if args.output == "stdout" else open(args.output, "w")
-
 coll_seq = CollectionSequence(in_file=args.input, parsing_mode="generator", get_stats=True)
-coll_seq.seq_lengths.to_csv(args.output, header=False)
+coll_seq.seq_lengths.to_csv(args.output, header=False, sep="\t")
 
 #record_dict = SequenceRoutines.parse_seq_file(args.input, args.mode, format=args.format, index_file="temp_index.idx")
 #lengths_dict = SequenceRoutines.get_lengths(record_dict, out_file=out_fd)
 
-print("Longest sequence: %i" % max(coll_seq.seq_lengths["length"]))
-print("Shortest sequence: %i" % min(coll_seq.seq_lengths["length"]))
-print("Total length: %i" % sum(coll_seq.seq_lengths["length"]))
+sys.stderr.write("Longest sequence: %i" % max(coll_seq.seq_lengths["length"]))
+sys.stderr.write("Shortest sequence: %i" % min(coll_seq.seq_lengths["length"]))
+sys.stderr.write("Total length: %i" % sum(coll_seq.seq_lengths["length"]))
 
 #if args.mode == "index_db":
 #    os.remove("temp_index.idx")
