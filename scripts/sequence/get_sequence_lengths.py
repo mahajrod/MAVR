9527469e900fc58240242500fe083c3270c18eff
mahajrod <zmeenosez72>
2020-05-04 14:08:24 +0300

    fixes

--- scripts/sequence/get_sequence_lengths.py
+++ scripts/sequence/get_sequence_lengths.py
@@ -21,12 +21,6 @@ args = parser.parse_args()
 coll_seq = CollectionSequence(in_file=args.input, parsing_mode="generator", get_stats=True)
 coll_seq.seq_lengths.to_csv(args.output, header=False, sep="\t")
 
-#record_dict = SequenceRoutines.parse_seq_file(args.input, args.mode, format=args.format, index_file="temp_index.idx")
-#lengths_dict = SequenceRoutines.get_lengths(record_dict, out_file=out_fd)
-
-sys.stderr.write("Longest sequence: %i" % max(coll_seq.seq_lengths["length"]))
-sys.stderr.write("Shortest sequence: %i" % min(coll_seq.seq_lengths["length"]))
-sys.stderr.write("Total length: %i" % sum(coll_seq.seq_lengths["length"]))
-
-#if args.mode == "index_db":
-#    os.remove("temp_index.idx")
+sys.stderr.write("Longest sequence: %i\n" % max(coll_seq.seq_lengths["length"]))
+sys.stderr.write("Shortest sequence: %i\n" % min(coll_seq.seq_lengths["length"]))
+sys.stderr.write("Total length: %i\n" % sum(coll_seq.seq_lengths["length"]))
