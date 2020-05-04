be9224a6d8e9a2ea8ca35c06bf1027d0ff85bcc9
mahajrod <zmeenosez72>
2020-05-04 14:08:08 +0300

    fixes

---
+++ scripts/alignment/get_windows_stats_from_coverage_file_stream_version.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+__author__ = 'mahajrod'
+
+import argparse
+from RouToolPa.Tools.Bedtools import GenomeCov
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Coverage file")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file")
+parser.add_argument("-w", "--window_size", action="store", dest="window_size", default=100000, type=int,
+                    help="Size of windows. Default: 100 000")
+parser.add_argument("-s", "--window_step", action="store", dest="window_step", default=None, type=int,
+                    help="Step of windows. Default: window size")
+parser.add_argument("-b", "--buffer_size", action="store", dest="buffer_size", default=10000000, type=int,
+                    help="Buffer size in bytes for reading file. Default: 10 000 000")
+args = parser.parse_args()
+
+GenomeCov.get_coverage_stats_in_windows(args.input, args.window_size, args.output, window_step=args.window_step,
+                                        buffering=args.buffer_size)
