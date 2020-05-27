130dc64231642c5535a3e2f691cedf8c691e65b8
mahajrod <zmeenosez72>
2020-05-27 10:15:11 +0300

    fixes

--- scripts/alignment/bowtie2/gather_stats_from_bowtie2_stat.py
+++ scripts/alignment/bowtie2/gather_stats_from_bowtie2_stat.py
@@ -6,9 +6,9 @@ from RouToolPa.Parsers.Bowtie2 import Bowtie2Table
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--input", action="store", dest="input", required=True, type=lambda s: s.split("\t"),
+parser.add_argument("-i", "--input", action="store", dest="input", required=True, type=lambda s: s.split(","),
                     help="Comma-separated list of files with stats reporterd by Bowtie2")
-parser.add_argument("-s", "--samples", action="store", dest="samples", required=True, type=lambda s: s.split("\t"),
+parser.add_argument("-s", "--samples", action="store", dest="samples", required=True, type=lambda s: s.split(","),
                     help="Comma-separated list of samples")
 parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
                     help="Prefix of output files")
