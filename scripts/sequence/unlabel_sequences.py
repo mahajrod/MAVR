476fa3dc448bdbda9f5df63fbc96d6ab6645c755
mahajrod <zmeenosez72>
2020-06-02 00:00:32 +0300

    fixes

--- scripts/sequence/unlabel_sequences.py
+++ scripts/sequence/unlabel_sequences.py
@@ -8,6 +8,8 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input", action="store", dest="input", required=True,
                     help="Input file with sequences")
+parser.add_argument("-l", "--label", action="store", dest="label", required=True,
+                    help="Label to use")
 parser.add_argument("-s", "--separator", action="store", dest="separator", default="@",
                     help="Separator to use. default - '@'")
 parser.add_argument("-o", "--output", action="store", dest="output", default="stdout",
@@ -18,8 +20,8 @@ args = parser.parse_args()
 out_file = "" if args.output == "stdout" else " > %s" % args.output
 
 if args.input[-3:] == ".gz":
-    sed_string = "zcat %s | sed 's/^>.*%s/>/'  %s" % (args.input, args.separator, out_file)
+    sed_string = "zcat %s | sed 's/^>%s%s/>/'  %s" % (args.input, args.label, args.separator, out_file)
 else:
-    sed_string = "sed 's/^>.*%s/>/' %s %s" % (args.separator, args.input, out_file)
+    sed_string = "sed 's/^>%s%s/>/' %s %s" % (args.label, args.separator, args.input, out_file)
 
 os.system(sed_string)
