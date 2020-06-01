dcc2a0354942d3d7a99b52a882fa032eef073f99
mahajrod <zmeenosez72>
2020-06-01 23:57:51 +0300

    fixes

--- scripts/sequence/unlabel_sequences.py
+++ scripts/sequence/unlabel_sequences.py
@@ -8,8 +8,6 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input", action="store", dest="input", required=True,
                     help="Input file with sequences")
-parser.add_argument("-l", "--label", action="store", dest="label", required=True,
-                    help="Label to use")
 parser.add_argument("-s", "--separator", action="store", dest="separator", default="@",
                     help="Separator to use. default - '@'")
 parser.add_argument("-o", "--output", action="store", dest="output", default="stdout",
