83842e726a8351ad3757f6437efda252b6e5034f
mahajrod <zmeenosez72>
2020-09-16 23:02:34 +0300

    fixes

--- scripts/sequence/prepare_region_list.py
+++ scripts/sequence/prepare_region_list.py
@@ -23,7 +23,8 @@ parser.add_argument("-b", "--scaffold_black_list_file", action="store", dest="sc
                     help="File with scaffolds from black list")
 parser.add_argument("-x", "--min_scaffold_len", action="store", dest="min_scaffold_len", type=int, default=None,
                     help="Minimum length of scaffold to be included in regions. Default: not set")
-
+parser.add_argument("-g", "--region_file_format", action="store", dest="region_file_format", default='simple',
+                    help="Output region file format. Allowed: 'simple' (default), 'GATK'")
 args = parser.parse_args()
 
 
@@ -35,6 +36,7 @@ SequenceRoutines.prepare_region_list_by_length(max_length=args.max_length,
                                                output_dir=args.output_dir,
                                                split_scaffolds=args.split_scaffolds,
                                                min_scaffold_length=args.min_scaffold_len,
-                                               black_list_scaffolds=None)
+                                               black_list_scaffolds=None,
+                                               region_file_format=args.region_file_format)
 
 
