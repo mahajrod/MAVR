9e6d0a8f0415179d0e694fbe008e181f74a6c4e8
mahajrod <zmeenosez72>
2020-11-05 00:52:45 +0300

    fixes

--- scripts/haplotypes/prepare_template_for_popart.py
+++ scripts/haplotypes/prepare_template_for_popart.py
@@ -8,12 +8,19 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-a", "--alignment", action="store", dest="aln_file", required=True,
                     help="Input file with alignment in fasta format")
-parser.add_argument("-y", "--hap_file", action="store", dest="hap_file", required=True,
+parser.add_argument("-y", "--hap_file", action="store", dest="hap_file",
                     help="Fam file with haplotypes")
 parser.add_argument("-o", "--output", action="store", dest="output", required=True,
                     help="Output file")
-
+parser.add_argument("-t", "--traits_file", action="store", dest="traits_file",
+                    help="Tab-separated file with traits for each haplotype")
+parser.add_argument("-w", "--whitelist_file", action="store", dest="whitelist_file",
+                    help="File with ids from whitelist. Default: not set")
 
 args = parser.parse_args()
 
-HaplotypeRoutines.prepare_template_for_popart(args.aln_file, args.hap_file, args.output)
+HaplotypeRoutines.prepare_template_for_popart(args.aln_file,
+                                              args.output,
+                                              haplotype_fam_file=args.hap_file,
+                                              traits_file=args.traits_file,
+                                              whitelist_file=args.whitelist_file)
