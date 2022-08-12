d8882f37908541c77f2973b095797723df24b472
mahajrod <mahajrod@gmail.com>
2022-08-12 23:13:33 +0300

    added scripts for manipulations with purge_dups output

---
+++ scripts/assembly/purge_dups/add_lengths_to_dups_bed.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import argparse
+
+from RouToolPa.Tools.AsseblyQC import PurgeDups
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", required=True,
+                    help="Input file with purge_dups coverage estimations. Usually it is 'TX.base.cov'")
+parser.add_argument("-l", "--length_file", action="store", dest="length_file", required=True,
+                    help="File with scaffold_lengths")
+parser.add_argument("-o", "--output", action="store", dest="output", required=True,
+                    help="Output file")
+args = parser.parse_args()
+
+PurgeDups.add_lengths_to_dups_bed(args.input, args.length_file, args.output)
