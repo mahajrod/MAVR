d4c2772c7256edd69b76d5bf4d8a448f5099b4d6
mahajrod <mahajrod@gmail.com>
2023-04-26 08:03:48 +0200

    added script to calculate coverage mask from mosdepth output.

--- scripts/filter/extract_seq_ids_by_taxa_from_kraken_report.py
+++ scripts/filter/extract_seq_ids_by_taxa_from_kraken_report.py
@@ -15,8 +15,6 @@ parser.add_argument("-k", "--kraken_output", action="store", dest="kraken_output
 parser.add_argument("-t", "--taxon_id_list", action="store", dest="taxon_id_list", required=True,
                     type=lambda s: pd.read_csv(s, header=None).squeeze("columns") if os.path.exists(s) else pd.Series(s.split(",")),
                     help="Comma-separated list of taxon ids")
-parser.add_argument("-c", "--check_read_id", action="store_true", dest="check_read_id", default=False,
-                    help="Check correspondence of read ids in read files and KRAKEN output")
 parser.add_argument("-o", "--output", action="store", dest="output", default=sys.stdout,
                     help="Output file to write extracted sequences. Default: stdout")
 
