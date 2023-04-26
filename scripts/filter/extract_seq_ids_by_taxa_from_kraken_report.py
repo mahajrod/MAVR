f2db813313eda4d685b5acbe030465bece4c2070
mahajrod <mahajrod@gmail.com>
2023-04-26 07:51:34 +0200

    added script to calculate coverage mask from mosdepth output.

--- scripts/filter/extract_seq_ids_by_taxa_from_kraken_report.py
+++ scripts/filter/extract_seq_ids_by_taxa_from_kraken_report.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 __author__ = 'Sergei F. Kliver'
 import os
-import pd
+import pandas as pd
+import sys
 import argparse
 from RouToolPa.Routines import FilteringRoutines
 
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-f", "--forward_fastq", action="store", dest="forward_fastq", required=True,
-                    help="Forward fastq file. Might be gzipped.")
-parser.add_argument("-r", "--reverse_fastq", action="store", dest="reverse_fastq",
-                    help="Reverse fastq file. Might be gzipped.")
+
 parser.add_argument("-k", "--kraken_output", action="store", dest="kraken_output", required=True,
                     help="File with kraken output. Might be gzipped")
 parser.add_argument("-t", "--taxon_id_list", action="store", dest="taxon_id_list", required=True,
@@ -19,12 +17,10 @@ parser.add_argument("-t", "--taxon_id_list", action="store", dest="taxon_id_list
                     help="Comma-separated list of taxon ids")
 parser.add_argument("-c", "--check_read_id", action="store_true", dest="check_read_id", default=False,
                     help="Check correspondence of read ids in read files and KRAKEN output")
-parser.add_argument("-o", "--output_prefix", action="store", dest="output_prefix", required=True,
-                    help="Prefix of output files")
+parser.add_argument("-o", "--output", action="store", dest="output", default=sys.stdout,
+                    help="Output file to write extracted sequences. Default: stdout")
 
 
 args = parser.parse_args()
 
-FilteringRoutines.extract_reads_by_kraken_report(args.forward_fastq, args.kraken_output, args.taxon_id_list,
-                                                 args.output_prefix, reverse_fastq=args.reverse_fastq, gzip=True,
-                                                 check_read_id=args.check_read_id)
+FilteringRoutines.extract_seq_ids_by_taxa_from_kraken_report(args.kraken_output, args.taxon_id_list, args.output)
