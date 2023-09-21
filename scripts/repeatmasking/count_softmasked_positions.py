cc0472dab6442b0a49ce6a795b88478fd1e85ba6
mahajrod <mahajrod@gmail.com>
2023-09-21 17:16:44 +0200

    fixes

---
+++ scripts/repeatmasking/count_softmasked_positions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import argparse
+from RouToolPa.Parsers.Sequence import CollectionSequence
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input_fasta", action="store", dest="input_fasta", default=sys.stdin,
+                    help="Input fasta file with sequences. Default: stdin")
+parser.add_argument("-o", "--output", action="store", dest="input_fasta", default=None,
+                    help="Output file with softmasked position counts per input sequence."
+                         " If output file is not stdout, a total count will be printed. "
+                         " Default: stdout")
+args = parser.parse_args()
+
+
+CollectionSequence(in_file=args.input_fasta).count_masked_positions(out_file=sys.stdout if args.output is None else args.output,
+                                                                    verbose=True if args.output is not None else False)
