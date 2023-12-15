a73ab34ee0295e5357f1e65f7879adc7c52b375c
mahajrod <mahajrod@gmail.com>
2023-12-15 18:41:22 +0100

    added script tp convert assembly file to contig bed file

---
+++ scripts/annotation/TOGA/filter_query_annotation_by_transcript_ids.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+__author__ = 'Sergei F. Kliver'
+import sys
+import bz2
+import gzip
+import argparse
+
+import pandas as pd
+
+if sys.version_info[0] == 3:
+    from io import TextIOWrapper as file
+
+
+def metaopen(filename, flags, buffering=None, compresslevel=5):
+        if not isinstance(filename, str): # or isinstance(filename, gzip.GzipFile) or isinstance(filename, bz2.BZ2File):
+            if isinstance(filename, file):
+                return filename
+            else:
+                raise ValueError("ERROR!!! Not file object or str: {}".format(str(filename)))
+        elif filename[-3:] == ".gz":
+            return gzip.open(filename, flags + ("t" if "b" not in flags else ""), compresslevel=compresslevel)
+        elif filename[-4:] == ".bz2":
+            return bz2.open(filename, flags + ("t" if "b" not in flags else ""), compresslevel=compresslevel)
+        else:
+            if buffering is not None:
+                return open(filename, flags, buffering=buffering)
+            else:
+                return open(filename, flags)
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", action="store", dest="input", default=sys.stdin,
+                    help="Input bed12 file with query_annotations (query_annotations.bed). Default: stdin")
+parser.add_argument("-d", "--transcript_ids", action="store", dest="transcript_ids", required=True,
+                    help="File with transcript ids extracted from query_isoforms.tsv")
+parser.add_argument("-o", "--output", action="store", dest="output", default=sys.stdout,
+                    help="Output filtered bed12 file. Default: stdout")
+
+args = parser.parse_args()
+
+transcript_set = set(pd.read_csv(args.transcript_ids, sep="\t", header=None))
+
+with metaopen(args.input, "r") as in_fd, metaopen(args.output, "w") as out_fd:
+    for line in in_fd:
+        if line.split("\t")[3] in transcript_set:
+            out_fd.write(line)
+
