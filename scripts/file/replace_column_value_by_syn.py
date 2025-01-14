6154bb5c697fdfdf4ae4a27e164e4c368e21cf02
mahajrod <mahajrod@gmail.com>
2025-01-15 00:46:31 +0100

    Added selection of key and  value columns to replace_column_value_by_syn.py

--- scripts/file/replace_column_value_by_syn.py
+++ scripts/file/replace_column_value_by_syn.py
@@ -15,6 +15,10 @@ parser.add_argument("-s", "--synonym_file", action="store", dest="syn_file", req
                     help="Synonym file")
 parser.add_argument("-e", "--separator", action="store", dest="separator", default="\t",
                     help="Separator used in input file")
+parser.add_argument("-k", "--key_index", action="store", dest="key_index", type=int, default=0,
+                    help="Key column in file with synonyms(0-based)")
+parser.add_argument("-v", "--value_index", action="store", dest="value_index", type=int, default=1,
+                    help="Value column in file with synonyms(0-based)")
 parser.add_argument("-o", "--output", action="store", dest="output", default=None,
                     help="Output file")
 
@@ -23,4 +27,4 @@ args = parser.parse_args()
 FileRoutines.replace_column_value_by_syn(args.input, args.syn_file, args.output, column=args.column_number,
                                          comment_prefix=args.comment_prefix, separator=args.separator,
                                          syn_header=False, syn_separator="\t",
-                                         syn_key_index=0, syn_value_index=1, syn_comment_prefix=None)
+                                         syn_key_index=args.key_index, syn_value_index=args.value_index, syn_comment_prefix=None)
